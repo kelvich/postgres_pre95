65a8e50c67524ada967b342618e15813e4193e92
sp <sp>
1990-01-31 03:51:20 +0000

    Prs2Locks has been changed to RulLock and many small modifications. However, most of the coed is still #ifedef out....

--- src/backend/rules/prs2/prs2delete.c
+++ src/backend/rules/prs2/prs2delete.c
@@ -29,6 +29,7 @@ Prs2Status prs2Delete() { }
  * Activate any rules that apply to the deleted tuple
  *
  * Arguments:
+ *      prs2EStateInfo: loop detection information stored in the EState
  *	tuple: the tuple to be deleted
  *	buffer:	the buffer for 'tuple'
  *	relation: the relation where 'tuple' belongs
@@ -36,48 +37,87 @@ Prs2Status prs2Delete() { }
  *	PRS2MGR_INSTEAD: do not perform the delete
  *      PRS2MGR_TUPLE_UNCHANGED: Ok, go ahead & delete the tuple
  */
-prs2Delete(tuple, buffer, relation)
+Prs2Status
+prs2Delete(prs2EStateInfo, tuple, buffer, relation)
+Prs2EStateInfo prs2EStateInfo;
 HeapTuple tuple;
 Buffer buffer;
 Relation relation;
 {
-    Prs2Locks locks;
+    RuleLock locks;
     Prs2OneLocks oneLock;
     int nlocks;
     int i;
     int status;
     LispDisplay plan;
-    LispDisplay planInfo;
+    LispDisplay ruleInfo;
     LispDisplay planQual;
     LispDisplay planAction;
+    ParamListInfo paramList;
+    AttributeValues attrValues;
 
+    /*
+     * Find the locks of the tuple.
+     * NOTE: Do not search the 'RelationRelation' for any
+     * locks! The reason is that the right locks have already
+     * been put to this tuple by the rule manager when it was
+     * called as part of a retrieve operation in the lower
+     * scan nodes opf the plan.
+     */
     locks = prs2GetLocksFromTuple (tuple, buffer, &(relation->rd_att));
+
+    /*
+     * now extract from the tuple the array of the attribute values.
+     */
+    attrValues = attributeValuesCreate(tuple, buffer, relation);
 
     nlocks = prs2GetNumberOfLocks(locks);
     for (i=0; i<nlocks; i++) {
 	oneLock = prs2GetOneLockFromLocks(locks, i);
-	if (prs2OneLockGetEventType != EventTypeDelete) {
+	if (prs2OneLockGetLockType == LockTypeDelete) {
 	    /*
-	     * Ignore this lock & go to the next one
+	     * We've found a lock. Go in the system catalogs and
+	     * extract the appropriate plan info...
 	     */
-	    continue;
+	    plan = prs2GetRulePlanFromCatalog(
+				prs2OneLockGetRuleId(oneLock),
+				prs2OneLockGetPlanNumber(oneLock),
+				&paramList);
+	    ruleInfo = prs2GetRuleInfoFromRulePlan(plan);
+	    planQual = prs2GetQualFromRulePlan(plan);
+	    planAction = prs2GetActionsFromRulePlan(plan);
+
+	    /*
+	     * Now calculate all the parameters needed to fill the plan
+	     */
+            prs2CalculateAttributesOfParamNodes(
+                        prs2EStateInfo,
+                        relation, tuple-t_oid,
+                        locks,
+                        paramList,
+                        attributeValues);
+	    /*
+	     * Check for the plan qualification
+	     */
+	    if (prs2CheckQual(planQual, paramList, prs2EStateInfo)) {
+		/*
+		 * the qualifcation is true. Activate the rule.
+		 */
+		prs2RunActionPlans(planAction, paramList, prs2EStateInfo);
+	    }
 	}
-	/*
-	 * We've found a lock. Go in the system catalogs and
-	 * extract the appropriate plan info...
-	 */
-	plan = prs2GetRulePlanFromCatalog(
-			    prs2OneLockGetRuleId(oneLock),
-			    prs2OneLockGetPlanNumber(oneLock));
-	planInfo = prs2GetRuleInfoFromRulePlan(plan);
-	planQual = prs2GetQualFromRulePlan(plan);
-	planAction = prs2GetActionsFromRulePlan(plan);
-
-
-	
-			
+    }/*for*/
 	
     prs2FreeLocks(locks);
-    return(status);
+    attributeValuesFree(attrValues);
+
+    /*
+     * Was this rule an 'instead' rule ???
+     */
+    if (prs2IsRuleInsteadFromRuleInfo(ruleInfo)) {
+	return(PRS2_STATUS_INSTEAD);
+    } else {
+	return(PRS2_STATUS_TUPLE_UNCHANGED);
+    }
 }
 #endif NOT_YET
