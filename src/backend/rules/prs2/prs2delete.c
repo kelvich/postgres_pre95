b4b2c6c4ade29ce0a1c1b08cf0a0ef51568b3420
sp <sp>
1990-03-07 17:19:00 +0000

    Now it works... (famous last words)

--- src/backend/rules/prs2/prs2delete.c
+++ src/backend/rules/prs2/prs2delete.c
@@ -19,9 +19,7 @@
 #include "rel.h"
 #include "prs2.h"
 
-Prs2Status prs2Delete() { }
 
-#ifdef NOT_YET
 /*------------------------------------------------------------------
  *
  * prs2Delete()
@@ -44,17 +42,10 @@ HeapTuple tuple;
 Buffer buffer;
 Relation relation;
 {
+
     RuleLock locks;
-    Prs2OneLocks oneLock;
-    int nlocks;
-    int i;
-    int status;
-    LispDisplay plan;
-    LispDisplay ruleInfo;
-    LispDisplay planQual;
-    LispDisplay planAction;
-    ParamListInfo paramList;
     AttributeValues attrValues;
+    bool insteadRuleFound;
 
     /*
      * Find the locks of the tuple.
@@ -64,60 +55,48 @@ Relation relation;
      * called as part of a retrieve operation in the lower
      * scan nodes opf the plan.
      */
-    locks = prs2GetLocksFromTuple (tuple, buffer, &(relation->rd_att));
+    locks = prs2GetLocksFromTuple (tuple, buffer,
+			RelationGetTupleDescriptor(relation));
 
     /*
      * now extract from the tuple the array of the attribute values.
      */
     attrValues = attributeValuesCreate(tuple, buffer, relation);
 
-    nlocks = prs2GetNumberOfLocks(locks);
-    for (i=0; i<nlocks; i++) {
-	oneLock = prs2GetOneLockFromLocks(locks, i);
-	if (prs2OneLockGetLockType == LockTypeDelete) {
-	    /*
-	     * We've found a lock. Go in the system catalogs and
-	     * extract the appropriate plan info...
-	     */
-	    plan = prs2GetRulePlanFromCatalog(
-				prs2OneLockGetRuleId(oneLock),
-				prs2OneLockGetPlanNumber(oneLock),
-				&paramList);
-	    ruleInfo = prs2GetRuleInfoFromRulePlan(plan);
-	    planQual = prs2GetQualFromRulePlan(plan);
-	    planAction = prs2GetActionsFromRulePlan(plan);
+    /*
+     * activate all 'on delete' rules...
+     * NOTE: all these rules must have an attribute no equal to
+     * `InvalidAttributeNumber' because they do not refer to any
+     * field in particular....
+     */
+    prs2ActivateForwardChainingRules(
+	    prs2EStateInfo,
+	    relation,
+	    InvalidAttributeNumber,
+	    LockTypeDeleteAction,
+	    PRS2_OLD_TUPLE,
+	    tuple->t_oid,
+	    attrValues,
+	    locks,
+	    LockTypeRetrieveWrite,
+	    InvalidObjectId,
+	    InvalidAttributeValues,
+	    InvalidRuleLock,
+	    LockTypeInvalid,
+	    &insteadRuleFound);
 
-	    /*
-	     * Now calculate all the parameters needed to fill the plan
-	     */
-            prs2CalculateAttributesOfParamNodes(
-                        prs2EStateInfo,
-                        relation, tuple-t_oid,
-                        locks,
-                        paramList,
-                        attributeValues);
-	    /*
-	     * Check for the plan qualification
-	     */
-	    if (prs2CheckQual(planQual, paramList, prs2EStateInfo)) {
-		/*
-		 * the qualifcation is true. Activate the rule.
-		 */
-		prs2RunActionPlans(planAction, paramList, prs2EStateInfo);
-	    }
-	}
-    }/*for*/
-	
+    /*
+     * free allocated stuff...
+     */
     prs2FreeLocks(locks);
     attributeValuesFree(attrValues);
 
     /*
      * Was this rule an 'instead' rule ???
      */
-    if (prs2IsRuleInsteadFromRuleInfo(ruleInfo)) {
+    if (insteadRuleFound) {
 	return(PRS2_STATUS_INSTEAD);
     } else {
 	return(PRS2_STATUS_TUPLE_UNCHANGED);
     }
 }
-#endif NOT_YET
