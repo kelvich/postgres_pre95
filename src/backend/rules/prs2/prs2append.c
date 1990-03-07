67cb1241151b2e48412a6397c6190199df3513a7
sp <sp>
1990-03-07 17:15:31 +0000

    Now it works!

--- src/backend/rules/prs2/prs2append.c
+++ src/backend/rules/prs2/prs2append.c
@@ -20,9 +20,6 @@
 #include "prs2.h"
 
 
-Prs2Status prs2Append() { };
-
-#ifdef NOT_YET
 /*-------------------------------------------------------------------
  *
  * prs2Append
@@ -37,29 +34,26 @@ Relation relation;
 HeapTuple *returnedTupleP;
 Buffer *returnedBufferP;
 {
-    RuleLock locks;
-    Prs2OneLocks oneLock;
-    int nlocks;
-    int i;
-    int status;
-    LispDisplay plan;
-    LispDisplay ruleInfo;
-    LispDisplay planQual;
-    LispDisplay planAction;
-    ParamListInfo paramList;
+    RuleLock locks, locksInTuple, locksInRelation;
     AttributeValues attrValues;
-    AttributeNumber numberOfAttributes;
-    AttributeNumber attrNo;
+    AttributeNumber numberOfAttributes, attrNo;
+    int newTupleMade;
+    bool insteadRuleFound;
+    Name relName;
 
     /*
      * Find the locks of the tuple.
-     * NOTE: Do not search the 'RelationRelation' for any
-     * locks! The reason is that the right locks have already
-     * been put to this tuple by the rule manager when it was
-     * called as part of a retrieve operation in the lower
-     * scan nodes opf the plan.
+     * Use both the locks stored in the tuple
+     * and the locks found in the RelationRelation
      */
-    locks = prs2GetLocksFromTuple (tuple, buffer, &(relation->rd_att));
+    locksInTuple = prs2GetLocksFromTuple (tuple, buffer, 
+                                RelationGetTupleDescriptor(relation));
+    relName = & ((RelationGetRelationTupleForm(relation))->relname);
+    locksInRelation = prs2GetLocksFromRelation(relName);
+    locks = prs2LockUnion(locksInTuple, locksInRelation);
+    prs2FreeLocks(locksInTuple);
+    prs2FreeLocks(locksInRelation);
+
 
     /*
      * now extract from the tuple the array of the attribute values.
@@ -69,72 +63,71 @@ Buffer *returnedBufferP;
     /*
      * first calculate all the attributes of the tuple.
      * I.e. see if there is any rule that will calculate a new
-     * value for a tuple's attribute.
-     * NOTE: these rules will have a LockTypeWrite.
+     * value for a tuple's attribute (i.e. a backward chaining rule).
      */
     numberOfAttributes = RelationGetNumberOfAttributes(relation);
     for (attrNo=1; attrNo <= numberOfAttributes; attrNo++) {
-	prs2CalculateOneAttribute(
-	    prs2EStateInfo,
-	    relation,
-	    tuple->t_oid,
-	    attrNo,
-	    locks,
-	    attrValues);
+	prs2ActivateBackwardChainingRules(
+		prs2EStateInfo,
+		relation,
+		attrNo,
+		PRS2_NEW_TUPLE,
+		InvalidObjectId,
+		InvalidAttributeValues,
+		InvalidRuleLock,
+		LockTypeInvalid,
+		tuple->t_oid,
+		attrValues,
+		locks,
+		LockTypeAppendWrite);
     }
 
     /*
      * now, try to activate all other 'on append' rules.
-     * (these ones will have a LockTypeOnAppend lock ....)
+     * (these ones will have a LockTypeAppendAction lock ....)
+     * NOTE: the attribute number storde in the locks for these
+     * rules must be `InvalidAttributeNumber' because they
+     * do not refer to any attribute in particular.
      */
-    nlocks = prs2GetNumberOfLocks(locks);
-    for (i=0; i<nlocks; i++) {
-	oneLock = prs2GetOneLockFromLocks(locks, i);
-	if (prs2OneLockGetLockType(oneLock) == LockTypeOnAppend) {
-	    /*
-	     * We've found a lock. Go in the system catalogs and
-	     * extract the appropriate plan info...
-	     */
-	    plan = prs2GetRulePlanFromCatalog(
-				prs2OneLockGetRuleId(oneLock),
-				prs2OneLockGetPlanNumber(oneLock),
-				&paramList);
-	    ruleInfo = prs2GetRuleInfoFromRulePlan(plan);
-	    planQual = prs2GetQualFromRulePlan(plan);
-	    planAction = prs2GetActionsFromRulePlan(plan);
+    prs2ActivateForwardChainingRules(
+	    prs2EStateInfo,
+	    relation,
+	    InvalidAttributeNumber,
+	    LockTypeAppendAction,
+	    PRS2_NEW_TUPLE,
+	    InvalidObjectId,
+	    InvalidAttributeValues,
+	    InvalidRuleLock,
+	    LockTypeInvalid,
+	    tuple->t_oid,
+	    attrValues,
+	    locks,
+	    LockTypeAppendWrite,
+	    &insteadRuleFound);
 
-	    /*
-	     * Now calculate all the parameters needed to fill the plan
-	     */
-            prs2CalculateAttributesOfParamNodes(
-                        prs2EStateInfo,
-                        relation, tuple-t_oid,
-                        locks,
-                        paramList,
-                        attributeValues);
-	    /*
-	     * Check for the plan qualification
-	     */
-	    if (prs2CheckQual(planQual, paramList, prs2EStateInfo)) {
-		/*
-		 * the qualifcation is true. Activate the rule.
-		 */
-		prs2RunActionPlans(planAction, paramList, prs2EStateInfo);
-	    }
-	}
-    }/*for*/
-	
+    /*
+     * Now all the correct values for the new tuple
+     * (the ones proposed by the user + the ones calculated
+     * by rule with 'LockTypeAppendWrite' locks,
+     * are in the attrValues array.
+     * Create a new tuple with the correct values.
+     */
+    newTupleMade = attributeValuesMakeNewTuple(
+				tuple, buffer,
+				attrValues, locks, LockTypeRetrieveWrite,
+				relation, returnedTupleP);
+
     prs2FreeLocks(locks);
     attributeValuesFree(attrValues);
 
-    /*
-     * Was this rule an 'instead' rule ???
-     */
-    if (prs2IsRuleInsteadFromRuleInfo(ruleInfo)) {
+    if (insteadRuleFound) {
 	return(PRS2_STATUS_INSTEAD);
+    }
+
+    if (newTupleMade) {
+	*returnedBufferP = InvalidBuffer;
+	return(PRS2_STATUS_TUPLE_CHANGED);
     } else {
 	return(PRS2_STATUS_TUPLE_UNCHANGED);
     }
 }
-
-#endif NOT_YET
