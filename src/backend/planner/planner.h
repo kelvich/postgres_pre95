5097a843f4c72fe516742517b5d3fae691ba728d
ong <ong>
1990-03-12 13:08:07 +0000

    added make_sortplan().

--- src/backend/planner/planner.h
+++ src/backend/planner/planner.h
@@ -1,5 +1,6 @@
 /* $Header$ */
 
 extern Plan planner ARGS((LispValue parse));
+extern Plan make_sortplan ARGS((List tlist, List sortkeys, List sortops, List plannode));
 extern Plan init_query_planner ARGS((LispValue root, LispValue tlist, LispValue qual));
 extern Existential make_existential ARGS((Plan left, Plan right));
