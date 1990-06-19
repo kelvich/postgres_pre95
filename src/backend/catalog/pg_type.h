e3cfe669c8f0075c2297a8ad5d32924393badc8c
cimarron <cimarron>
1990-06-19 08:15:21 +0000

    made changes to support RELATION types

--- src/backend/catalog/pg_type.h
+++ src/backend/catalog/pg_type.h
@@ -36,7 +36,7 @@ CATALOG(pg_type) BOOTSTRAP {
     int2  	typlen;
     int2  	typprtlen;
     bool  	typbyval;
-    bool  	typisrel;
+    char  	typtype;
     bool  	typisdefined;
     oid 	typrelid;
     oid  	typelem;
@@ -65,7 +65,7 @@ typedef FormData_pg_type	*Form_pg_type;
 #define Anum_pg_tupe_typlen		3
 #define Anum_pg_tupe_typprtlen		4
 #define Anum_pg_tupe_typbyval		5
-#define Anum_pg_tupe_typisrel		6
+#define Anum_pg_tupe_typtype		6
 #define Anum_pg_tupe_typisdefined	7
 #define Anum_pg_tupe_typrelid		8
 #define Anum_pg_tupe_typelem		9
@@ -80,35 +80,36 @@ typedef FormData_pg_type	*Form_pg_type;
  * ----------------
  */
 
-DATA(insert OID = 16 (  bool       6  1   1 t f t 0   0 boolin boolout boolin boolout - ));
-DATA(insert OID = 17 (  bytea      6 -1  -1 f f t 0  18 byteain byteaout byteain byteaout - ));
-DATA(insert OID = 18 (  char       6  1   1 t f t 0   0 charin charout charin charout - ));
-DATA(insert OID = 19 (  char16     6 16  16 f f t 0  18 char16in char16out char16in char16out - ));
-DATA(insert OID = 20 (  dt         6  4  10 t f t 0   0 dtin dtout dtin dtout - ));
-DATA(insert OID = 21 (  int2       6  2   5 t f t 0   0 int2in int2out int2in int2out - ));
-DATA(insert OID = 22 (  int28      6 16  50 f f t 0  21 int28in int28out int28in int28out - ));
-DATA(insert OID = 23 (  int4       6  4  10 t f t 0   0 int4in int4out int4in int4out - ));
-DATA(insert OID = 24 (  regproc    6  4  16 t f t 0   0 regprocin regprocout regprocin regprocout - ));
-DATA(insert OID = 25 (  text       6 -1  -1 f f t 0  18 textin textout textin textout - ));
-DATA(insert OID = 26 (  oid        6  4  10 t f t 0   0 int4in int4out int4in int4out - ));
-DATA(insert OID = 27 (  tid        6  6  19 f f t 0   0 tidin tidout tidin tidout - ));
-DATA(insert OID = 28 (  xid        6  5  12 f f t 0   0 xidin xidout xidin xidout - ));
-DATA(insert OID = 29 (  cid        6  1   3 t f t 0   0 cidin cidout cidin cidout - ));
-DATA(insert OID = 30 (  oid8       6 32  89 f f t 0  26 oid8in oid8out oid8in oid8out - ));
-DATA(insert OID = 31 (  lock       6 -1  -1 f f t 0  -1 lockin lockout lockin lockout - ));
+DATA(insert OID = 15 (  relation   6 -1  -1 t r t 0   0 textin textout textin textout - ));
+DATA(insert OID = 16 (  bool       6  1   1 t b t 0   0 boolin boolout boolin boolout - ));
+DATA(insert OID = 17 (  bytea      6 -1  -1 f b t 0  18 byteain byteaout byteain byteaout - ));
+DATA(insert OID = 18 (  char       6  1   1 t b t 0   0 charin charout charin charout - ));
+DATA(insert OID = 19 (  char16     6 16  16 f b t 0  18 char16in char16out char16in char16out - ));
+DATA(insert OID = 20 (  dt         6  4  10 t b t 0   0 dtin dtout dtin dtout - ));
+DATA(insert OID = 21 (  int2       6  2   5 t b t 0   0 int2in int2out int2in int2out - ));
+DATA(insert OID = 22 (  int28      6 16  50 f b t 0  21 int28in int28out int28in int28out - ));
+DATA(insert OID = 23 (  int4       6  4  10 t b t 0   0 int4in int4out int4in int4out - ));
+DATA(insert OID = 24 (  regproc    6  4  16 t b t 0   0 regprocin regprocout regprocin regprocout - ));
+DATA(insert OID = 25 (  text       6 -1  -1 f b t 0  18 textin textout textin textout - ));
+DATA(insert OID = 26 (  oid        6  4  10 t b t 0   0 int4in int4out int4in int4out - ));
+DATA(insert OID = 27 (  tid        6  6  19 f b t 0   0 tidin tidout tidin tidout - ));
+DATA(insert OID = 28 (  xid        6  5  12 f b t 0   0 xidin xidout xidin xidout - ));
+DATA(insert OID = 29 (  cid        6  1   3 t b t 0   0 cidin cidout cidin cidout - ));
+DATA(insert OID = 30 (  oid8       6 32  89 f b t 0  26 oid8in oid8out oid8in oid8out - ));
+DATA(insert OID = 31 (  lock       6 -1  -1 f b t 0  -1 lockin lockout lockin lockout - ));
     
-DATA(insert OID = 591 (  ref 6 8 1 f f t 0 0 refn refout refin refout - ));
+DATA(insert OID = 591 (  ref 	   6  8   1 f b t 0   0 refn refout refin refout - ));
     
-DATA(insert OID = 600 (  point     6 16  24 f f t 0   0 point_in point_out point_in point_out - ));
-DATA(insert OID = 601 (  lseg      6 32  48 f f t 0 600 lseg_in lseg_out lseg_in lseg_out - ));
-DATA(insert OID = 602 (  path      6 -1  -1 f f t 0 600 path_in path_out path_in path_out - ));
-DATA(insert OID = 603 (  box       6 32 100 f f t 0 600 box_in box_out box_in box_out - ));
+DATA(insert OID = 600 (  point     6 16  24 f b t 0   0 point_in point_out point_in point_out - ));
+DATA(insert OID = 601 (  lseg      6 32  48 f b t 0 600 lseg_in lseg_out lseg_in lseg_out - ));
+DATA(insert OID = 602 (  path      6 -1  -1 f b t 0 600 path_in path_out path_in path_out - ));
+DATA(insert OID = 603 (  box       6 32 100 f b t 0 600 box_in box_out box_in box_out - ));
     
-DATA(insert OID = 700 (  float4    6  4  12 f f t 0   0 float4in float4out float4in float4out - ));
-DATA(insert OID = 701 (  float8    6  8  24 f f t 0   0 float8in float8out float8in float8out - ));
-DATA(insert OID = 702 (  abstime   6  4  20 t f t 0   0 abstimein abstimeout abstimein abstimeout - ));
-DATA(insert OID = 703 (  reltime   6  4  20 t f t 0   0 reltimein reltimeout reltimein reltimeout - ));
-DATA(insert OID = 704 (  tinterval 6 12  47 f f t 0   0 tintervalin tintervalout tintervalin tintervalout - ));
+DATA(insert OID = 700 (  float4    6  4  12 f b t 0   0 float4in float4out float4in float4out - ));
+DATA(insert OID = 701 (  float8    6  8  24 f b t 0   0 float8in float8out float8in float8out - ));
+DATA(insert OID = 702 (  abstime   6  4  20 t b t 0   0 abstimein abstimeout abstimein abstimeout - ));
+DATA(insert OID = 703 (  reltime   6  4  20 t b t 0   0 reltimein reltimeout reltimein reltimeout - ));
+DATA(insert OID = 704 (  tinterval 6 12  47 f b t 0   0 tintervalin tintervalout tintervalin tintervalout - ));
     
 /* ----------------
  *	old definition of TypeTupleForm
@@ -124,7 +125,7 @@ typedef struct	TypeTupleFormD {
 	int16		typlen;
 	int16		typprtlen;
 	Boolean		typbyval;
-	Boolean		typisrel;
+	char		typtype;
 	Boolean 	typisdefined;
 	ObjectId	typrelid;
 	ObjectId	typelem;
@@ -153,7 +154,7 @@ struct	type {
 	int16	typlen;
 	int16	typprtlen;
 	Boolean	typbyval;
-	Boolean	typisrel;
+	char	typtype;
 	Boolean typisdefined;
 	OID	typrelid;
 	OID	typelem;
