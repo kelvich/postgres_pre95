d80103dea1f9733b6c2b4195225c62135fad1a66
hong <hong>
1990-08-17 13:40:03 +0000

    new routines for ScanTemp

--- src/backend/executor/x_scan.h
+++ src/backend/executor/x_scan.h
@@ -1,2 +1,5 @@
 /* $Header$ */
 extern List ExecScan ARGS((Plan node, int accessMtd));
+extern List ExecScanTemp ARGS((ScanTemp node));
+extern List ExecInitScanTemp ARGS((ScanTemp node, EState estate, int level));
+extern void ExecEndScanTemp ARGS((ScanTemp node));
