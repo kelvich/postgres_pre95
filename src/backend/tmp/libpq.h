0dc100b01aaa00eced0611c5658fee010bc926ad
cimarron <cimarron>
1990-08-23 20:15:20 +0000

    version of libpq.h from Igor Metz

--- src/backend/tmp/libpq.h
+++ src/backend/tmp/libpq.h
@@ -1,3 +1,22 @@
+/* 
+ * POSTGRES Data Base Management System
+ * 
+ * Copyright (c) 1989 Regents of the University of California
+ * 
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for educational, research, and non-profit purposes and
+ * without fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright notice and
+ * this permission notice appear in supporting documentation, and that
+ * the name of the University of California not be used in advertising
+ * or publicity pertaining to distribution of the software without
+ * specific, written prior permission.  Permission to incorporate this
+ * software into commercial products can be obtained from the Campus
+ * Software Office, 295 Evans Hall, University of California, Berkeley,
+ * Ca., 94720.  The University of California makes no representations
+ * about the suitability of this software for any purpose.  It is
+ * provided "as is" without express or implied warranty.
+ */ 
 /*
  * libpq.h -- 
  *	POSTGRES frontend LIBPQ buffer structure definitions.
@@ -11,7 +30,6 @@
 
 #include <stdio.h>
 #include <string.h>
-
 #include "utils/exc.h"
 
 /*
@@ -110,35 +128,35 @@ extern Exception MemoryError, PortalError, PostquelError, ProtocolError;
 /*
  * External functions.
  */
-extern PortalBuffer *addPortal ();
-extern GroupBuffer *addGroup ();
-extern TypeBlock *addTypes ();
-extern TupleBlock *addTuples ();
-extern char **addTuple ();
-extern char *addValues ();
-extern PortalEntry *addPortalEntry ();
-extern void freePortalEntry ();
-extern void freePortal ();
-extern PortalBuffer *portal_setup ();
-extern void portal_close ();
-extern int PQnportals ();
-extern void PQpnames ();
-extern PortalBuffer *PQparray ();
-extern int PQrulep ();
-extern int PQntuples ();
-extern int PQngroups ();
-extern int PQntuplesGroup ();
-extern int PQnfieldsGroup ();
-extern int PQfnumberGroup ();
-extern char *PQfnameGroup ();
+extern PortalBuffer *addPortal ARGS((void));
+extern GroupBuffer *addGroup ARGS((PortalBuffer *portal));
+extern TypeBlock *addTypes ARGS((int n));
+extern TupleBlock *addTuples ARGS((void));
+extern char **addTuple ARGS((int n));
+extern char *addValues ARGS((int n));
+extern PortalEntry *addPortalEntry ARGS((void));
+extern void freePortalEntry ARGS((int i));
+extern void freePortal ARGS((PortalBuffer *portal));
+extern PortalBuffer *portal_setup ARGS((char *pname));
+extern void portal_close ARGS((char *pname));
+extern int PQnportals ARGS((int rule_p));
+extern void PQpnames ARGS((char **pnames, int rule_p));
+extern PortalBuffer *PQparray ARGS((char *pname));
+extern int PQrulep ARGS((PortalBuffer *portal));
+extern int PQntuples ARGS((PortalBuffer *portal));
+extern int PQngroups ARGS((PortalBuffer *portal));
+extern int PQntuplesGroup ARGS((PortalBuffer *portal, int group_index));
+extern int PQnfieldsGroup ARGS((PortalBuffer *portal, int group_index));
+extern int PQfnumberGroup ARGS((PortalBuffer *portal, int group_index, char *field_name));
+extern char *PQfnameGroup ARGS((PortalBuffer *portal, int group_index, int field_number));
 extern int PQftypeGroup ();
-extern int PQnfields ();
-extern int PQfnumber ();
-extern char *PQfname ();
-extern int PQftype ();
-extern int PQsametype ();
-extern int PQgetgroup ();
-extern char *PQgetvalue ();
+extern int PQnfields ARGS((PortalBuffer *portal, int tuple_index));
+extern int PQfnumber ARGS((PortalBuffer *portal, int tuple_index, char *field_name));
+extern char *PQfname ARGS((PortalBuffer *portal, int tuple_index, int field_number));
+extern int PQftype ARGS((PortalBuffer *portal, int tuple_index, int field_number));
+extern int PQsametype ARGS((PortalBuffer *portal, int tuple_index1, int tuple_index2));
+extern int PQgetgroup ARGS((PortalBuffer *portal, int tuple_index));
+extern char *PQgetvalue ARGS((PortalBuffer *portal, int tuple_index, int field_number));
 extern void PQerror ();
 
 extern char *PQdb ARGS((void));
@@ -155,6 +173,37 @@ extern read_remark ARGS((char id[]));
 extern char *PQfn ARGS((int fnid, void *result_buf, int result_len, int result_is_int, PQArgBlock *args, int nargs));
 extern char *PQexec ARGS((char *query));
 extern void InitVacuumDemon ARGS((String host, String database, String terminal, String option, String port, String vacuum));
+
+
+/*
+ *The following internal variables of libqp can be accessed by the programmer:
+ */
+extern
+char    *PQhost;                /* the machine on which POSTGRES backend
+                                   is running */
+extern
+char    *PQport;         	/* the communication port with the
+                                   POSTGRES backend */
+extern
+char    *PQtty;                 /* the tty on PQhost backend message
+                                   is displayed */
+extern
+char    *PQoption;              /* the optional arguments to POSTGRES
+                                   backend */
+extern
+char    *PQdatabase;            /* the POSTGRES database to access */
+
+extern
+int     PQportset;          	/* 1 if the communication with backend
+                                   is set */
+extern
+int     PQxactid ;           	/* the transaction id of the current
+                                   transaction */
+extern
+char    *PQinitstr;      	/* the initialization string passed
+                                   to backend */
+extern
+int     PQtracep;           	/* 1 to print out debugging message */
 
 
 #endif	/* ifndef LibpqIncluded */
