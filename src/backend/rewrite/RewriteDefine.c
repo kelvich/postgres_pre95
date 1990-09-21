c9ec00b99e594e5a26b720051fb950dde6b2dbd3
goh <goh>
1990-09-21 09:43:25 +0000

    oops, forgot this, matches up with View.c,1.2
    - jeff

--- src/backend/rewrite/RewriteDefine.c
+++ src/backend/rewrite/RewriteDefine.c
@@ -30,7 +30,78 @@ typedef EventType Event;
 					    parse_root(ptree),0))
 #define ShowParseQual(ptree)	lispDisplay(parse_qualification(ptree),0)
 
-OID  InsertRule ();
+/*
+ *	InsertRule
+ *	DESC :  takes the arguments and inserts them
+ *		as attributes into the system relation "pg_prs2rule"
+ *
+ *	MODS :	changes the value of LastOidProcessed as a side
+ *		effect of inserting the rule tuple
+ *
+ *	ARGS :	rulname		-	name of the rule
+ *		evtype		-	one of RETRIEVE,REPLACE,DELETE,APPEND
+ *		evobj		-	name of relation
+ *		evslot		-	comma delimited list of slots
+ *					if null => multi-attr rule
+ *		evinstead	-	is an instead rule
+ *		actiontree	-	parsetree(s) of rule action
+ */
+
+
+OID
+InsertRule ( rulname , evtype , evobj , evslot , evqual, evinstead ,
+	     actiontree , necessary, sufficient )
+     Name 	rulname;
+     int 	evtype;
+     Name 	evobj;
+     Name 	evslot;
+     char	*evqual;
+     bool	evinstead;
+     char	*actiontree;
+     double	necessary, sufficient;
+{
+    static char	rulebuf[4096];
+    ObjectId rule_oid = InvalidObjectId;
+    ObjectId eventrel_oid = InvalidObjectId;
+    AttributeNumber evslot_index = InvalidAttributeNumber;
+    Relation eventrel = NULL;
+
+    eventrel = amopenr( evobj );
+    if ( eventrel == NULL ) {
+	elog ( WARN, "rules cannot be defined on relations not in schema");
+    }
+    eventrel_oid = RelationGetRelationId(eventrel);
+
+    /*
+     * if the slotname is null, we know that this is a multi-attr
+     * rule
+     */
+    if ( evslot == NULL )
+      evslot_index = -1;
+    else
+      evslot_index = varattno ( eventrel,evslot );
+
+    sprintf(rulebuf,
+"	    append pg_prs2rule (prs2name=\"%s\",prs2eventtype=\"%d2\"::char,\
+	    prs2eventrel=%d::oid,prs2eventattr= %d::int2,\
+	    prs2text= `%s`::text , \
+            necessary = %f , sufficient = %f )",
+	    rulname,
+	    AtomValueGetString(evtype),
+	    eventrel_oid,
+	    evslot_index,
+	    actiontree,
+	    necessary,
+	    sufficient);
+
+    elog(NOTICE,"rule is \n%s\n", rulebuf );
+
+    pg_eval(rulebuf);
+
+    elog(NOTICE,"RuleOID is : %d\n", LastOidProcessed );
+
+    return ( LastOidProcessed );
+}
 
 /*
  *	for now, event_object must be a single attribute
@@ -129,230 +200,6 @@ DefineQueryRewrite ( args )
 		       prs2actiontype  );
     }
 }
-
-/*
- *	InsertRule
- *	ARGS :	rulname		-	name of the rule
- *		evtype		-	one of RETRIEVE,REPLACE,DELETE,APPEND
- *		evobj		-	name of relation
- *		evslot		-	comma delimited list of slots
- *		evinstead	-	is an instead rule
- *		actiontree	-	parsetree(s) of rule action
- */
-
-
-OID
-InsertRule ( rulname , evtype , evobj , evslot , evqual, evinstead ,
-	     actiontree , necessary, sufficient )
-     Name 	rulname;
-     int 	evtype;
-     Name 	evobj;
-     Name 	evslot;
-     char	*evqual;
-     bool	evinstead;
-     char	*actiontree;
-     double	necessary, sufficient;
-{
-    static char	rulebuf[1024];
-    ObjectId rule_oid = InvalidObjectId;
-    ObjectId eventrel_oid = InvalidObjectId;
-    AttributeNumber evslot_index = InvalidAttributeNumber;
-    Relation eventrel = NULL;
-
-    /* XXX - executor cannot
-       handle appends with oids yet, so this
-       won't work. temporarily, hack executor/result.c
-       to set LastOidProcesed.
-
-       rule_oid = newoid(); /* lib/catalog/catalog.c */
-
-    eventrel = amopenr( evobj );
-    if ( eventrel == NULL ) {
-	elog ( WARN, "rules cannot be defined on relations not in schema");
-    }
-    eventrel_oid = RelationGetRelationId(eventrel);
-
-    /*
-     * if the slotname is null, we know that this is a multi-attr
-     * rule
-     */
-    if ( evslot == NULL )
-      evslot_index = -1;
-    else
-      evslot_index = varattno ( eventrel,evslot );
-
-    sprintf(rulebuf,
-"	    append pg_prs2rule (prs2name=\"%s\",prs2eventtype=\"%d2\"::char,\
-	    prs2eventrel=%d::oid,prs2eventattr= %d::int2,\
-	    prs2text= `%s`::text , \
-            necessary = %f , sufficient = %f )",
-	    rulname,
-	    AtomValueGetString(evtype),
-	    eventrel_oid,
-	    evslot_index,
-	    actiontree,
-	    necessary,
-	    sufficient);
-
-    printf("rule is \n%s\n", rulebuf );
-    pg_eval(rulebuf);
-
-    printf("RuleOID is : %d\n", LastOidProcessed );
-
-    return ( LastOidProcessed );
-}
-
-/*	DefineVirtualRelation
- *	- takes a relation name and a targetlist
- *	and generates a string "create relation ( ... )"
- *	by taking the attributes and types from the tlist
- *	and reconstructing the attr-list for create.
- *	then calls "pg-eval" to evaluate the creation,
- */
-  
-#include "nodes/relation.h"
-#include "nodes/relation.a.h"
-#include "catalog_utils.h"
-#include "nodes/primnodes.h"
-#include "nodes/primnodes.a.h"
-
-DefineVirtualRelation ( relname , tlist )
-     char *relname;
-     List tlist;
-{
-    LispValue element;
-    static char querybuf[1024];
-    char *index = querybuf;
-    int i;
-
-    for ( i = 0 ; i < 1024 ; i++ ) {
-	querybuf[i] = NULL;
-    }
-
-    sprintf(querybuf,"create %s(",relname );
-    index += strlen(querybuf);
-    
-    if ( ! null ( tlist )) {
-	foreach ( element, tlist ) {
-	    TLE 	entry = get_entry((TL)element);
-	    Resdom 	res   = get_resdom(entry);
-	    Name	resname = get_resname(res);
-	    ObjectId	restype = get_restype(res);
-	    Name	restypename = tname(get_id_type(restype));
-
-	    sprintf(index,"%s = %s,",resname,restypename);
-	    index += strlen(index);
-	}
-	*(index-1) = ')';
-	
-    } else
-	elog ( WARN, "attempted to define virtual relation with no attrs");
-    printf("\n%s\n\n",querybuf);
-    pg_eval(querybuf);
-    fflush(stdout);
-}    
-
-Name
-attname ( relname , attnum )
-     Name relname;
-     int attnum;
-{
-    Relation relptr;
-    HeapTuple atp;
-    ObjectId reloid;
-    Name varname;
-
-    relptr = amopenr (relname );
-    reloid = RelationGetRelationId ( relptr );
-   
-    atp = SearchSysCacheTuple ( ATTNUM, reloid, attnum , NULL, NULL );
-    if (!HeapTupleIsValid(atp)) {
-	elog(WARN, "getattnvals: no attribute tuple %d %d",
-	     reloid, attnum);
-	return(0);
-    }
-    varname = (Name)&( ((AttributeTupleForm) GETSTRUCT(atp))->attname );
-
-    return ( varname );
-}
-
-/*	HandleView
- *	- takes a "viewname", "parsetree" pair and then
- *	1)	construct the "virtual" relation 
- *	2)	commit the command, so that the relation exists
- *		before the rules are defined.
- *	2)	define the "n" rules specified in the PRS2 paper
- *		over the "virtual" relation
- */
-
-DefineView ( viewname, parsetree )
-     String viewname;
-     List parsetree;
-{
-    List tlist = parse_targetlist(parsetree);
-    LispValue element;
-    extern LispValue p_rtable;
-
-    int i;
-
-    if ( getreldesc (viewname) == NULL ) 
-      DefineVirtualRelation ( viewname , tlist );
-
-    if ( ! null ( tlist )) {
-	i = 0;
-	foreach ( element, tlist ) {
-	    TLE 	entry = get_entry((TL)element);
-	    Resdom 	res   = get_resdom(entry);
-	    Var		var   = get_expr(entry);
-	    int		varno = get_varno(var);
-	    Name	resname = get_resname(res);
-	    Name	varname;
-	    Name	varattname;
-	    LispValue	temp;
-
-	    temp = (LispValue)root_rangetable(parse_root(parsetree));
-	    for ( i = 1 ; i < varno ; i ++ )
-	      temp = CDR(temp);
-
-	    varname = (Name)CString(CAR(CAR(temp)));
-
-	    varattname = attname ( varname, get_varattno(var) );
-	    DefineViewRule ( viewname, resname, varname, varattname, i++ );
-
-	}
-    }	
-
-}
-
-DefineViewRule(viewname,viewattr,basename,baseattr,attrno)
-     String viewname;
-     String viewattr;
-     String basename;
-     String baseattr;
-     AttributeNumber attrno;
-{
-    static char rulebuffer[1024];
-    int i = 0;
-
-    for ( i = 0 ; i < 1024 ; i++ ) {
-	rulebuffer[i] = NULL;
-    }
-
-    sprintf(rulebuffer,
-    "define rule _r%s%d on retrieve to %s.%s do instead retrieve %s.%s",
-	    viewname,attrno,viewname,viewattr,basename,baseattr);
-    sprintf(rulebuffer,
-	    "define rule _a%s%d on append to %s.%s do instead",
-	    viewname,attrno,viewname,viewattr,basename,baseattr);
-    sprintf(rulebuffer,
-	    "define rule _d%s%d on delete to %s do instead",
-	    viewname,attrno,viewname,viewattr,basename,baseattr);
-    sprintf(rulebuffer,
-	    "define rule _i%s%d on replace to %s do instead", 
-	    viewname,attrno,viewname,viewattr,basename,baseattr);
-    
-}
-
 
 ShowRuleAction(ruleaction)
      LispValue ruleaction;
