2215be063f433649bcf23264eb26edbd2e854ad9
cimarron <cimarron>
1990-10-01 12:04:33 +0000

    Initial revision

---
+++ src/backend/executor/x_hash.h
@@ -0,0 +1,13 @@
+/* $Header$ */
+extern List ExecHash ARGS((Hash node));
+extern List ExecInitHash ARGS((Hash node, EState estate, int level, Plan parent));
+extern void ExecEndHash ARGS((Hash node));
+extern HashJoinTable ExecHashTableCreate ARGS((Plan node, int nbatch));
+extern void ExecHashTableInsert ARGS((HashJoinTable hashtable, ExprContext econtext, Var hashkey, HashState hashstate));
+extern void ExecHashTableDestroy ARGS((HashJoinTable hashtable));
+extern int ExecHashGetBucket ARGS((HashJoinTable hashtable, ExprContext econtext, Var hashkey));
+extern void ExecHashOverflowInsert ARGS((HashJoinTable hashtable, HashBucket bucket, HeapTuple heapTuple));
+extern HeapTuple ExecScanHashBucket ARGS((HashJoinState hjstate, HashBucket bucket, HeapTuple curtuple, List hjclauses, ExprContext econtext));
+extern int hashFunc ARGS((char *key, int len));
+extern int ExecHashPartition ARGS((Hash node));
+extern void ExecHashTableReset ARGS((HashJoinTable hashtable, int ntuples));
---
+++ src/backend/executor/x_hashjoin.h
@@ -0,0 +1,9 @@
+/* $Header$ */
+extern TupleTableSlot ExecHashJoin ARGS((HashJoin node));
+extern List ExecInitHashJoin ARGS((Sort node, EState estate, int level, Plan parent));
+extern void ExecEndHashJoin ARGS((HashJoin node));
+extern TupleTableSlot ExecHashJoinOuterGetTuple ARGS((Plan node, int curbatch, HashJoinState hjstate));
+extern TupleTableSlot ExecHashJoinGetSavedTuple ARGS((HashJoinState hjstate, char *buffer, File file, int *block, char **position));
+extern void ExecHashJoinNewBatch ARGS((HashJoinState hjstate, int newbatch));
+extern int ExecHashJoinGetBatch ARGS((int bucketno, HashJoinTable hashtable, int nbatch));
+extern char *ExecHashJoinSaveTuple ARGS((HeapTuple heapTuple, char *buffer, File file, char *position));
