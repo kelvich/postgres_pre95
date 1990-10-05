0cb17cd36c6f25e483f7f5c10e34671b625b3a83
goh <goh>
1990-10-05 10:03:30 +0000

    Initial revision

---
+++ src/backend/rewrite/RewriteDefine.h
@@ -0,0 +1,7 @@
+
+/* module: /users/goh/postgres/src/parser/RewriteDefine.c */
+
+unsigned int InsertRule(struct char16 *, int, struct char16 *, struct char16 *, char *, char, char *, double, double);
+void ModifyActionToReplaceCurrent(struct _LispValue *);
+int DefineQueryRewrite(struct _LispValue *);
+int ShowRuleAction(struct _LispValue *);
