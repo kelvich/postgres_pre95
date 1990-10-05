1ab75d9bdd2015c384df28fbdc9aace10a488103
goh <goh>
1990-10-05 11:43:17 +0000

    changed some args in prototypes

--- src/backend/rewrite/RewriteHandler.h
+++ src/backend/rewrite/RewriteHandler.h
@@ -11,7 +11,7 @@ struct _LispValue *ModifyVarNodes(struct _LispValue *, int, int, struct Relation
 struct _LispValue *MatchLocks(char, struct Prs2LocksData *, int, struct _LispValue *);
 struct _LispValue *MatchReplaceLocks(struct Prs2LocksData *, int, struct _LispValue *);
 struct _LispValue *MatchAppendLocks(struct Prs2LocksData *, int, struct _LispValue *);
-struct _LispValue *MatchDeleteLocks(void);
+struct _LispValue *MatchDeleteLocks(struct Prs2LocksData *, int, struct _LispValue *);
 struct _LispValue *ModifyUpdateNodes(struct _LispValue *, struct _LispValue *, char *, int);
 struct _LispValue *ProcessOneLock(struct _LispValue *, struct RelationData *, struct _LispValue *, int, int, char *);
 struct _LispValue *QueryRewrite(struct _LispValue *);
