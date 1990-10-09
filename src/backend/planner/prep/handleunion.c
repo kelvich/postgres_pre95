9fdead65556f1d1110c1badcafdf0446cc4935b7
ong <ong>
1990-10-09 11:51:04 +0000

    Initial revision

---
+++ src/backend/planner/prep/handleunion.c
@@ -0,0 +1,456 @@
+/*
+ *  Routine takes the 'union' parsetree, messes around with it, 
+ *  strips the union relations, and returns
+ *  a list of orthogonal plans.
+ *
+ *  NOTE: Current implementation of versions disallows schema changes 
+ *        between versions.  Thus we may assume that the attributes
+ *        remain the same between versions and their deltas.
+ *
+ * $Header:
+ */
+
+#include "tmp/c.h"
+
+#include "nodes/nodes.h"
+#include "nodes/pg_lisp.h"
+#include "nodes/execnodes.h"
+#include "nodes/plannodes.h"
+#include "nodes/plannodes.a.h"
+#include "nodes/relation.h"
+#include "nodes/relation.a.h"
+
+#include "parser/parse.h"
+#include "parser/parsetree.h"
+#include "tmp/utilities.h"
+#include "utils/log.h"
+#include "utils/lsyscache.h"
+
+#include "planner/internal.h"
+#include "planner/plancat.h"
+#include "planner/planner.h"
+#include "planner/prepunion.h"
+#include "planner/clauses.h"
+#include "planner/handleunion.h"
+#include "planner/semanopt.h"
+/* #include "planner/cfi.h" */
+
+List
+handleunion (root,rangetable, tlist, qual)
+     List root, rangetable, tlist, qual;
+{
+
+  List new_root = LispNil;
+  List temp_tlist = LispNil;
+  List temp_qual = LispNil;
+  List i = LispNil;
+  List tlist_qual = LispNil;
+  List rt_entry = LispNil;
+  List temp = LispNil;
+  List new_parsetree = LispNil;
+  List union_plans = LispNil;
+
+  /*
+   * SplitTlistQual returns a list of the form:
+   * ((tlist1 qual1) (tlist2 qual2) ...) 
+   */
+
+  tlist_qual = SplitTlistQual (root,rangetable,tlist,qual);
+
+  foreach (i, tlist_qual) {
+    temp = CAR(i);
+    new_parsetree = lispCons(root,lispCons(CAR(temp),
+					   lispCons(CADR(temp),
+						    LispNil) ));
+    union_plans = nappend1(union_plans,
+			   planner(new_parsetree));
+
+  }
+  
+return(union_plans);  
+
+}
+
+
+/*
+ *  Given a 'union'ed tlist and qual, this routine splits them
+ *  up into separate othrogonal (tlist qual) pairs.
+ *  For example, 
+ *  tlist:  (emp | emp1).name
+ *  qual:   (emp | emp1). salary < 5000
+ *  will be split into:
+ *  ( ((emp.name) (emp.salary < 5000)) ((emp1.name) (emp1.salary < 5000)) )
+ */
+
+
+List
+SplitTlistQual(root,rangetable,tlist, qual)
+     List root,rangetable,tlist, qual;
+{
+  List i = LispNil;
+  List tqlist = LispNil;
+  List  temp = LispNil;
+  List tl_list = LispNil;
+  List qual_list = LispNil;
+  List unionlist = LispNil;
+  List mod_qual = LispNil;
+
+  unionlist = find_union_vars(rangetable);
+  /*
+   *  If query is a delete, the tlist is null.
+   */
+  if (CAtom(root_command_type_atom(root)) == DELETE) 
+    foreach(i,unionlist) 
+      tl_list = nappend1(tl_list, LispNil);
+  else
+    tl_list = SplitTlist(unionlist,tlist);
+
+  qual_list = SplitQual(unionlist,qual); 
+
+/*
+ *  The assumption here is that both the tl_list and the qual_list, 
+ *  must be of the same length, if not, something funky has happened.
+ */
+
+  if (length (tl_list) != length(qual_list))
+    elog (WARN, "UNION:  tlist with missing qual, or vice versa");
+
+  foreach(i, tl_list) {
+    temp = CAR(i);
+    mod_qual = SemantOpt(root,rangetable,temp,CAR(qual_list));
+    tqlist = nappend1(tqlist,
+		      lispCons(temp, 
+			       lispCons(mod_qual,
+					LispNil)));
+    qual_list = CDR(qual_list);
+  }
+
+return (tqlist);
+
+}
+
+
+
+/*
+ * SplitTlist
+ * returns a list of the form (tlist1 tlist2 ...)
+ */
+
+List
+SplitTlist (unionlist,tlist)
+     List unionlist;
+     List tlist;
+{
+  List i = LispNil;
+  List j = LispNil;
+  List x = LispNil;
+  List varnum = LispNil;
+  List new_tlist ;
+  List tle = LispNil;
+  List varlist = LispNil;
+  Var varnode = (Var)NULL;
+  List tl_list = LispNil;
+
+  foreach (i, unionlist) {
+    varnum = CAR(i);
+    new_tlist = LispNil;
+    new_tlist = copy_seq_tree(tlist);
+
+    foreach (x, new_tlist) {
+      tle = CAR(x);
+      varlist = tl_expr(tle);
+      if (IsA(varlist,Const) || IsA(varlist,Var)) ;
+      else
+	if (CAtom(CAR(varlist)) == UNION) {
+	  foreach (j, CDR(varlist)) {
+	    varnode = (Var)CAR(j);
+	    /*
+	     * Find matching entry, and change the tlist to it.
+	     *
+	     * NOTE: Also need to handle the case when a particular
+	     *       rel. is not found in the union.  In that case, 
+	     *       we should remove the tle (i.e, (resdom var) pair).
+	     *       HOWEVER, this is not done just yet.
+	     */
+	    if (CInteger(varnum) == get_varno(varnode)) {
+	      tl_expr(tle) = (List)varnode;
+	      break;
+	    }
+	  } /* for, varlist */
+	}
+    } /* for, new_tlist*/
+    
+    /*
+     *  At this point, the new_tlist is an orthogonal targetlist, without
+     *  any union relations.
+     */
+    
+    if (new_tlist == NULL) 
+      elog(WARN, "UNION: resulting tlist is empty");
+    
+    tl_list = nappend1(tl_list,new_tlist);
+  }
+  return(tl_list);
+
+}
+
+
+/*
+ *  find_union_sets
+ *  runs through the rangetable, and forms a list of all union
+ *  sets.  Routine removes the union flag from the rte after
+ *  it is done processing .
+ */
+
+List
+find_union_sets(rangetable)
+     List rangetable;
+{
+
+}
+/*
+ *  find_union_vars
+ *  runs through the rangetable, and forms a list of all the 
+ *  relations that are unioned. It will also remove the union
+ *  flag from the rangetable entries after it is done processing
+ *  them.
+ *  REturns a list of varnos.
+ */
+
+List
+find_union_vars (rangetable)
+     List rangetable;
+{
+  
+  List i = LispNil;
+  List unionlist = LispNil;
+  List rt_entry = LispNil;
+  Index uvarno = 0;
+
+  /*
+   * XXX what about the case when there are 2 emps in the rangetable, and
+   * both are union relations?
+   * Currently, both would appear in the unionlist.
+   */
+
+  foreach( i, rangetable) {
+    rt_entry = CAR(i);
+    uvarno += 1;
+    if (member(lispAtom("union"),rt_flags(rt_entry))) {
+      unionlist = nappend1(unionlist, lispInteger(uvarno));
+      rt_flags(rt_entry) = remove(lispAtom("union"), 
+				  rt_flags(rt_entry)); 
+    }     
+  }
+  return(unionlist);
+}
+
+/*
+  List tle = LispNil;
+  List expr = LispNil;
+  List x = LispNil;
+  List varlist = LispNil;
+  Var unionvar = (Var)NULL;
+
+  foreach (i,tlist) {
+    tle = CAR(i);
+    expr = tl_expr(tle);
+
+    if (CAtom(CAR(expr)) == UNION ) {
+      varlist = CDR(expr);
+      foreach (x, varlist) {
+	unionvar = (Var)CAR(x);
+	uvarno = get_varno(unionvar);
+	if (!(member(lispInteger(uvarno), unionlist)))
+	  unionlist = nappend1(unionlist,
+			       lispInteger(uvarno));
+      }
+    }
+  }
+*/
+
+
+/*
+ *  SplitQual:
+ *
+ *  Given a list of union rels, this routine splits the qualification
+ *  up accordingly.  
+ *  NOTE:  Only union rels in the qualification will be split.
+ *  ( I believe that utimately, we want to split union rels by position. )
+ *
+ *  Input:  (uvar1 uvar2 ...) (unioned qual)
+ *  Output: (qual1 qual2 ...)
+ */
+
+List 
+SplitQual (ulist, uqual)
+     List ulist;
+     List uqual;
+{
+  List i = LispNil;
+  List uvarno = LispNil;
+  List qual_list = LispNil;
+  List currentlist = LispNil;
+  List new_qual = LispNil;
+
+  if (uqual == LispNil) {
+    foreach(i, ulist) 
+      qual_list = nappend1(qual_list, LispNil);
+    return(qual_list);
+  }
+  foreach(i, ulist) {
+    uvarno = CAR(i);
+    currentlist = nappend1(currentlist, uvarno);
+    new_qual = copy_seq_tree(uqual);
+    new_qual = find_matching_union_qual(currentlist, new_qual);
+    qual_list = nappend1(qual_list, new_qual);
+    currentlist = LispNil;
+  }
+    
+return(qual_list);
+
+}
+
+/*
+ * find_matching_union_qual
+ *
+ * Given a union qual, and a union relation, returns 
+ * the qualifications that apply to it.
+ */
+
+List 
+find_matching_union_qual(ulist, qual)
+     List ulist, qual;
+{
+  List leftop = LispNil;
+  List rightop = LispNil;
+  List op = LispNil;
+  List matching_clauseargs = LispNil;
+  List retqual = LispNil;
+  List i = LispNil;
+  List tmp_list = LispNil;
+
+  if (null(qual))
+    return(LispNil);
+  else
+    if (is_clause(qual)) {
+      leftop = (List)get_leftop(qual);
+      rightop = (List)get_rightop(qual);
+      op = get_op(qual);
+      leftop = find_matching_union_qual(ulist,leftop);
+      rightop = find_matching_union_qual(ulist,rightop);
+      match_union_clause(ulist, &leftop, &rightop);
+      retqual = make_opclause(op,leftop,rightop);
+
+      /*
+       * if the clause does not belong to the current union relation,
+       * remove it from the qualification.
+       */
+
+      /*
+      if (matching_clauseargs == LispNil)
+	remove(clause, clauses);  * NOTE: may have to mod. qual *
+      else
+	clauseargs = matching_clauseargs; 
+      */
+    }
+  else
+    if (and_clause (qual)) {
+      foreach(i, get_andclauseargs(qual)) {
+	tmp_list = nappend1(tmp_list, find_matching_union_qual(ulist, 
+							     CAR(i)));
+      }
+      retqual = make_andclause(tmp_list);
+    }
+    else
+      if (or_clause (qual)) {
+	foreach(i, get_orclauseargs(qual)) {
+	  tmp_list = nappend1(tmp_list, find_matching_union_qual(ulist, 
+								 CAR(i)));
+	}
+	retqual = make_orclause(tmp_list);
+      }
+      else
+	if (not_clause (qual)) {
+	  retqual = find_matching_union_qual(ulist, 
+					     get_notclausearg(qual));
+	  retqual = make_notclause(retqual);
+	}
+	else
+	  retqual = qual;
+    
+  return(retqual);
+}
+/*
+ *  match_union_clauses:
+ *
+ *  Given a list of *2* clause args, (leftarg rightarg), 
+ *  it picks out the clauses within unions that
+ *  are relevant to the particular union relation being scanned.
+ *
+ *  returns nothing. It modifies the qual in place.
+ */
+
+void
+match_union_clause (unionlist, leftarg, rightarg)
+     List unionlist, *leftarg, *rightarg;
+{
+  List i = LispNil;
+  List varlist = LispNil;
+  Var uvarnode = (Var)NULL;
+  List varlist2 = LispNil;
+
+  /*
+   *  If leftarg is a union var.
+   */
+  if (consp(*leftarg) && CAtom(CAR(*leftarg)) == UNION) {
+    varlist = CDR(*leftarg);
+
+    /* (Op (uvar) Const) */
+    if (IsA(*rightarg,Const)) {
+      foreach (i, varlist) {
+	uvarnode = (Var)CAR(i);
+	if (member(lispInteger(get_varno(uvarnode)), unionlist)) {
+	  *leftarg = (List)uvarnode;
+	  break;
+	}
+      }
+    } /* Const */
+    /* (Op (uvar) var) */
+    else if (IsA(*rightarg,Var)) {
+      foreach (i, varlist) {
+	uvarnode = (Var)CAR(i);
+	if (member(lispInteger(get_varno(uvarnode)), unionlist)) {
+	  *leftarg = (List)uvarnode;
+	  break;
+	}
+      }
+    } /* Var */
+
+    /* 
+     * (Op (uvar1) (uvar2) )
+     *  This gets real hairy.
+     *
+     *  If we ever get here, we have a union join.
+     *  In this case, we need to have a ulist for each set 
+     *  of union joins.
+     *  Eg:
+     *  retrieve (A | B).all where (A | B).foo = (B | C).bar 
+     *    and (B | A). bla = const
+     *  the 2 ulists are: (a b) and (b c), and the union join query
+     *  will be split into
+     *  retrieve (A.all) where A.foo = B.bar and B.bla = const
+     *  and
+     *  retrieve (B.all) where B.foo = C.bar and A.bla = const
+     *
+     *  XXX sounds problematic.
+     *  Supported.
+     */
+    else if (consp(*rightarg)) {
+      if (CAtom(CAR(*rightarg)) == UNION) {
+	varlist2 = CDR(*rightarg);
+      }
+    }
+  } /* UNION */
+
+}
---
+++ src/backend/planner/prep/semanopt.c
@@ -0,0 +1,356 @@
+/*
+ *  This file contains routines to perform semantic optimization for
+ *  versions (i.e, union queries that pertain to versions).
+ *  As such, heuristics used to govern the optimization are applicable
+ *  only to the current implementation of versions, and should not be 
+ *  used as a general purpose semantic optimizer.
+ *
+ *  $Header$
+ */
+
+#include <stdio.h>
+#include <strings.h>
+#include "tmp/c.h"
+
+#include "nodes/nodes.h"
+#include "nodes/pg_lisp.h"
+#include "nodes/execnodes.h"
+#include "nodes/plannodes.h"
+#include "nodes/plannodes.a.h"
+#include "nodes/primnodes.h"
+#include "nodes/primnodes.a.h"
+#include "nodes/relation.h"
+#include "nodes/relation.a.h"
+
+#include "parser/parse.h"
+#include "parser/parsetree.h"
+#include "tmp/utilities.h"
+#include "tmp/datum.h"
+#include "utils/log.h"
+#include "utils/lsyscache.h"
+
+#include "planner/internal.h"
+#include "planner/plancat.h"
+#include "planner/planner.h"
+#include "planner/prepunion.h"
+#include "planner/clauses.h"
+#include "planner/semanopt.h"
+
+/*
+ *  SemantOpt:
+ * 
+ *  Given a tlist and qual pair, routine runs through the 
+ *  qualification and substitute all redundent clauses.  Eg:
+ *
+ *   TClause : tautological clauses  (A.oid = A.oid) becomes (1=1)
+ *   CClause : contradictory clauses (A.oid = B.oid where A != B)
+ *             becomes where 1=2
+ *   EClause : clauses that causes an unwanted existential query.
+ *             Clauses that have no link back to the targetlist
+ *             defaults to true (i.e., 1=1).
+ *             THEREFORE, this will only work for AND clauses.
+ *
+ *   We cannot simply remove CClauses and EClauses from the qualification
+ *   because then any boolean operations done on the qualification may
+ *   result in the wrong answer.
+ *
+ *  Returns the optimized qual.
+ */
+
+List
+SemantOpt(root,rangetable,tlist, qual)
+     List root,rangetable,tlist, qual;
+{
+  List retqual = LispNil;
+  List varlist = LispNil;
+  Index leftvarno = 0;
+  Index rightvarno = 0;
+  ObjectId op = 0;
+  List tmp_list = LispNil;
+  List i = LispNil;
+
+  /*
+   *   NOTE:  Routine doesn't handle func clauses.
+   */
+
+  /*
+   *  Varlist contains the list of varnos of relations that participate
+   *  in the current query.  It is used to detect existential clauses.
+   *  Initially, varlist contains the list of target relations (varnos).
+   */
+  varlist = find_allvars(root,rangetable,tlist,qual);
+  retqual = qual;
+
+  if (null(qual))
+    return(LispNil);
+  else
+    if (is_clause(qual)) {
+      op = get_opno(get_op(qual));
+      if (op == 627) {
+	leftvarno = get_varno(get_leftop(qual));
+	rightvarno = ConstVarno(rangetable, get_rightop(qual));
+	/*
+	 *  test for existential clauses first 
+	 */
+	if (!member(lispInteger(leftvarno),varlist) &&
+	    !member(lispInteger(rightvarno),varlist))
+	  retqual = MakeTClause();
+	else 
+	  if (leftvarno == rightvarno)
+	    retqual = MakeFClause();
+      } else 
+	if (op == 558) {
+	  leftvarno = get_varno(get_leftop(qual));
+	  rightvarno = get_varno(get_rightop(qual));
+
+	  if (!member(lispInteger(leftvarno),varlist) &&
+	      !member(lispInteger(rightvarno),varlist))
+	    retqual = MakeTClause();	
+	  else 
+	    if (leftvarno != rightvarno)
+	      retqual = MakeFClause();
+	    else
+	      retqual = MakeTClause();
+	} else 
+	  /* Just a normal op clause, check to see if it is existential */
+	  if (IsA(get_leftop(qual),Var)) {
+	    leftvarno = get_varno(get_leftop(qual));
+	    if (IsA(get_rightop(qual),Var)) {
+	      rightvarno = get_varno(get_rightop(qual));
+	      if (!member(lispInteger(leftvarno),varlist) &&
+		  !member(lispInteger(rightvarno),varlist))
+		retqual = MakeTClause();       /* this is true only for ANDS */
+	    }
+	    if (!member(lispInteger(leftvarno),varlist))
+	      retqual = MakeTClause();
+	  }	      
+    } else  
+      if (and_clause (qual)) {
+	foreach(i,get_andclauseargs(qual)) {
+	  tmp_list = nappend1(tmp_list,SemantOpt(root,
+						     rangetable, 
+						     tlist, 
+						     CAR(i)));
+	}
+	retqual = make_andclause(tmp_list);
+      }
+      else
+	if (or_clause (qual)) {
+	  foreach(i,get_orclauseargs(qual)) {
+	    tmp_list = nappend1(tmp_list,SemantOpt(root,
+						   rangetable, 
+						   tlist, 
+						   CAR(i)));
+	  }
+	  retqual = make_orclause(tmp_list);
+	}
+	else
+	  if (not_clause (qual))
+	    retqual = make_notclause(SemantOpt(root,
+					       rangetable, 
+					       tlist, 
+					       get_notclausearg(qual)));
+  
+  return (retqual);
+  
+}
+
+/*
+ * routine is no longer needed.
+ */
+List
+add_varlist(leftvarno, rightvarno, varlist,qual)
+     List varlist,qual;
+     Index leftvarno,rightvarno;
+{
+  if (!member(lispInteger(leftvarno), varlist))
+    varlist = nappend1(varlist, lispInteger(leftvarno));  
+  if ((rightvarno != 0) && 
+      !member(lispInteger(rightvarno),varlist)) 
+    varlist = nappend1(varlist, lispInteger(rightvarno));
+
+  return(varlist);
+}
+	  
+
+
+List
+find_allvars (root,rangetable,tlist,qual)
+     List root,rangetable,tlist,qual;
+{
+  List varlist = LispNil;
+  List i = LispNil;
+  List tle = LispNil;
+  List expr = LispNil;
+  int current_nvars;
+  int prev_nvars = 0;
+
+  if (CAtom(root_command_type_atom(root)) != RETRIEVE) 
+    varlist = nappend1(varlist, root_result_relation(root));
+  
+  foreach(i, tlist) {
+    tle = CAR(i);
+    expr = tl_expr(tle);
+    /*
+     *  Assume that each (non const) expr is a varnode by this time.
+     */
+    if (IsA(expr,Var)) {
+      if (!member(lispInteger(get_varno(expr)),varlist))
+	varlist = nappend1(varlist, lispInteger(get_varno(expr)));
+    }
+  }
+  current_nvars = length(varlist);
+
+  while(current_nvars > prev_nvars) {
+    prev_nvars = current_nvars;
+    varlist = update_vars(rangetable,varlist,qual);
+    current_nvars = length(varlist);
+  }
+  return(varlist);
+}
+
+/*
+ *  Simply goes through the qual once and
+ *  add any new varnos (rels) that participate in a 
+ *  join. 
+ *  A rel. participates in the query if there is a link back to 
+ *  the target relations.
+ */
+
+List
+update_vars(rangetable,varlist,qual)
+     List rangetable,varlist,qual;
+{
+  List leftop;
+  List rightop;
+  ObjectId op = 0;
+  Index leftvarno = 0;
+  Index rightvarno = 0;
+
+  if (null(qual))
+    return(varlist);
+  else 
+    if (is_clause(qual)) {
+      op = get_opno(get_op(qual));
+      if (op == 627) {  /* Greg's horrendous not-in op */
+	leftvarno = get_varno(get_leftop(qual));
+	rightvarno = ConstVarno(rangetable,get_rightop(qual));
+
+	if (member(lispInteger(leftvarno),varlist)) {
+	  if (rightvarno != 0 && !member(lispInteger(rightvarno),varlist))
+	    varlist = nappend1(varlist,lispInteger(rightvarno));
+	}
+	else
+	  if (member(lispInteger(rightvarno),varlist))
+	    if (!member(lispInteger(leftvarno),varlist))
+	      varlist = nappend1(varlist,lispInteger(leftvarno));	
+      } else {
+	leftop = (List)get_leftop(qual);
+	rightop = (List)get_rightop(qual);
+	if (IsA(leftop,Var)) {
+	  if (IsA(rightop,Var)) {
+	    if (member(lispInteger(get_varno(leftop)),varlist)) {
+	      if (!member(lispInteger(get_varno(rightop)),varlist))
+		varlist = nappend1(varlist,lispInteger(get_varno(rightop)));
+	    }
+	    else
+	      if (member(lispInteger(get_varno(rightop)),varlist))
+		if (!member(lispInteger(get_varno(leftop)),varlist))
+		  varlist = nappend1(varlist,lispInteger(get_varno(leftop)));
+	  }
+	} /* leftop var */
+      } /*else */
+    } else  /* is_clause */
+      if (and_clause(qual))
+	varlist = update_vars(rangetable,
+			      varlist,
+			      get_andclauseargs(qual));
+      else
+	if (or_clause(qual))
+	  varlist = update_vars(rangetable,
+				varlist,
+				get_orclauseargs(qual));
+	else
+	  if (not_clause(qual))
+	    varlist = update_vars(rangetable,
+				  varlist,
+				  get_notclausearg(qual));
+
+  return(varlist);
+}
+
+/*
+ *  This routine is necessary thanks to Greg's not-in operator.
+ *  Given a const node, routine returns the varno of the relation
+ *  referenced or 0 if relation is not in the rangetable.
+ */
+
+Index
+ConstVarno(rangetable,constnode)  
+     List rangetable;
+     Const constnode;
+{
+  static char tmpstring[32];
+  char *ptr;
+  char *relname;
+  List i = LispNil;
+  Index position = 0;  /* should be quite safe since varnos start from 1. */
+
+  if (!IsA(constnode,Const))
+    elog(WARN, "Arg. is not a const node.");
+  else {
+    ptr = DatumGetPointer(get_constvalue(constnode));
+    strcpy (tmpstring, ptr);
+    relname = strtok(tmpstring, ".");
+    
+    foreach (i, rangetable) {
+      position += 1;
+      if (strcmp(relname, CString(CADR(CAR(i)))) == 0) 
+	return (position);
+    }
+  }
+return(0);
+  
+}
+
+/*
+ *  Routine returns a clause of the form 1=1
+ */
+List
+MakeTClause()
+{
+  Oper newop;
+  Const leftconst;
+  Const rightconst;
+  ObjectId objid = 96;
+  ObjectId rettype = 16;
+  int opsize = 0;
+  
+  newop = MakeOper(objid, 0,rettype,opsize);
+  leftconst = MakeConst(23, 4, Int32GetDatum(1), 0, 1);
+  rightconst = MakeConst(23, 4, Int32GetDatum(1), 0, 1);
+
+  return(make_opclause(newop,leftconst,rightconst));
+  
+}
+
+/*
+ *  Routine returns a clause of the form 1=2
+ */
+List
+MakeFClause()
+{
+  Oper newop;
+  Const leftconst;
+  Const rightconst;
+  ObjectId objid = 96;
+  ObjectId rettype = 16;
+  int opsize = 0;
+  
+  newop = MakeOper(objid, 0,rettype,opsize);
+  leftconst = MakeConst(23, 4, Int32GetDatum(1), 0);
+  rightconst = MakeConst(23, 4,Int32GetDatum(2), 0);
+
+  return(make_opclause(newop,leftconst,rightconst));
+  
+}
