8925534098043d7c3ecf338d47329076668b2d2b
kemnitz <kemnitz>
1990-10-12 21:49:37 +0000

    Added new structs used by dfmgr and the new dynamic loader.

--- src/backend/utils/dynamic_loader.h
+++ src/backend/utils/dynamic_loader.h
@@ -21,4 +21,34 @@ typedef struct {
 
 extern FList ExtSyms[];
 
+/*
+ * The new dynamic loader scheme loads each file one at a time.  Therefore,
+ * we must note each function in the file at load time.
+ *
+ */
+
+/*
+ * Dynamically loaded function list.
+ */
+
+typedef struct df_list
+{
+    char funcname[16];				/* Name of function */
+    func_ptr func;					/* Function address */
+    struct df_list *next;
+}
+DynamicFunctionList;
+
+/*
+ * List of dynamically loaded files.
+ */
+
+typedef struct df_files
+{
+    char filename[256];				/* Full pathname of file */
+    DynamicFunctionList *func_list;	/* List of functions */
+    struct df_files *next;
+}
+DynamicFileList;
+
 #endif Dynamic_loaderHIncluded
