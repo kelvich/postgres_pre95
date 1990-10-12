7b7648adb1fea2a094bfea685fc45348f64472ce
ong <ong>
1990-10-12 11:20:37 +0000

    Initial revision

---
+++ src/backend/planner/handleunion.h
@@ -0,0 +1,11 @@
+/* $Header: */
+
+extern List handleunion ARGS((List root, List rangetable, List tlist, List qual));
+extern List SplitTlistQual ARGS(( List root, List rangetable,List tlist, List qual));
+extern List SplitTlist ARGS((List unionlist, List tlist));
+extern List find_union_vars ARGS((List tlist));
+extern List find_union_sets ARGS((List tlist));
+extern List SplitQual ARGS((List ulist, List uqual));
+extern void match_union_clause ARGS((List unionlist, List leftop, List rightop));
+extern List find_matching_union_qual ARGS((List ulist, List qual));     
+
---
+++ src/backend/planner/semanopt.h
@@ -0,0 +1,8 @@
+extern List SemantOpt ARGS((List root, List rangetable, List tlist, tlist qual));
+extern List add_varlist ARGS((Index leftvarno, Index rightvarno, List varlist, List qual));
+extern List find_allvars ARGS((List root,List rangetable, List tlist, List qual));
+extern Index ConstVarno ARGS((List rangetable, Const const));
+extern List MakeTClause ARGS(());
+extern List MakeFClause ARGS(());
+extern List update_vars ARGS((List rangetable, List varlist, List qual));
+
