1ab018d44139c5c3d528f2c2b0958d6da7a6d251
goh <goh>
1990-10-12 11:52:25 +0000

    added $Id:$

--- src/backend/rewrite/RewriteDefine.h
+++ src/backend/rewrite/RewriteDefine.h
@@ -1,5 +1,6 @@
 
 /* module: /users/goh/postgres/src/parser/RewriteDefine.c */
+/* $Id$ */
 
 unsigned int InsertRule(struct char16 *, int, struct char16 *, struct char16 *, char *, char, char *, double, double);
 void ModifyActionToReplaceCurrent(struct _LispValue *);
--- src/backend/rewrite/RewriteHandler.h
+++ src/backend/rewrite/RewriteHandler.h
@@ -1,11 +1,12 @@
 
 /* module: /users/goh/postgres/o/rewrite/RewriteHandler.c */
+/* $Id$ */
 
 int ChangeTheseVars(int, short, struct _LispValue *, struct _LispValue *);
 int ReplaceVarWithMulti(int, short, struct _LispValue *, struct _LispValue *);
 int FixResdom(struct _LispValue *);
 char ThisLockWasTriggered(int, short, struct _LispValue *);
-struct _LispValue *MatchRetrieveLocks(struct Prs2LocksData *, struct _LispValue *, int);
+struct _LispValue *MatchRetrieveLocks(struct Prs2LocksData *, int, struct _LispValue *);
 struct _LispValue *TheseLocksWereTriggered(struct Prs2LocksData *, struct _LispValue *, int, int);
 struct _LispValue *ModifyVarNodes(struct _LispValue *, int, int, struct RelationData *, struct _LispValue *, struct _LispValue *, struct _LispValue *);
 struct _LispValue *MatchLocks(char, struct Prs2LocksData *, int, struct _LispValue *);
@@ -15,4 +16,3 @@ struct _LispValue *MatchDeleteLocks(struct Prs2LocksData *, int, struct _LispVal
 struct _LispValue *ModifyUpdateNodes(struct _LispValue *, struct _LispValue *, char *, int);
 struct _LispValue *ProcessOneLock(struct _LispValue *, struct RelationData *, struct _LispValue *, int, int, char *);
 struct _LispValue *QueryRewrite(struct _LispValue *);
-struct _LispValue *ModifyDeleteQueries(char *);
