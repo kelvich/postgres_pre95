010454f6ddd44b6415c37c6693871f5533256182
kemnitz <kemnitz>
1990-10-14 16:13:42 +0000

    Replace ANSI-type function declarations with ARGS macro.

--- src/backend/rewrite/RewriteHandler.h
+++ src/backend/rewrite/RewriteHandler.h
@@ -2,17 +2,17 @@
 /* module: /users/goh/postgres/o/rewrite/RewriteHandler.c */
 /* $Id$ */
 
-int ChangeTheseVars(int, short, struct _LispValue *, struct _LispValue *);
-int ReplaceVarWithMulti(int, short, struct _LispValue *, struct _LispValue *);
-int FixResdom(struct _LispValue *);
-char ThisLockWasTriggered(int, short, struct _LispValue *);
-struct _LispValue *MatchRetrieveLocks(struct Prs2LocksData *, int, struct _LispValue *);
-struct _LispValue *TheseLocksWereTriggered(struct Prs2LocksData *, struct _LispValue *, int, int);
-struct _LispValue *ModifyVarNodes(struct _LispValue *, int, int, struct RelationData *, struct _LispValue *, struct _LispValue *, struct _LispValue *);
-struct _LispValue *MatchLocks(char, struct Prs2LocksData *, int, struct _LispValue *);
-struct _LispValue *MatchReplaceLocks(struct Prs2LocksData *, int, struct _LispValue *);
-struct _LispValue *MatchAppendLocks(struct Prs2LocksData *, int, struct _LispValue *);
-struct _LispValue *MatchDeleteLocks(struct Prs2LocksData *, int, struct _LispValue *);
-struct _LispValue *ModifyUpdateNodes(struct _LispValue *, struct _LispValue *, char *, int);
-struct _LispValue *ProcessOneLock(struct _LispValue *, struct RelationData *, struct _LispValue *, int, int, char *);
-struct _LispValue *QueryRewrite(struct _LispValue *);
+int ChangeTheseVars ARGS(int, short, struct _LispValue *, struct _LispValue *);
+int ReplaceVarWithMulti ARGS(int, short, struct _LispValue *, struct _LispValue *);
+int FixResdom ARGS(struct _LispValue *);
+char ThisLockWasTriggered ARGS(int, short, struct _LispValue *);
+struct _LispValue *MatchRetrieveLocks ARGS(struct Prs2LocksData *, int, struct _LispValue *);
+struct _LispValue *TheseLocksWereTriggered ARGS(struct Prs2LocksData *, struct _LispValue *, int, int);
+struct _LispValue *ModifyVarNodes ARGS(struct _LispValue *, int, int, struct RelationData *, struct _LispValue *, struct _LispValue *, struct _LispValue *);
+struct _LispValue *MatchLocks ARGS(char, struct Prs2LocksData *, int, struct _LispValue *);
+struct _LispValue *MatchReplaceLocks ARGS(struct Prs2LocksData *, int, struct _LispValue *);
+struct _LispValue *MatchAppendLocks ARGS(struct Prs2LocksData *, int, struct _LispValue *);
+struct _LispValue *MatchDeleteLocks ARGS(struct Prs2LocksData *, int, struct _LispValue *);
+struct _LispValue *ModifyUpdateNodes ARGS(struct _LispValue *, struct _LispValue *, char *, int);
+struct _LispValue *ProcessOneLock ARGS(struct _LispValue *, struct RelationData *, struct _LispValue *, int, int, char *);
+struct _LispValue *QueryRewrite ARGS(struct _LispValue *);
