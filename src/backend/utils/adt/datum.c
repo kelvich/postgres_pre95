e4a654961e0559a85fae245310ffd0db32bd1e2c
sp <sp>
1990-10-16 17:23:18 +0000

    This is actually a completely new file now It contains Datum related utility routines...

--- src/backend/utils/adt/datum.c
+++ src/backend/utils/adt/datum.c
@@ -1,275 +1,168 @@
-/*
- * datum.c --
- *	POSTGRES abstract data type datum representation code.
+/*===================================================================
+ *
+ * FILE:
+ * datum.c
+ *
+ * HEADER:
+ * $Header$
+ *
+ * DESCRIPTION:
+ *
+ * In the implementation of the next routines we assume the following:
+ *
+ * A) if a type is "byVal" then all the information is stored in the
+ * Datum itself (i.e. no pointers involved!). In this case the
+ * length of the type is always greater than zero and less than
+ * "sizeof(Datum)"
+ * B) if a type is not "byVal" and it has a fixed length, then
+ * the "Datum" always contain a pointer to a stream of bytes.
+ * The number of significant bytes are always equal to the length of thr
+ * type.
+ * C) if a type is not "byVal" and is of variable length (i.e. it has
+ * length == -1) then "Datum" always points to a "struct varlena".
+ * This varlena structure has information about the actual length of this
+ * particular instance of the type and about its value.
+ *
  */
 
-#include "c.h"
+#include "tmp/c.h"
+#include "tmp/postgres.h"
+#include "tmp/datum.h"
+#include "catalog/syscache.h"
+#include "catalog/pg_type.h"
+#include "utils/log.h"
 
-RcsId("$Header$");
-
-#include "datum.h"
-
-
-/*
- * DatumGet --
- *	Datum accessor macro.
+/*-------------------------------------------------------------------------
+ * datumGetRealLengthAndByVal
+ *
+ * Given a datum and its type, find the "real" length of the datum's value
+ * and if the type is passed "by value" or not.
+ * If the type's length is positive (i.e. fixed) then the "real" length is
+ * just the type's length.
+ * But if it is -1 (i.e. we are dealing with a variable length type)
+ * then we have to look at the struct varlena pointed by the datum.
  */
-#define DatumGet(datum, field)	(datum.field.value)
+void
+datumGetRealLengthAndByVal(value, type, sizeP, byValP)
+Datum value;
+ObjectId type;
+Size *sizeP;
+bool *byValP;
+{
 
-/*
- * DatumSet --
- *	Datum assignment macro.
+    Size typeLength;
+    HeapTuple typeTuple;
+    int i;
+    struct varlena *s;
+
+    /*
+     * find the type's "length" and "byVal"
+     */
+    typeTuple = SearchSysCacheTuple(TYPOID,
+				    (char *) type,
+				    (char *) NULL,
+				    (char *) NULL,
+				    (char *) NULL);
+    if (!HeapTupleIsValid(typeTuple)) {
+	elog(WARN,
+	    "datumGetRealLengthAndByVal: Cache lookup of type %ld failed",
+	    (long) type);
+    }
+    typeLength	= ((ObjectId) ((TypeTupleForm)
+			GETSTRUCT(typeTuple))->typlen);
+    *byValP	= ((bool) ((TypeTupleForm)
+			GETSTRUCT(typeTuple))->typbyval);
+
+    if (*byValP) {
+	if (typeLength >= 0) {
+	    *sizeP = typeLength;
+	} else {
+	    elog(WARN,
+		"datumGetRealLengthAndByVal: type %ld: (len<0 and byVal)",
+		(long) type);
+	}
+    } else { /*  not byValue */
+	if (typeLength <= -1) {
+	    /*
+	     * variable length type
+	     * Look at the varlena struct for its real length...
+	     */
+	    s = (struct varlena *) DatumGetPointer(value);
+	    if (!PointerIsValid(s)) {
+		elog(WARN,
+		    "datumGetRealLengthAndByVal: Invalid Datum Pointer");
+	    }
+	    *sizeP = (Size) PSIZE(s);
+	} else {
+	    /*
+	     * fixed length type
+	     */
+	    *sizeP = typeLength;
+	}
+    }
+}
+
+/*-------------------------------------------------------------------------
+ * copyDatum
+ *
+ * make a copy of a datum
+ *
+ * If the type of the datum is not passed by value (i.e. "byVal=flase")
+ * then we assume that the datum contains a pointer and we copy all the
+ * bytes pointed by this pointer
  */
-#define DatumSet(datum, field, newValue)	(datum.field.value) = newValue
-
-char
-DatumGetChar(datum)
-	Datum	datum;
-{
-	return (DatumGet(datum, character));
-}
-
 Datum
-CharGetDatum(character)
-	char	character;
+copyDatum(value, type)
+Datum value;
+ObjectId type;
 {
-	Datum	datum;
 
-	bzero(&datum, sizeof(Datum));
-	DatumSet(datum, character, character);
-	return (datum);
+    Size length;
+    bool byVal;
+    Datum res;
+    Pointer s;
+    char *palloc();
+
+    datumGetRealLengthAndByVal(value, type, &length, &byVal);
+
+    if (byVal) {
+	res = value;
+    } else {
+	s = (Pointer) palloc(length);
+	if (s == NULL) {
+	    elog(WARN,"copyDatum: out of memory\n");
+	}
+	bcopy(DatumGetPointer(value), s, length);
+	res = PointerGetDatum(s);
+    }
+    return(res);
 }
 
-int8
-DatumGetInt8(datum)
-	Datum	datum;
+/*-------------------------------------------------------------------------
+ * freeDatum
+ *
+ * Free the space occupied by a datum CREATED BY "copyDatum"
+ *
+ * NOTE: DO NOT USE THIS ROUTINE with datums returned by amgetattr() etc.
+ * ONLY datums created by "copyDatum" can be freed!
+ */
+void
+freeDatum(value, type)
+Datum value;
+ObjectId type;
 {
-	return (DatumGet(datum, integer8));
-}
 
-Datum
-Int8GetDatum(integer8)
-	int8	integer8;
-{
-	Datum	datum;
+    Size length;
+    bool byVal;
+    Pointer s;
 
-	bzero(&datum, sizeof(Datum));
-	DatumSet(datum, integer8, AsInt8(integer8));
-	return (datum);
-}
+    datumGetRealLengthAndByVal(value, type, &length, &byVal);
 
-uint8
-DatumGetUInt8(datum)
-	Datum	datum;
-{
-	return (DatumGet(datum, unsignedInteger8));
-}
-
-Datum
-UInt8GetDatum(unsignedInteger8)
-	uint8	unsignedInteger8;
-{
-	Datum	datum;
-
-	bzero(&datum, sizeof(Datum));
-	DatumSet(datum, unsignedInteger8, AsUint8(unsignedInteger8));
-	return (datum);
-}
-
-int16
-DatumGetInt16(datum)
-	Datum	datum;
-{
-	return (DatumGet(datum, integer16));
-}
-
-Datum
-Int16GetDatum(integer16)
-	int16	integer16;
-{
-	Datum	datum;
-
-	bzero(&datum, sizeof(Datum));
-	DatumSet(datum, integer16, integer16);
-	return (datum);
-}
-
-uint16
-DatumGetUInt16(datum)
-	Datum	datum;
-{
-	return (DatumGet(datum, unsignedInteger16));
-}
-
-Datum
-UInt16GetDatum(unsignedInteger16)
-	uint16	unsignedInteger16;
-{
-	Datum	datum;
-
-	bzero(&datum, sizeof(Datum));
-	DatumSet(datum, unsignedInteger16, unsignedInteger16);
-	return (datum);
-}
-
-int32
-DatumGetInt32(datum)
-	Datum	datum;
-{
-	return (DatumGet(datum, integer32));
-}
-
-Datum
-Int32GetDatum(integer32)
-	int32	integer32;
-{
-	Datum	datum;
-
-	bzero(&datum, sizeof(Datum));
-	DatumSet(datum, integer32, integer32);
-	return (datum);
-}
-
-uint32
-DatumGetUInt32(datum)
-	Datum	datum;
-{
-	return (DatumGet(datum, unsignedInteger32));
-}
-
-Datum
-UInt32GetDatum(unsignedInteger32)
-	uint32	unsignedInteger32;
-{
-	Datum	datum;
-
-	bzero(&datum, sizeof(Datum));
-	DatumSet(datum, unsignedInteger32, unsignedInteger32);
-	return (datum);
-}
-
-float32
-DatumGetFloat32(datum)
-	Datum	datum;
-{
-	return (DatumGet(datum, smallFloat));
-}
-
-Datum
-Float32GetDatum(smallFloat)
-	float32	smallFloat;
-{
-	Datum	datum;
-
-	bzero(&datum, sizeof(Datum));
-	DatumSet(datum, smallFloat, smallFloat);
-	return (datum);
-}
-
-float64
-DatumGetFloat64(datum)
-	Datum	datum;
-{
-	return (DatumGet(datum, largeFloat));
-}
-
-Datum
-Float64GetDatum(largeFloat)
-	float64	largeFloat;
-{
-	Datum	datum;
-
-	bzero(&datum, sizeof(Datum));
-	DatumSet(datum, largeFloat, largeFloat);
-	return (datum);
-}
-
-Pointer
-DatumGetPointer(datum)
-	Datum	datum;
-{
-	return (DatumGet(datum, pointer));
-}
-
-Datum
-PointerGetDatum(pointer)
-	Pointer	pointer;
-{
-	Datum	datum;
-
-	bzero(&datum, sizeof(Datum));
-	DatumSet(datum, pointer, pointer);
-	return (datum);
-}
-
-Pointer *
-DatumGetPointerPointer(datum)
-	Datum	datum;
-{
-	return (DatumGet(datum, pointerPointer));
-}
-
-Datum
-PointerPointerGetDatum(pointerPointerInP)
-	Pointer	*pointerPointerInP;
-{
-	Datum	datum;
-
-	bzero(&datum, sizeof(Datum));
-	DatumSet(datum, pointerPointer, pointerPointerInP);
-	return (datum);
-}
-
-AnyStruct *
-DatumGetStructPointer(datum)
-	Datum	datum;
-{
-	return (DatumGet(datum, structPointer));
-}
-
-Datum
-StructPointerGetDatum(structPointerInP)
-	AnyStruct	*structPointerInP;
-{
-	Datum	datum;
-
-	bzero(&datum, sizeof(Datum));
-	DatumSet(datum, structPointer, structPointerInP);
-	return (datum);
-}
-
-Name
-DatumGetName(datum)
-	Datum	datum;
-{
-	return (DatumGet(datum, name));
-}
-
-Datum
-NameGetDatum(name)
-	Name	name;
-{
-	Datum	datum;
-
-	bzero(&datum, sizeof(Datum));
-	DatumSet(datum, name, name);
-	return (datum);
-}
-
-ObjectId
-DatumGetObjectId(datum)
-	Datum	datum;
-{
-	return (DatumGet(datum, objectId));
-}
-
-Datum
-ObjectIdGetDatum(objectId)
-	ObjectId	objectId;
-{
-	Datum	datum;
-
-	bzero(&datum, sizeof(Datum));
-	DatumSet(datum, objectId, objectId);
-	return (datum);
+    if (!byVal) {
+	/*
+	 * free the space palloced by "copyDatum()"
+	 */
+	s = DatumGetPointer(value);
+	pfree(s);
+    }
 }
