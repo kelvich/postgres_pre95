3223c683c82f5ea42f828a4701c57e3164508c7d
ong <ong>
1990-10-16 14:12:55 +0000

    added remove_subsets

--- src/backend/planner/handleunion.h
+++ src/backend/planner/handleunion.h
@@ -5,6 +5,7 @@ extern List SplitTlistQual ARGS(( List root, List rangetable,List tlist, List qu
 extern List SplitTlist ARGS((List unionlist, List tlist));
 extern List find_union_vars ARGS((List tlist));
 extern List find_union_sets ARGS((List tlist));
+extern List remove_subsets ARGS((List usets));
 extern List SplitQual ARGS((List ulist, List uqual));
 extern void match_union_clause ARGS((List unionlist, List leftop, List rightop));
 extern List find_matching_union_qual ARGS((List ulist, List qual));     
