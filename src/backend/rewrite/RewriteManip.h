e0f231bbea8705e60f6e2835048acd4980017a08
goh <goh>
1990-10-23 11:09:14 +0000

    added new routine

--- src/backend/rewrite/RewriteManip.h
+++ src/backend/rewrite/RewriteManip.h
@@ -1,6 +1,11 @@
 
-/* module: /users/goh/postgres/src/parser/RewriteManip.c */
+/* module: /users/goh/postgres/src/parser/RewriteManip.c 
+ * $File:$
+ * $Author$
+ * $Version:$
+ */
 
+void OffsetVarNodes ARGS(( List , int ));
 void FixRangeTable ARGS(( List , List ));
 void AddQualifications ARGS(( List , List , int ));
 void HandleVarNodes ARGS(( List, List, int, int ));
