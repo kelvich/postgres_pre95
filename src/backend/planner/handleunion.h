e777aeac0b5721c3664252c3744c71ffd601969a
ong <ong>
1990-10-26 11:54:48 +0000

    added header files for new routines.

--- src/backend/planner/handleunion.h
+++ src/backend/planner/handleunion.h
@@ -5,6 +5,9 @@ extern List SplitTlistQual ARGS(( List root, List rangetable,List tlist, List qu
 extern List SplitTlist ARGS((List unionlist, List tlist));
 extern List find_union_vars ARGS((List tlist));
 extern List find_union_sets ARGS((List tlist));
+extern List collect_union_sets ARGS((List tlist, List qual));
+extern List find_qual_union_sets ARGS((List qual));
+extern List flatten_union_list ARGS((List ulist));
 extern List remove_subsets ARGS((List usets));
 extern List SplitQual ARGS((List ulist, List uqual));
 extern void match_union_clause ARGS((List unionlist, List leftop, List rightop));
