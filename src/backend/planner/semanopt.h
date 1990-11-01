24e8e650d60954640bbbe647dac0f5b4c757f7db
ong <ong>
1990-11-01 10:07:07 +0000

    added function is_redundent_query

--- src/backend/planner/semanopt.h
+++ src/backend/planner/semanopt.h
@@ -1,4 +1,5 @@
 extern List SemantOpt ARGS((List root, List rangetable, List tlist, tlist qual));
+extern List is_redundent_query ARGS((List qual, int is_first));
 extern List add_varlist ARGS((Index leftvarno, Index rightvarno, List varlist, List qual));
 extern List find_allvars ARGS((List root,List rangetable, List tlist, List qual));
 extern Index ConstVarno ARGS((List rangetable, Const const));
