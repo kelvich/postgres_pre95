aebdc3e65421d4a8c49fa7546b4827b52d22497b
hong <hong>
1990-11-07 12:05:31 +0000

    cleaning up

--- src/backend/storage/file/fd.c
+++ src/backend/storage/file/fd.c
@@ -15,7 +15,11 @@
  * set.
  */
 
+#ifdef sequent
+#define RESERVE_FOR_LD	5
+#else
 #define RESERVE_FOR_LD	10
+#endif
 
 /*
  * CURRENT HACK
@@ -508,49 +512,6 @@ tryAgain:
 
 	return file;
 }
-
-#ifdef _xprs_
-FileNumber
-FileOpen(fileName, fileFlags, fileMode) 
-	FileName	fileName;
-	int		fileFlags;
-	int		fileMode;
-{
-	static int osRanOut = 0;
-	FileNumber	file;
-	Vfd	*vfdP;
-	int     tmpfd;
-	
-	DO_DB(printf("DEBUG: FileOpen: %s %x %o\n",fileName,fileFlags,fileMode));
-
-	file = AllocateVfd();
-	vfdP = &VfdCache[file];
-
-	vfdP->fd = FileClosed;
-
-	vfdP->fileName = malloc(strlen(fileName)+1);
-	strcpy(vfdP->fileName,fileName);
-
-	vfdP->seekPos = 0;
-	vfdP->fileFlags = fileFlags & ~(O_TRUNC|O_EXCL);
-	vfdP->fileMode = fileMode;
-
-	return file;
-}
-
-void
-ReOpenFiles()
-{
-    FileNumber file;
-    Vfd *fileP;
-
-    for (file = VfdCache[0].lruMoreRecently; file != 0; file = VfdCache[file].lruMoreRecently) {
-	fileP = &VfdCache[file];
-	close(fileP->fd);
-	fileP->fd = open(fileP->fileName, fileP->fileFlags, fileP->fileMode);
-	}
-}
-#endif /* _xprs_ */
 
 void
 fileClose(file)
