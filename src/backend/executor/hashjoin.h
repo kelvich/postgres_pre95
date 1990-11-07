1b0e597edf43d33243334fe13c3fabfca6ea4f65
hong <hong>
1990-11-07 12:00:51 +0000

    changes in hash table structure for parallel execution

--- src/backend/executor/hashjoin.h
+++ src/backend/executor/hashjoin.h
@@ -20,6 +20,9 @@
 #include "access/htup.h"
 #include "storage/ipc.h"
 
+typedef char    **charPP;
+typedef int     *intP;
+
 /* ----------------------------------------------------------------
  *		hash-join hash table structures
  * ----------------------------------------------------------------
@@ -32,8 +35,25 @@ typedef struct HashTableData {
 	char		*top;
 	char		*bottom;
 	char		*overflownext;
+#ifdef sequent
+	slock_t		overflowlock;
+#endif
 	char		*batch;
 	char		*readbuf;
+        int             nbatch;
+#ifdef sequent
+	slock_t		*batchLock;
+	sbarrier_t	batchBarrier;
+	slock_t		tableLock;
+#endif
+        charPP          outerbatchNames;
+        charPP          outerbatchPos;
+        charPP          innerbatchNames;
+	charPP		innerbatchPos;
+        intP            innerbatchSizes;
+        int             curbatch;
+	int		nprocess;
+	int		pcount;
 } HashTableData;  /* real hash table follows here */
 
 typedef HashTableData	*HashJoinTable;
@@ -50,8 +70,20 @@ typedef struct HashBucketData {
 	HeapTuple	bottom;
 	OverflowTuple	firstotuple;
 	OverflowTuple	lastotuple;
+#ifdef sequent
+	slock_t		bucketlock;
+#endif
 } HashBucketData;  /* real bucket follows here */
 
 typedef HashBucketData	*HashBucket;
+
+typedef struct HashBufferData {
+	int	pageend;
+#ifdef sequent
+	slock_t	bufferlock;
+#endif
+} HashBufferData;  /* real buffer follows here */
+
+typedef HashBufferData *HashBuffer;
 
 #endif	/* !defined(HashJoinIncluded) */
--- src/backend/executor/x_hashjoin.h
+++ src/backend/executor/x_hashjoin.h
@@ -4,6 +4,6 @@ extern List ExecInitHashJoin ARGS((Sort node, EState estate, int level, Plan par
 extern void ExecEndHashJoin ARGS((HashJoin node));
 extern TupleTableSlot ExecHashJoinOuterGetTuple ARGS((Plan node, int curbatch, HashJoinState hjstate));
 extern TupleTableSlot ExecHashJoinGetSavedTuple ARGS((HashJoinState hjstate, char *buffer, File file, int *block, char **position));
-extern void ExecHashJoinNewBatch ARGS((HashJoinState hjstate, int newbatch));
+extern int ExecHashJoinNewBatch ARGS((HashJoinState hjstate, int newbatch));
 extern int ExecHashJoinGetBatch ARGS((int bucketno, HashJoinTable hashtable, int nbatch));
 extern char *ExecHashJoinSaveTuple ARGS((HeapTuple heapTuple, char *buffer, File file, char *position));
