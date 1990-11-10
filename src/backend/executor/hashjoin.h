6ca736c489cc3389250441ce0a6402563ab62ee8
hong <hong>
1990-11-10 12:25:34 +0000

    change of hash table structure

--- src/backend/executor/hashjoin.h
+++ src/backend/executor/hashjoin.h
@@ -20,6 +20,21 @@
 #include "access/htup.h"
 #include "storage/ipc.h"
 
+/* -----------------
+ *  have to use relative address as pointers in the hashtable
+ *  because the hashtable may reallocate in difference processes
+ * -----------------
+ */
+typedef int	RelativeAddr;
+
+/* ------------------
+ *  the relative addresses are always relative to the head of the
+ *  hashtable, the following macro converts them to absolute address.
+ * ------------------
+ */
+#define ABSADDR(X)	(X < 0 ? NULL: (char*)hashtable + X)
+#define RELADDR(X)	(RelativeAddr)((char*)X - (char*)hashtable)
+
 typedef char    **charPP;
 typedef int     *intP;
 
@@ -32,25 +47,25 @@ typedef struct HashTableData {
 	int		totalbuckets;
 	int		bucketsize;
 	IpcMemoryId	shmid;
-	char		*top;
-	char		*bottom;
-	char		*overflownext;
+	RelativeAddr	top;			/* char* */
+	RelativeAddr	bottom;			/* char* */
+	RelativeAddr	overflownext;		/* char* */
 #ifdef sequent
 	slock_t		overflowlock;
 #endif
-	char		*batch;
-	char		*readbuf;
+	RelativeAddr	batch;			/* char* */
+	RelativeAddr	readbuf;		/* char* */
         int             nbatch;
 #ifdef sequent
-	slock_t		*batchLock;
+	RelativeAddr	batchLock;		/* slock_t* */
 	sbarrier_t	batchBarrier;
 	slock_t		tableLock;
 #endif
-        charPP          outerbatchNames;
-        charPP          outerbatchPos;
-        charPP          innerbatchNames;
-	charPP		innerbatchPos;
-        intP            innerbatchSizes;
+        RelativeAddr    outerbatchNames;	/* RelativeAddr* */
+        RelativeAddr    outerbatchPos;		/* RelativeAddr* */
+        RelativeAddr    innerbatchNames;	/* RelativeAddr* */
+	RelativeAddr	innerbatchPos;		/* RelativeAddr* */
+        RelativeAddr    innerbatchSizes;	/* int* */
         int             curbatch;
 	int		nprocess;
 	int		pcount;
@@ -59,17 +74,17 @@ typedef struct HashTableData {
 typedef HashTableData	*HashJoinTable;
 
 typedef struct OverflowTupleData {
-	HeapTuple tuple;
-	struct OverflowTupleData *next;
+	RelativeAddr tuple;		/* HeapTuple */
+	RelativeAddr next;		/* struct OverflowTupleData * */
 } OverflowTupleData;   /* real tuple follows here */
 
 typedef OverflowTupleData *OverflowTuple;
 
 typedef struct HashBucketData {
-	HeapTuple	top;
-	HeapTuple	bottom;
-	OverflowTuple	firstotuple;
-	OverflowTuple	lastotuple;
+	RelativeAddr	top;		/* HeapTuple */
+	RelativeAddr	bottom;		/* HeapTuple */
+	RelativeAddr	firstotuple;	/* OverflowTuple */
+	RelativeAddr	lastotuple;	/* OverflowTuple */
 #ifdef sequent
 	slock_t		bucketlock;
 #endif
@@ -77,13 +92,6 @@ typedef struct HashBucketData {
 
 typedef HashBucketData	*HashBucket;
 
-typedef struct HashBufferData {
-	int	pageend;
-#ifdef sequent
-	slock_t	bufferlock;
-#endif
-} HashBufferData;  /* real buffer follows here */
-
-typedef HashBufferData *HashBuffer;
+#define HASH_PERMISSION         0700
 
 #endif	/* !defined(HashJoinIncluded) */
