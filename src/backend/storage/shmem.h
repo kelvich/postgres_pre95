e7c64bed6835001ab2bd3aa705785d16a0fd68d0
hong <hong>
1991-01-18 21:31:58 +0000

    Initial revision

---
+++ src/backend/storage/shmem.h
@@ -0,0 +1,57 @@
+/*
+ * shmem.h -- shared memory management structures
+ *
+ */
+
+#ifndef	ShMemIncluded		/* Included this file only once */
+#define ShMemIncluded	1
+
+/*
+ * Identification:
+ */
+#define SHMEM_H	"$Header$"
+
+/* The shared memory region can start at a different address
+ * in every process.  Shared memory "pointers" are actually
+ * offsets relative to the start of the shared memory region(s).
+ */
+typedef unsigned int SHMEM_OFFSET;
+#define INVALID_OFFSET (-1)
+#define BAD_LOCATION (-1)
+
+/* start of the lowest shared memory region.  For now, assume that
+ * there is only one shared memory region 
+ */
+extern SHMEM_OFFSET ShmemBase;
+
+
+/* coerce an offset into a pointer in this process's address space */
+#define MAKE_PTR(xx_offs)\
+  (ShmemBase+((unsigned int)(xx_offs)))
+
+/* coerce a pointer into a shmem offset */
+#define MAKE_OFFSET(xx_ptr)\
+  (SHMEM_OFFSET) (((unsigned int)(xx_ptr))-ShmemBase)
+
+#define SHM_PTR_VALID(xx_ptr)\
+  (((unsigned int)xx_ptr) > ShmemBase)
+
+/* cannot have an offset to ShmemFreeStart (offset 0) */
+#define SHM_OFFSET_VALID(xx_offs)\
+  ((xx_offs != 0) && (xx_offs != INVALID_OFFSET))
+
+/* shmemqueue.c */
+typedef struct SHM_QUEUE {
+  SHMEM_OFFSET	prev;
+  SHMEM_OFFSET	next;
+} SHM_QUEUE;
+
+/* shmem.c */
+int *ShmemAlloc();
+int *ShmemInitStruct();
+/* dont declare this so we avoid nested include files */
+/*HTAB *ShmemInitHash(); */
+
+typedef int TableID;
+typedef char *Addr;
+#endif	/* !defined(ShMemIncluded) */
