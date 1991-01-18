9f3ba700c4a954f47db12c1283e6b762db449d25
hong <hong>
1991-01-18 22:09:50 +0000

    Initial revision

---
+++ src/backend/storage/buffer/buf_table.c
@@ -0,0 +1,156 @@
+/*
+ * buf_table.c -- routines for finding buffers in the buffer pool.
+ *
+ * Identification:
+ *	$Header$
+ *
+ * Data Structures:
+ *
+ *	Buffers are identified by their BufferTag (buf.h).  This
+ * file contains routines for allocating a shmem hash table to
+ * map buffer tags to buffer descriptors.
+ *
+ * Synchronization:
+ *  
+ *  All routines in this file assume buffer manager spinlock is
+ *  held by their caller.
+ */
+#include "internal.h"
+#include "storage/shmem.h"
+#include "storage/spin.h"
+#include "utils/hsearch.h"
+#include "utils/log.h"
+
+HTAB *SharedBufHash,*ShmemInitHash();
+int tag_hash();
+
+/* 
+ * declared for debugging only.  
+ */
+extern SPINLOCK *BufMgrLock;
+
+typedef struct lookup { 
+  BufferTag	key; 
+  Buffer	id; 
+} LookupEnt;
+
+/*
+ * Initialize shmem hash table for mapping buffers
+ */
+InitBufTable()
+{
+  HASHCTL info;
+  int hash_flags;
+
+  /* assume lock is held */
+  is_LOCKED(BufMgrLock);
+
+  /* BufferTag maps to Buffer */
+  info.keysize = sizeof(BufferTag);
+  info.datasize = sizeof(Buffer);
+  info.hash = tag_hash;
+
+  hash_flags = (HASH_ELEM | HASH_FUNCTION);
+
+
+  SharedBufHash = (HTAB *) ShmemInitHash("Shared Buf Lookup Table",
+		  NUM_DATA_BUFS,NUM_DATA_BUFS,
+		  &info,hash_flags);
+
+  if (! SharedBufHash) {
+    elog(FATAL,"couldn't initialize shared buffer pool Hash Tbl");
+    exit(1);
+  }
+
+}
+
+BufferDesc *
+BufTableLookup(tagPtr)
+BufferTag *tagPtr;
+{
+  LookupEnt *	result;
+  Boolean	found;
+
+  if (tagPtr->blockNum == NEW_BLOCK)
+      return(NULL);
+  is_LOCKED(BufMgrLock);
+
+
+  result = (LookupEnt *) 
+    hash_search(SharedBufHash,tagPtr,FIND,&found);
+
+  if (! result){
+    elog(WARN,"BufTableLookup: BufferLookup table corrupted");
+    return(NULL);
+  }
+  if (! found) {
+    return(NULL);
+  }
+  return(&(BufferDescriptors[result->id]));
+}
+
+/*
+ * BufTableDelete
+ */
+BufTableDelete(buf)
+BufferDesc *buf;
+{
+  LookupEnt *	result;
+  Boolean	found;
+
+  /* buffer not initialized or has been removed from
+   * table already.  BM_DELETED keeps us from removing 
+   * buffer twice.
+   */
+  if (buf->flags & BM_DELETED) {
+    return(TRUE);
+  }
+
+  buf->flags |= BM_DELETED;
+
+  result = (LookupEnt *)
+    hash_search(SharedBufHash,&(buf->tag),REMOVE,&found);
+
+  if (! (result && found)) {
+    elog(WARN,"BufTableDelete: BufferLookup table corrupted");    
+    return(FALSE);
+  }
+
+  return(TRUE);
+}
+
+BufTableInsert(buf)
+BufferDesc *buf;
+{
+  LookupEnt *	result;
+  Boolean	found;
+
+  /* cannot insert it twice */
+  Assert (buf->flags & BM_DELETED);
+  buf->flags &= ~(BM_DELETED);
+
+  result = (LookupEnt *)
+    hash_search(SharedBufHash,&(buf->tag),ENTER,&found);
+
+  if (! result) {
+    elog(WARN,"BufTableInsert: BufferLookup table corrupted");
+    return(FALSE);
+  }
+  /* found something else in the table ! */
+  if (found) {
+    elog(WARN,"BufTableInsert: BufferLookup table corrupted");
+    return(FALSE);
+  } 
+
+  result->id = buf->id;
+  return(TRUE);
+}
+
+/* prints out collision stats for the buf table */
+DBG_LookupListCheck(nlookup)
+int nlookup;
+{
+  nlookup = 10;
+
+  hash_stats("Shared",SharedBufHash);
+}
---
+++ src/backend/storage/buffer/freelist.c
@@ -0,0 +1,208 @@
+/*
+ * freelist.c -- routines for manipulating the buffer pool's
+ *	replacement strategy freelist.
+ *
+ * Identification:
+ *	$Header$
+ *
+ * Data Structures:
+ *	SharedFreeList is a circular queue.  Notice that this
+ *	is a shared memory queue so the next/prev "ptrs" are
+ *	buffer ids, not addresses.
+ *
+ * Sync: all routines in this file assume that the buffer
+ * 	semaphore has been acquired by the caller.
+ */
+#include "internal.h"
+#include "storage/spin.h"
+#include "utils/log.h"
+
+static
+BufferDesc 	*SharedFreeList;
+
+/* only actually used in debugging.  The lock
+ * should be acquired before calling the freelist manager.
+ */
+extern SPINLOCK *BufMgrLock;
+
+#define IsInQueue(bf) \
+  Assert((bf->freeNext != INVALID_DESCRIPTOR));\
+  Assert((bf->freePrev != INVALID_DESCRIPTOR));\
+  Assert((bf->flags & BM_FREE))
+
+#define NotInQueue(bf) \
+  Assert((bf->freeNext == INVALID_DESCRIPTOR));\
+  Assert((bf->freePrev == INVALID_DESCRIPTOR));\
+  Assert(! (bf->flags & BM_FREE))
+
+
+/*
+ * AddBufferToFreelist --  
+ *
+ * In theory, this is the only routine that needs to be changed
+ * if the buffer replacement strategy changes.  Just change
+ * the manner in which buffers are added to the freelist queue.
+ * Currently, they are added on an LRU basis.
+ */
+AddBufferToFreelist(bf)
+BufferDesc *bf;
+{
+  NotInQueue(bf);
+
+  /* change bf so it points to inFrontOfNew and its successor */
+  bf->freePrev = SharedFreeList->freePrev;
+  bf->freeNext = FREE_LIST_DESCRIPTOR;
+
+  /* insert new into chain */
+  BufferDescriptors[bf->freeNext].freePrev = bf->id;
+  BufferDescriptors[bf->freePrev].freeNext = bf->id;
+}
+
+/*
+ * PinBuffer -- make buffer unavailable for replacement.
+ */
+PinBuffer(buf)
+    BufferDesc *buf;
+{
+  is_LOCKED(BufMgrLock);
+
+  /* Assert (buf->refcount < 25); */
+
+  if (buf->refcount == 0) {
+    IsInQueue(buf);
+
+    /* remove from freelist queue */
+    BufferDescriptors[buf->freeNext].freePrev = buf->freePrev;
+    BufferDescriptors[buf->freePrev].freeNext = buf->freeNext;
+    buf->freeNext = buf->freePrev = INVALID_DESCRIPTOR;
+
+    /* mark buffer as no longer free */
+    buf->flags &= ~BM_FREE;
+  } else {
+    NotInQueue(buf);
+  }
+
+  buf->refcount++;
+
+}
+
+/*
+ * UnpinBuffer -- make buffer available for replacement.
+ */
+UnpinBuffer(buf)
+    BufferDesc *buf;
+{
+  is_LOCKED(BufMgrLock);
+
+  Assert (buf->refcount);
+  buf->refcount--;
+  NotInQueue(buf);
+
+  if (buf->refcount == 0) {
+    AddBufferToFreelist(buf);
+    buf->flags |= BM_FREE;
+  } else {
+    /* do nothing */
+  }
+
+}
+
+
+/*
+ * GetFreeBuffer() -- get the 'next' buffer from the freelist.
+ *
+ */
+BufferDesc *
+GetFreeBuffer()
+{
+    BufferDesc *buf;
+
+    is_LOCKED(BufMgrLock);
+
+
+    if (FREE_LIST_DESCRIPTOR == SharedFreeList->freeNext) {
+
+      /* queue is empty. All buffers in the buffer pool are pinned. */
+      elog(DEBUG,"out of free buffers: time to abort !\n");
+      return(NULL);
+    }
+    buf = &(BufferDescriptors[SharedFreeList->freeNext]);
+
+    /* remove from freelist queue */
+    BufferDescriptors[buf->freeNext].freePrev = buf->freePrev;
+    BufferDescriptors[buf->freePrev].freeNext = buf->freeNext;
+    buf->freeNext = buf->freePrev = INVALID_DESCRIPTOR;
+
+    buf->flags &= ~(BM_FREE);
+
+    return(buf);
+}
+
+/*
+ * InitFreeList -- initialize the dummy buffer descriptor used
+ *   	as a freelist head.
+ *
+ * Assume: All of the buffers are already linked in a circular
+ *	queue.   Only called by postmaster and only during 
+ * 	initialization.
+ */
+InitFreeList(init)
+Boolean init;
+{
+  SharedFreeList = &(BufferDescriptors[FREE_LIST_DESCRIPTOR]);
+
+  if (init) {
+      /* we only do this once, normally the postmaster */
+      SharedFreeList->data = NULL;
+      SharedFreeList->flags = 0;
+      SharedFreeList->flags &= ~(BM_VALID | BM_DELETED | BM_FREE);
+      SharedFreeList->id = FREE_LIST_DESCRIPTOR;
+
+      /* insert it into a random spot in the circular queue */
+      SharedFreeList->freeNext = BufferDescriptors[0].freeNext;
+      SharedFreeList->freePrev = 0;
+      BufferDescriptors[SharedFreeList->freeNext].freePrev = 
+	BufferDescriptors[SharedFreeList->freePrev].freeNext = 
+	  FREE_LIST_DESCRIPTOR;
+    }
+}
+
+
+/*
+ * print out the free list and check for breaks.
+ */
+DBG_FreeListCheck(nfree)
+int nfree;
+{
+  int i;
+  BufferDesc *buf;
+
+  buf = &(BufferDescriptors[SharedFreeList->freeNext]);
+  for (i=0;i<nfree;i++,buf = &(BufferDescriptors[buf->freeNext])) {
+
+    if (! (buf->flags & (BM_FREE))){
+      if (buf != SharedFreeList) {
+	printf("\tfree list corrupted: %d flags %x\n",
+	       buf->id,buf->flags);
+      } else  {
+	printf("\tfree list corrupted: too short -- %d not %d\n",
+	       i,nfree);
+
+      }
+      
+
+    }
+    if ((BufferDescriptors[buf->freeNext].freePrev != buf->id) ||
+	(BufferDescriptors[buf->freePrev].freeNext != buf->id)) {
+      printf("\tfree list links corrupted: %d %d %d\n",
+	     buf->id,buf->freePrev,buf->freeNext);
+    }
+
+  }
+  if (buf != SharedFreeList) {
+    printf("\tfree list corrupted: %d-th buffer is %d\n",
+	   nfree,buf->id);
+	   
+  }
+}
+
