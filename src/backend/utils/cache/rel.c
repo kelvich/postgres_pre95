b439f57297e880847b8b8e7a976811ecea832133
cimarron <cimarron>
1991-02-06 19:21:36 +0000

    Initial revision

---
+++ src/backend/utils/cache/rel.c
@@ -0,0 +1,211 @@
+/*
+ * rel.c --
+ *	POSTGRES relation descriptor code.
+ */
+
+/* #define RELREFDEBUG	1 */
+
+#include "tmp/postgres.h"
+#include "tmp/miscadmin.h"
+
+RcsId("$Header$");
+
+#include "access/istrat.h"
+#include "access/tupdesc.h"
+
+#include "utils/rel.h"
+#include "storage/fd.h"
+
+/* ----------------
+ *	RelationIsValid
+ * ----------------
+ */
+bool
+RelationIsValid(relation)
+    Relation	relation;
+{
+    return (bool)
+	PointerIsValid(relation);
+}
+
+/* ----------------
+ *	RelationGetSystemPort
+ * ----------------
+ */
+File
+RelationGetSystemPort(relation)
+    Relation	relation;
+{
+    return (relation->rd_fd);
+}
+
+/* ----------------
+ *	RelationHasReferenceCountZero
+ * ----------------
+ */
+bool
+RelationHasReferenceCountZero(relation)
+    Relation	relation;
+{
+    return ((bool)(relation->rd_refcnt == 0));
+}
+
+/* ----------------
+ *	RelationSetReferenceCount
+ * ----------------
+ */
+void
+RelationSetReferenceCount(relation, count)
+    Relation	relation;
+    Count		count;
+{
+#ifdef	RELREFDEBUG
+    elog(-2, "Set(%.16s[%d->%d])", RelationGetRelationName(relation),
+	 relation->rd_refcnt, count);
+#endif
+
+    relation->rd_refcnt = count;
+}
+
+/* ----------------
+ *	RelationIncrementReferenceCount
+ * ----------------
+ */
+void
+RelationIncrementReferenceCount(relation)
+    Relation	relation;
+{
+#ifdef	RELREFDEBUG
+    elog(-2, "Increment(%.16s[%d->%d])", RelationGetRelationName(relation),
+	 relation->rd_refcnt, 1 + relation->rd_refcnt);
+#endif
+
+    relation->rd_refcnt += 1;
+}
+
+/* ----------------
+ *	RelationDecrementReferenceCount
+ * ----------------
+ */
+void
+RelationDecrementReferenceCount(relation)
+    Relation	relation;
+{
+#ifdef	RELREFDEBUG
+    elog(-2, "Decrement(%.16s[%d->%d])", RelationGetRelationName(relation),
+	 relation->rd_refcnt, -1 + relation->rd_refcnt);
+#endif
+    
+    relation->rd_refcnt -= 1;
+}
+
+/* ----------------
+ *	RelationGetAccessMethodTupleForm
+ * ----------------
+ */
+AccessMethodTupleForm
+RelationGetAccessMethodTupleForm(relation)
+    Relation	relation;
+{
+    return (relation->rd_am);
+}
+
+/* ----------------
+ *	RelationGetRelationTupleForm
+ * ----------------
+ */
+RelationTupleForm
+RelationGetRelationTupleForm(relation)
+    Relation	relation;
+{
+    return (relation->rd_rel);
+}
+
+
+/* ----------------
+ *	RelationGetTupleDescriptor
+ * ----------------
+ */
+TupleDescriptor
+RelationGetTupleDescriptor(relation)
+    Relation	relation;
+{
+    return (&relation->rd_att);
+}
+
+/* ----------------
+ *	RelationGetIndexStrategy
+ * ----------------
+ */
+IndexStrategy
+RelationGetIndexStrategy(relation)
+    Relation	relation;
+{
+    return (IndexStrategy)
+	relation->rd_att.data[ relation->rd_rel->relnatts];
+}
+
+/* ----------------
+ *	RelationSetIndexStrategy
+ * ----------------
+ */
+void
+RelationSetIndexStrategy(relation, strategy)
+    Relation	relation;
+    IndexStrategy	strategy;
+{
+    IndexStrategy	*relationIndexStrategyP;
+
+    Assert(PointerIsValid(relation));
+    Assert(IndexStrategyIsValid(strategy));
+
+    relationIndexStrategyP = (IndexStrategy *)
+	&relation->rd_att.data[relation->rd_rel->relnatts];
+
+    *relationIndexStrategyP = strategy;
+}
+
+/* ----------------
+ *	RelationGetRelationId
+ * ----------------
+ */
+ObjectId
+RelationGetRelationId(relation)
+    Relation	relation;
+{
+    return(relation->rd_id);
+}
+
+/* ----------------
+ *	RelationGetRelationName
+ * ----------------
+ */
+Name
+RelationGetRelationName(relation)
+    Relation	relation;
+{
+    return(&relation->rd_rel->relname);
+}
+
+/* ----------------
+ *	RelationGetFile
+ * ----------------
+ */
+File
+RelationGetFile(relation)
+    Relation	relation;
+{
+    return(relation->rd_fd);
+}
+
+/* ----------------
+ *	RelationGetNumberOfAttributes
+ * ----------------
+ */
+AttributeNumber
+RelationGetNumberOfAttributes(relation)
+    Relation	relation;
+{
+    return(relation->rd_rel->relnatts);
+}
+
