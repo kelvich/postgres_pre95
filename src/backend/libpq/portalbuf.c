0cd7e8464d399e57123f9a5532085b984c1664bb
cimarron <cimarron>
1991-02-20 00:38:36 +0000

    made all portal buffer support routines begin with pbuf_

--- src/backend/libpq/portalbuf.c
+++ src/backend/libpq/portalbuf.c
@@ -6,25 +6,35 @@
  *	portal buffer support routines for src/libpq/portal.c
  *
  *   INTERFACE ROUTINES
- *	pq_alloc 	- allocate memory for libpq routines
- *	pq_free 	- free memory for libpq routines
- *	addPortal 	- Allocate a new portal buffer
- *	addGroup 	- Add a new tuple group to the portal
- *	addTypes 	- Allocate n type blocks
- *	addTuples 	- Allocate a tuple block
- *	addTuple 	- Allocate a tuple of n fields (attributes)
- *	addValues 	- Allocate n bytes for a value
- *	addPortalEntry 	- Allocate a portal entry
- *	freePortalEntry - Free a portal entry in the portal table
- *	freeTypes 	- Free up the space used by a portal 
- *	freeTuples 	- free space used by tuple block
- *	freeGroup 	- free space used by group, types and tuples
- *	freePortal 	- free space used by portal and portal's group
- *	
+ *	pbuf_alloc 	  - allocate memory for libpq routines
+ *	pbuf_free 	  - free memory for libpq routines
+ *	pbuf_addPortal 	  - Allocate a new portal buffer
+ *	pbuf_addGroup 	  - Add a new tuple group to the portal
+ *	pbuf_addTypes 	  - Allocate n type blocks
+ *	pbuf_addTuples 	  - Allocate a tuple block
+ *	pbuf_addTuple 	  - Allocate a tuple of n fields (attributes)
+ *	pbuf_addValues 	  - Allocate n bytes for a value
+ *	pbuf_addEntry 	  - Allocate a portal entry
+ *	pbuf_freeEntry    - Free a portal entry in the portal table
+ *	pbuf_freeTypes 	  - Free up the space used by a portal 
+ *	pbuf_freeTuples   - free space used by tuple block
+ *	pbuf_freeGroup 	  - free space used by group, types and tuples
+ *	pbuf_freePortal   - free space used by portal and portal's group
+ *	pbuf_getIndex 	  - Return the index of the portal entry
+ *	pbuf_setup 	  - Set up a portal for dumping data
+ *	pbuf_close 	  - Close a portal, remove it from the portal table
+ *	pbuf_findGroup 	  - Return group given the group_index
+ *	pbuf_findFnumber  - Return field index of a given field within a group
+ *	pbuf_findFname 	  - Find the field name given the field index
+ *	pbuf_checkFnumber - signal an error if field number is out of bounds
+ *
  *   NOTES
  *	These functions may be used by both frontend routines which
  *	communicate with a backend or by user-defined functions which
  *	are compiled or dynamically loaded into a backend.
+ *
+ *	the portals[] array should be organized as a hash table for
+ *	quick portal-by-name lookup.
  *
  *   IDENTIFICATION
  *	$Header$
@@ -43,17 +53,16 @@ RcsId ("$Header$");
 PortalEntry *portals[MAXPORTALS];
 
 /* --------------------------------
- *	pq_alloc - allocate memory for libpq routines
+ *	pbuf_alloc - allocate memory for portal buffers
  *
  *	remember: palloc() in the backend uses the postgres MemoryContext
- *	library and palloc() in the frontend (pqstubs.c) just calls malloc().
+ *	library and palloc() in the frontend (fe-pqstubs.c) calls malloc().
  * --------------------------------
  */
 caddr_t
-pq_alloc(size)
+pbuf_alloc(size)
     size_t size;
 {
-    extern	caddr_t	malloc();
     caddr_t 	addr;
 
     if (size <= 0)
@@ -67,30 +76,30 @@ pq_alloc(size)
 }
 
 /* --------------------------------
- *	pq_free - free memory for libpq routines
+ *	pbuf_free - free memory for portal buffers
  *
  *	remember: pfree() in the backend uses the postgres MemoryContext
- *	library and pfree() in the frontend (pqstubs.c) just calls free().
+ *	library and pfree() in the frontend (fe-pqstubs.c) calls free().
  * --------------------------------
  */
 void
-pq_free(pointer)
+pbuf_free(pointer)
     caddr_t pointer;
 {
     pfree(pointer);
 }
 
 /* --------------------------------
- *	addPortal - Allocate a new portal buffer
+ *	pbuf_addPortal - Allocate a new portal buffer
  * --------------------------------
  */
 PortalBuffer *
-addPortal()
+pbuf_addPortal()
 {
     PortalBuffer *portal;
     
     portal = (PortalBuffer *)
-	pq_alloc(sizeof (PortalBuffer));
+	pbuf_alloc(sizeof (PortalBuffer));
     
     portal->rule_p = 0;
     portal->no_tuples = 0;
@@ -101,17 +110,17 @@ addPortal()
 }
 
 /* --------------------------------
- *	addGroup - Add a new tuple group to the portal
+ *	pbuf_addGroup - Add a new tuple group to the portal
  * --------------------------------
  */
 GroupBuffer *
-addGroup(portal)
+pbuf_addGroup(portal)
     PortalBuffer *portal;
 {
     GroupBuffer *group, *group1;
 
     group = (GroupBuffer *)
-	pq_alloc(sizeof (GroupBuffer));
+	pbuf_alloc(sizeof (GroupBuffer));
     
     /* Initialize the new group buffer. */
     group->no_tuples  = 0;
@@ -132,33 +141,33 @@ addGroup(portal)
 }
 
 /* --------------------------------
- *	addTypes - Allocate n type blocks
+ *	pbuf_addTypes - Allocate n type blocks
  * --------------------------------
  */
 TypeBlock *
-addTypes(n)
+pbuf_addTypes(n)
     int n;
 {
     TypeBlock *types;
 
     types = (TypeBlock *)
-	pq_alloc(n * sizeof (TypeBlock));
+	pbuf_alloc(n * sizeof (TypeBlock));
 
     return (types);
 }
 
 /* --------------------------------
- *	addTuples - Allocate a tuple block
+ *	pbuf_addTuples - Allocate a tuple block
  * --------------------------------
  */
 /*  */
 TupleBlock *
-addTuples()
+pbuf_addTuples()
 {
     TupleBlock *tuples;
 
     tuples = (TupleBlock *)
-	pq_alloc(sizeof (TupleBlock));
+	pbuf_alloc(sizeof (TupleBlock));
     
     tuples->next = NULL;
 
@@ -166,71 +175,71 @@ addTuples()
 }
 
 /* --------------------------------
- *	addTuple - Allocate a tuple of n fields (attributes)
+ *	pbuf_addTuple - Allocate a tuple of n fields (attributes)
  * --------------------------------
  */
 char **
-addTuple(n)
+pbuf_addTuple(n)
 {
     return (char **)
-	pq_alloc(n * sizeof (char *));
+	pbuf_alloc(n * sizeof (char *));
 }
 
 /* --------------------------------
- *	addValues - Allocate n bytes for a value
+ *	pbuf_addValues - Allocate n bytes for a value
  * --------------------------------
  */
 char *
-addValues(n)
+pbuf_addValues(n)
     int n;
 {
     return
-	pq_alloc(n);
+	pbuf_alloc(n);
 }
 
 /* --------------------------------
- *	addPortalEntry - Allocate a portal entry
+ *	pbuf_addEntry - Allocate a portal entry
  * --------------------------------
  */
 PortalEntry *
-addPortalEntry()
+pbuf_addEntry()
 {
     return (PortalEntry *)
-	pq_alloc (sizeof (PortalEntry));
+	pbuf_alloc (sizeof (PortalEntry));
 }
 
 /* --------------------------------
- *	freePortalEntry - Free a portal entry in the portal table
+ *	pbuf_freeEntry - Free a portal entry in the portal table
  *	the portal is freed separately.
  * --------------------------------
  */
 void
-freePortalEntry(i)
+pbuf_freeEntry(i)
     int i;
 {
-    pq_free (portals[i]);
+    pbuf_free (portals[i]);
     portals[i] = NULL;
 }
 
 
 /* --------------------------------
- *	freeTypes - Free up the space used by a portal 
+ *	pbuf_freeTypes - Free up the space used by a portal 
  * --------------------------------
  */
 
 void 
-freeTypes(types)
+pbuf_freeTypes(types)
     TypeBlock *types;
 {
-    pq_free(types);
+    pbuf_free(types);
 }
 
 /* --------------------------------
- *	freeTuples - free space used by tuple block
+ *	pbuf_freeTuples - free space used by tuple block
  * --------------------------------
  */
 void
-freeTuples(tuples, no_tuples, no_fields)
+pbuf_freeTuples(tuples, no_tuples, no_fields)
     TupleBlock *tuples;
     int		no_tuples;
     int		no_fields;
@@ -238,7 +247,7 @@ freeTuples(tuples, no_tuples, no_fields)
     int i, j;
     
     if (no_tuples > TupleBlockSize) {
-	freeTuples (tuples->next, no_tuples - TupleBlockSize, no_fields);
+	pbuf_freeTuples (tuples->next, no_tuples - TupleBlockSize, no_fields);
 	no_tuples = TupleBlockSize;
     }
     
@@ -246,43 +255,184 @@ freeTuples(tuples, no_tuples, no_fields)
     for (i = 0; i < no_tuples; i++) {
 	for (j = 0; j < no_fields; j++)
 	    if (tuples->values[i][j] != NULL)
-		pq_free(tuples->values[i][j]);
-	pq_free(tuples->values[i]);
+		pbuf_free(tuples->values[i][j]);
+	pbuf_free(tuples->values[i]);
     }
     
-    pq_free(tuples);
+    pbuf_free(tuples);
 }
 
 /* --------------------------------
- *	freeGroup - free space used by group, types and tuples
+ *	pbuf_freeGroup - free space used by group, types and tuples
  * --------------------------------
  */
 void
-freeGroup(group)
+pbuf_freeGroup(group)
     GroupBuffer *group;
 {
     if (group->next != NULL)
-	freeGroup(group->next);
+	pbuf_freeGroup(group->next);
 
     if (group->types != NULL)
-	freeTypes(group->types);
+	pbuf_freeTypes(group->types);
 
     if (group->tuples != NULL)
-	freeTuples(group->tuples, group->no_tuples,group->no_fields);
+	pbuf_freeTuples(group->tuples, group->no_tuples,group->no_fields);
 
-    pq_free(group);
+    pbuf_free(group);
 }
 
 /* --------------------------------
- *	freePortal - free space used by portal and portal's group
+ *	pbuf_freePortal - free space used by portal and portal's group
  * --------------------------------
  */
 void
-freePortal(portal)
+pbuf_freePortal(portal)
     PortalBuffer *portal;
 {
     if (portal->groups != NULL)
-	freeGroup(portal->groups);
+	pbuf_freeGroup(portal->groups);
     
-    pq_free(portal);
+    pbuf_free(portal);
 }
+
+/* --------------------------------
+ *	pbuf_getIndex - Return the index of the portal entry
+ * 	note: portals[] maps portal names to portal buffers.
+ * --------------------------------
+ */
+int 
+pbuf_getIndex(pname)
+    char *pname;
+{
+    int i;
+
+    for (i = 0; i < MAXPORTALS; i++) 
+	if (portals[i] != NULL && strcmp(portals[i]->name, pname) == 0)
+	    return i;
+    
+    return (-1);
+}
+
+/* --------------------------------
+ *	pbuf_setup - Set up a portal for dumping data
+ * --------------------------------
+ */
+PortalEntry *
+pbuf_setup(pname)
+    char *pname;
+{
+    int i;
+
+    /* If a portal with the same name already exists, close it. */
+    if ((i = pbuf_getIndex(pname)) != -1) 
+	pbuf_freePortal(portals[i]->portal);
+    /* Look for an empty entry in the portal table. */
+    else {
+	for (i = 0; i < MAXPORTALS; i++)
+	    if (portals[i] == NULL)
+		break;
+	/* If the portal table is full, signal an error. */
+	if (i >= MAXPORTALS) 
+	    libpq_raise(PortalError, form("Portal Table overflows!"));
+	
+	portals[i] = pbuf_addEntry();
+	strcpy(portals[i]->name, pname);
+    }
+    portals[i]->portal = pbuf_addPortal();
+
+    return (PortalEntry *)
+	portals[i];
+}
+
+/* --------------------------------
+ *	pbuf_close - Close a portal, remove it from the portal table
+ *			and free up the space
+ * --------------------------------
+ */
+void
+pbuf_close(pname)
+    char *pname;
+{
+    int i;
+
+    if ((i = pbuf_getIndex(pname)) == -1) 
+	libpq_raise(PortalError, form("Portal %s does not exist.", pname));
+
+    pbuf_freePortal(portals[i]->portal);
+    pbuf_freeEntry(i);
+}
+
+/* --------------------------------
+ *	pbuf_findGroup - Return the group given the group_index
+ * --------------------------------
+ */
+GroupBuffer *
+pbuf_findGroup(portal, group_index)
+    PortalBuffer *portal;
+    int 	 group_index;
+{
+    GroupBuffer *group;
+
+    group = portal->groups;
+    while (group_index > 0 && group != NULL) {
+	group = group->next;
+	group_index--;
+    }
+
+    if (group == NULL)
+	libpq_raise(PortalError, 
+		    form("Group index %d out of bound.", group_index));
+
+    return (group);
+}
+
+/* --------------------------------
+ *	pbuf_findFnumber - Return the field index of a given field within a group
+ * --------------------------------
+ */
+pbuf_findFnumber(group, field_name)
+    GroupBuffer *group;
+    char	*field_name;
+{	
+    TypeBlock *types;
+    int i;
+
+    types = group->types;
+
+    for (i = 0; i < group->no_fields; i++, types++) 
+	if (strcmp(types->name, field_name) == 0)
+	    return (i);
+	
+    libpq_raise(PortalError, 
+		form("Field-name %s does not exist.", field_name));
+}
+
+/* --------------------------------
+ *	pbuf_checkFnumber - signal an error if field number is out of bounds
+ * --------------------------------
+ */
+void
+pbuf_checkFnumber(group, field_number)
+    GroupBuffer *group;
+    int		 field_number;
+{
+    if (field_number < 0 || field_number >= group->no_fields)
+	libpq_raise(PortalError, 
+		    form("Field number %d out of bound.", field_number));
+}
+
+/* --------------------------------
+ *	pbuf_findFname - Find the field name given the field index
+ * --------------------------------
+ */
+char *
+pbuf_findFname(group, field_number)
+    GroupBuffer *group;
+    int		field_number;
+{
+     pbuf_checkFnumber(group, field_number);
+     return
+	 (group->types + field_number)->name;
+}
+
