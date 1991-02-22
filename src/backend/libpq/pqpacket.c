a8f2acdf1fd6b0953f49601615614228057f8632
mer <mer>
1991-02-22 17:09:48 +0000

    Initial revision

---
+++ src/backend/libpq/pqpacket.c
@@ -0,0 +1,276 @@
+/*
+ * pqpacket.c -- routines for reading and writing data packets
+ *	sent/received by POSTGRES clients and servers
+ *
+ * This is the module that understands the lowest-level part
+ * of the communication protocol.  All of the trickyness in
+ * this module is for making sure that non-blocking IO in
+ * the Postmaster works correctly.   Check the notes in PacketRecv
+ * on non-blocking I/O.
+ *
+ * Data Structures:
+ *	Port has two important functions. (1) It records the 
+ *	sock/addr used in communication. (2) It holds partially
+ *	read in messages.  This is especially important when
+ *	we haven't seen enough to construct a complete packet 
+ *	header.
+ *
+ * PacketBuf -- None of the clients of this module should know
+ *	what goes into a packet hdr (although they know how big
+ *	it is).  This routine is in charge of host to net order
+ *	conversion for headers.  Data conversion is someone elses
+ *	responsibility.
+ *
+ * Memory Allocation:  is not handled in this module
+ *
+ * IMPORTANT: these routines are called by backends, clients, and
+ *	the Postmaster.
+ *
+ * NOTE: assume htons() is sufficient for all header structs.  If
+ *	header structs become longs, must switch to htonl().
+ */
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+
+#include "tmp/postgres.h"
+#include "utils/log.h"
+#include "storage/ipci.h"
+#include "tmp/pqcomm.h"
+
+
+/*
+ * PacketRecieve -- receive a packet on a port.
+ *
+ * RETURNS: connection id of the packet sender, if one
+ * is available.
+ */
+PacketReceive(port, buf, nonBlocking, connIdP)
+Port		*port;	/* receive port */
+PacketBuf	*buf;	/* MAX_PACKET_SIZE-worth of buffer space */
+Boolean		nonBlocking; /* NON_BLOCKING or BLOCKING i/o */
+ConnId		*connIdP;    /* sender's connection (seqpack) */
+{
+  int		status;
+  PacketLen	cc;	    	/* character count -- bytes recvd */
+  PacketLen	packetLen; 	/* remaining packet chars to read */
+  Addr	tmp;	   	/* curr recv buf pointer */
+  PacketHdr	*hdr = (PacketHdr *) buf;  /* incoming hdr information */
+  int		addrLen = sizeof(struct sockaddr_in);
+  int		flag;
+  int		decr;
+
+  if (nonBlocking) {
+    flag = MSG_PEEK;
+    decr = 0;
+  } else {
+    flag = 0;
+    decr = sizeof(PacketHdr);
+  }
+  /*
+   * Assume port->nBytes is zero unless we were interrupted during
+   * non-blocking I/O.  This first recvfrom() is to get the hdr
+   * information so we know how many bytes to read.  Life would
+   * be very complicated if we read too much data (buffering).
+   */
+  tmp = ((Addr)buf) + port->nBytes;
+  if (port->nBytes >= sizeof(PacketHdr)) {
+    packetLen = htons(hdr->len) + sizeof(PacketHdr);
+  } else {
+    cc = recvfrom(port->sock, tmp, sizeof(PacketHdr), flag, 
+		  &(port->addr), &addrLen);
+    if ( cc < sizeof(PacketHdr)) {
+      /* if cc is negative, the system call failed */
+      if (cc < 0) {
+	return(STATUS_ERROR);
+      }
+      /* 
+       * cc == 0 means the connection was broken at the 
+       * other end.
+       */
+      else if (! cc) {
+	return(STATUS_INVALID);
+
+      } else {
+	/*
+	 * Worst case.  We didn't even read in enough data to
+	 * get the header.  IN UDP (seq-pack) THIS IS IMPOSSIBLE.
+	 * Messages are delivered in full packets.  In the stream
+	 * protocol, it probably won't happen unless the client
+	 * is malicious (so its even harder to test).
+	 *
+	 * Don't save the number of bytes we've read so far.
+	 * Since we only peeked at the incoming message, the
+	 * kernel is going to keep it for us.
+	 */
+
+	*connIdP = INVALID_ID;
+	return(STATUS_NOT_DONE);
+      }
+    } else {
+
+      /* great. got the header. now get the true length (including
+       * header size).
+       */
+      packetLen = htons(hdr->len);
+      packetLen -= decr;
+      tmp += decr - port->nBytes;
+    }
+  }
+
+  printf(" reading packet of size %d\n",packetLen);
+  /*
+   * Now that we know how big it is, read the packet.  We read
+   * the entire packet, since the last call was just a peek.
+   */
+  while (packetLen) { 
+    cc = recvfrom(port->sock, tmp, packetLen, NULL, 
+		  &(port->addr), &addrLen);
+    if (cc < 0)
+      return(STATUS_ERROR);
+
+    /* 
+     * cc == 0 means the connection was broken at the 
+     * other end.
+     */
+    else if (! cc) 
+      return(STATUS_INVALID);
+
+    tmp += cc;
+    packetLen -= cc;
+
+
+    /* if non-blocking, we're done. */
+    if (nonBlocking && packetLen) {
+      port->nBytes += cc;
+      return(STATUS_NOT_DONE);
+    }
+  }
+
+  *connIdP = htons(hdr->connId);
+  port->nBytes = 0;
+  return(STATUS_OK);
+}
+
+
+/*
+ * PacketSend -- send a single-packet message.
+ *
+ * RETURNS: STATUS_ERROR if the write fails, STATUS_OK otherwise.
+ * SIDE_EFFECTS: may block.
+ * NOTES: Non-blocking writes would significantly complicate 
+ *	buffer management.  For now, we're not going to do it.
+ */
+PacketSend(port, conn, buf, type, len, nonBlocking)
+Port		*port;
+Connection	*conn;
+PacketBuf	*buf;
+MsgType		type;
+PacketLen	len;
+Boolean		nonBlocking;
+{
+  int			status;
+  PacketLen		totalLen;
+  PacketHdr		*hdr = (PacketHdr *) buf;
+  int			addrLen = sizeof (struct sockaddr_in);
+
+  Assert( ! nonBlocking );
+  Assert( buf );
+
+  totalLen = len;
+
+  hdr->seqno = htons(conn->seqno++);
+  hdr->connId = htons(conn->id);
+  hdr->type = (MsgType)htons(type);
+  hdr->len = htons(len);
+
+  len = sendto(port->sock, (Addr) buf, totalLen, /* flags */ 0,
+	       (struct sockaddr *)port->addr, addrLen);
+
+  if (len < totalLen) {
+    fprintf(stderr,"PacketSend: couldn't send complete packet\n");
+    return(STATUS_ERROR);
+  }
+
+  return(STATUS_OK);
+}
+
+/*
+ * PacketData -- return components of msg hdr.
+ *
+ * Clients of this module don't know what the headers look
+ * like.  
+ *
+ * NOTE: seqnoP is an IN/OUT parameter.  The others are out only.
+ *
+ */
+PacketData(buf, dataP, bufSizeP, msgTypeP, seqnoP)
+Addr		buf;
+Addr 	*dataP;
+int  		*bufSizeP;
+MsgType		*msgTypeP;
+SeqNo		*seqnoP;
+{
+  PacketHdr	*hdr = (PacketHdr *) buf;
+
+  /* seq pack protocol only */
+  if (htons(hdr->seqno) != *seqnoP)
+    *msgTypeP = DUPLICATE_MSG;
+  else
+    *seqnoP++;
+
+  *msgTypeP = (MsgType)htons(hdr->type);
+  *bufSizeP = htons(hdr->len);
+
+  /* data part starts right after the header */
+  *dataP = (Addr) (hdr+1);
+}
+
+
+/*
+ * PacketRetransmit -- resend a buffer.  
+ *
+ * Assume that the header is already initialized.  We're
+ * just going to resend the same message.
+ */
+PacketRetransmit(buf,port,addr)
+Addr		buf;
+Port		*port;
+Addr		addr;
+{
+  PacketHdr 	*hdr = (PacketHdr *) buf;
+  PacketLen	len,totalLen;
+  int		addrLen = sizeof (struct sockaddr_in);
+
+  len = totalLen = htons(hdr->len);
+  len = sendto(port->sock, (Addr) buf, totalLen, /* flags */ 0,
+	       (struct sockaddr *)addr, addrLen);
+
+  if (len < totalLen) {
+    fprintf(stderr,"PacketWrite: couldn't send complete packet\n");
+    return(STATUS_ERROR);
+  }
+
+  return(STATUS_OK);
+}
+
+
+/*
+ * PacketBuf -- remove the header from a packet buf for
+ * 	a client.
+ *
+ * NOTE: not sure this is necessary.  Some clients are going
+ *	to have to know how big the thing is in order to do
+ *	memory allocation.
+ */
+PacketBufInit(bufP, bufSizeP)
+Addr 		*bufP;
+PacketLen  	*bufSizeP;
+{
+  *bufP  += sizeof(PacketHdr);
+  *bufSizeP -= sizeof(PacketHdr);
+}
+
---
+++ src/backend/tmp/pqcomm.h
@@ -0,0 +1,120 @@
+/*
+ * comm.h -- parameters for the communication module
+ *
+ * Some of this should move to pqlib.h
+ */
+
+#include "catalog/pg_user.h"
+/*
+ * startup msg paramters: path length, argument string length
+ */
+#define PATH_SIZE	64
+#define ARGV_SIZE	64
+
+/*
+ * For sequenced packet, this is fixed to 2K by UDP protocol.
+ * For the stream implementation, it depends only on available
+ *	buffering.
+ */
+#define MAX_PACKET_SIZE	(2*1024)
+
+typedef enum MsgType {
+  ACK_MSG = 0,		/* acknowledge a message */
+  ERROR_MSG=1,		/* error response to client from server */
+  RESET_MSG=2,		/* client must reset connection */
+  PRINT_MSG=3,		/* tuples for client from server */
+  NET_ERROR=4,		/* error in net system call */
+  FUNCTION_MSG=5,	/* fastpath call (unused) */
+  QUERY_MSG=6,		/* client query to server */
+
+  STARTUP_MSG=7,	/* initialize a connection with a backend */
+  DUPLICATE_MSG=8,/* duplicate message arrived (errors msg only) */
+  INVALID_MSG=9,	/* for some control functions */
+} MsgType;
+
+typedef char *Addr;
+typedef int SeqNo;	/* (seq pack protocol) sequence numbers */
+typedef int ConnId;	/* (seq pack protocol) connection ids */
+typedef int PacketLen;	/* packet length */
+
+typedef struct PacketHdr {
+  SeqNo		seqno;
+  PacketLen	len;
+  ConnId	connId;
+  MsgType	type;
+} PacketHdr;
+
+typedef struct StartupPacket {
+  PacketHdr	hdr;
+  char		database[PATH_SIZE];	/* database name */
+  char		user[USER_NAMESIZE];	/* user name */
+  char		options[ARGV_SIZE];	/* possible additional args */
+  char		execFile[ARGV_SIZE];	/*  possible backend to use */
+} StartupPacket;
+
+/* amount of available data in a packet buffer */
+#define MESSAGE_SIZE	(MAX_PACKET_SIZE - sizeof(PacketHdr))
+
+typedef struct MsgPacket {
+  PacketHdr	hdr;
+  char		message[MESSAGE_SIZE];
+} MsgPacket;
+
+/* I/O can be blocking or non-blocking */
+#define BLOCKING 	(FALSE)
+#define NON_BLOCKING	(TRUE)
+
+/* buffers for incoming packets */
+typedef int PacketBufId;
+
+/*
+ * Watch out if PacketBufId changes size.  Some compilers
+ * will allocate padding.  PacketBuf should be exactly
+ * MESSAGE_SIZE big.  I have an assert statement in InitComm
+ */
+typedef struct PacketBufHdr {
+  PacketBufId	id,prev,next;
+} PacketBufHdr;
+
+typedef struct PacketBuf {
+  PacketBufId	id,prev,next;
+  char 		data[MESSAGE_SIZE - sizeof(PacketBufHdr)];
+} PacketBuf;
+
+
+/*
+ * socket descriptor port 
+ */
+typedef struct Port {
+  int			sock;	/* file descriptor */
+  int			mask;	/* select mask */
+  int   		nBytes;	/* nBytes read in so far */
+  struct sockaddr_in	addr;	/* addr of sender */
+  PacketBufId		id;	/* id of packet buf currently in use */
+  PacketBuf		buf;	/* stream implementation (curr pack buf) */
+} Port;
+
+/*
+ * Connection to client
+ */
+typedef struct Connection {
+  struct sockaddr_in	addr;	/* addr of client */
+  SeqNo			seqno;	/* next expected sequence number */
+  ConnId		id,prev,next;      /* connection queue links. */
+  PacketBufId		packSend,packRecv; /* send/receive packet bufs */
+  long			lastUse;/* last recieve time for Gbg Collection */
+  int			uid;	/* unique id (generation number) */
+} Connection;
+
+/* invalid socket descriptor */
+#define INVALID_SOCK	(-1)
+
+#define INVALID_ID (-1)
+#define MAX_CONNECTIONS	10
+#define N_PACK_BUFS	20
+
+/* no multi-packet messages yet */
+#define MAX_PACKET_BACKLOG	1
+
+#define INITIAL_SEQNO		1
+#define	DEFAULT_STRING		""
