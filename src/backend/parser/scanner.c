8573da483a665e4aa63d1c8648e750205b88efdf
kemnitz <kemnitz>
1991-04-04 18:26:50 +0000

    handles special case of arrays now - array_in has to do some of the parsing so it can handle {"string","bad\"string"}

--- src/backend/parser/scanner.c
+++ src/backend/parser/scanner.c
@@ -63,7 +63,7 @@ char *buf;
 int len;
 {
 	delimiter = '\}';
-	scancon(buf, len);
+	scanarr(buf, len);
 }
 
 /*
@@ -71,6 +71,7 @@ int len;
  * read.  Be sure to gobble up the whole thing, including the
  * trailing delimiter.
  */
+
 scancon(buf, len)
 char *buf;
 int len;
@@ -129,3 +130,67 @@ out:
 	return(cspec);
 }
 
+/*
+ * Scan input for array_in.  The leading delimiter ({) has already been
+ * read.  Be sure to gobble up the whole thing, including the
+ * trailing delimiter.
+ */
+
+scanarr(buf, len)
+char *buf;
+int len;
+{
+	register char *cp = buf;
+	register int c, c2, dc, cspec;
+
+	cspec = 0;
+
+	while ((c = input()) != delimiter) {
+		if (cp - buf > len - 1) {
+			serror("String/char constant too large");
+			cp = buf;
+		}
+		switch (c) {
+		default:
+			*cp++ = c;
+			break;
+
+		case 0:
+		case '\n':
+			serror("Unterminated char/string constant");
+			goto out;
+
+		case '\\':
+			c = input();
+			if (c == '\n')
+				continue;
+			/* *cp++ = '\\'; When _should_ this be done? XXX */
+			if (isdigit(c)) {
+				dc = 0;
+				while (dc++ < 3 && isdigit(c2)) {
+					*cp++ = c;
+					c = input();
+				}
+				unput(c);
+				break;
+			}
+			if (c != 0) {
+				switch (c) {
+				case 't': c = '\t'; break;
+				case 'n': c = '\n'; break;
+				case 'r': c = '\r'; break;
+				case 'b': c = '\b'; break;
+				case 'f': c = '\f'; break;
+				default: *cp++ = '\\'; break;
+				}
+				*cp++ = c;
+			}
+			break;
+		}
+		cspec++;
+	}
+
+out:
+	*cp = 0;
+	return(cspec);
+}
