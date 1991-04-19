a91c1fb75d0e92aef2fe061e29853c49104a4f5c
kemnitz <kemnitz>
1991-04-19 05:36:38 +0000

    added a boolean to make it not use the syscaches if they don't exist.

--- src/backend/lib/fcache.c
+++ src/backend/lib/fcache.c
@@ -22,78 +22,86 @@
  *
  * Initialize the 'FunctionCache' given the PG_PROC oid.
  *
+ *
+ * NOTE:  This function can be called when the system cache is being
+ *        initialized.  Therefore, use_syscache should ONLY be true
+ *        when the function return type is interesting (ie: set_fcache).
  *-----------------------------------------------------------------
  */
+
 FunctionCachePtr
-init_fcache(foid)
+init_fcache(foid, use_syscache)
 ObjectId foid;
+Boolean use_syscache;
 {
-	HeapTuple	procedureTuple;
-	HeapTuple	typeTuple;
-	struct proc	*procedureStruct;
-	Form_pg_type	typeStruct;
-	FunctionCachePtr retval;
-	
-	/* ----------------
-	 *   get the procedure tuple corresponding to the given
-	 *   functionOid.  If this fails, returnValue has been
-	 *   pre-initialized to "null" so we just return it.
-	 * ----------------
-	 */
-	procedureTuple = SearchSysCacheTuple(PROOID,
-					     (char *) foid,
-					     NULL,
-					     NULL,
-					     NULL);
+    HeapTuple    procedureTuple;
+    HeapTuple    typeTuple;
+    struct proc    *procedureStruct;
+    Form_pg_type    typeStruct;
+    FunctionCachePtr retval;
     
-	if (!HeapTupleIsValid(procedureTuple)) {
-	    elog(WARN, "init_fcache: %s %d",
-		 "Cache lookup failed for procedure",
-		 foid);
-	}
+    /* ----------------
+     *   get the procedure tuple corresponding to the given
+     *   functionOid.  If this fails, returnValue has been
+     *   pre-initialized to "null" so we just return it.
+     * ----------------
+     */
+    retval = (FunctionCachePtr) palloc(sizeof(FunctionCache));
 
-	/* ----------------
-	 *   get the return type from the procedure tuple
-	 * ----------------
-	 */
-	procedureStruct = (struct proc *) GETSTRUCT(procedureTuple);
-	
-	/* ----------------
-	 *   get the type tuple corresponding to the return type
-	 *   If this fails, returnValue has been pre-initialized
-	 *   to "null" so we just return it.
-	 * ----------------
-	 */
-	typeTuple = SearchSysCacheTuple(TYPOID,
-					(procedureStruct)->prorettype,
-					NULL,
-					NULL,
-					NULL);
+    if (use_syscache)
+    {
+        procedureTuple = SearchSysCacheTuple(PROOID, (char *) foid,
+                                             NULL, NULL, NULL);
 
-	if (!HeapTupleIsValid(typeTuple)) {
-	    elog(WARN, "init_fcache: %s %d",
-		 "Cache lookup failed for type",
-		 (procedureStruct)->prorettype);
-	}
-	
-	/* ----------------
-	 *   get the type length and by-value from the type tuple and
-	 *   save the information in our one element cache.
-	 * ----------------
-	 */
-	typeStruct = (Form_pg_type) GETSTRUCT(typeTuple);
+        if (!HeapTupleIsValid(procedureTuple)) {
+            elog(WARN, "init_fcache: %s %d",
+                 "Cache lookup failed for procedure", foid);
+        }
 
-    /*
-	 * This must persist throughout the duration of the query.
-	 */
+        /* ----------------
+         *   get the return type from the procedure tuple
+         * ----------------
+         */
+        procedureStruct = (struct proc *) GETSTRUCT(procedureTuple);
+    
+        /* ----------------
+         *   get the type tuple corresponding to the return type
+         *   If this fails, returnValue has been pre-initialized
+         *   to "null" so we just return it.
+         * ----------------
+         */
+        typeTuple = SearchSysCacheTuple(TYPOID,
+                        (procedureStruct)->prorettype,
+                        NULL,
+                        NULL,
+                        NULL);
 
-	retval = (FunctionCachePtr) palloc(sizeof(FunctionCache));
+        if (!HeapTupleIsValid(typeTuple)) {
+            elog(WARN, "init_fcache: %s %d",
+             "Cache lookup failed for type",
+             (procedureStruct)->prorettype);
+        }
+    
+        /* ----------------
+         *   get the type length and by-value from the type tuple and
+         *   save the information in our one element cache.
+         * ----------------
+         */
+        typeStruct = (Form_pg_type) GETSTRUCT(typeTuple);
+        retval->typlen = (typeStruct)->typlen;
+        retval->typbyval = (typeStruct)->typbyval ? true : false ;
+    }
+    else
+    {
+        /* default to int4 */
 
-	fmgr_info(foid, &(retval->func), &(retval->nargs));
-	retval->typlen = (typeStruct)->typlen;
-	retval->typbyval = (typeStruct)->typbyval ? true : false ;
+        retval->typlen = sizeof(int4);
+        retval->typbyval = true;
+    }
 
-	return(retval);
+    fmgr_info(foid, &(retval->func), &(retval->nargs));
+
+    return(retval);
 }
 
 void
@@ -103,24 +111,24 @@ Node node;
 ObjectId foid;
 
 {
-	Func fnode;
-	Oper onode;
-	FunctionCachePtr fcache, init_fcache();
+    Func fnode;
+    Oper onode;
+    FunctionCachePtr fcache, init_fcache();
 
-	fcache = init_fcache(foid);
+    fcache = init_fcache(foid, true);
 
-	if (IsA(node,Oper))
-	{
-		onode = (Oper) node;
-		onode->op_fcache = fcache;
-	}
-	else if (IsA(node,Func))
-	{
-		fnode = (Func) node;
-		fnode->func_fcache = fcache;
-	}
-	else
-	{
-		elog(WARN, "init_fcache: node must be Oper or Func!");
-	}
+    if (IsA(node,Oper))
+    {
+        onode = (Oper) node;
+        onode->op_fcache = fcache;
+    }
+    else if (IsA(node,Func))
+    {
+        fnode = (Func) node;
+        fnode->func_fcache = fcache;
+    }
+    else
+    {
+        elog(WARN, "init_fcache: node must be Oper or Func!");
+    }
 }
