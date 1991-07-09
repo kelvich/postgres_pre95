10bfaf0111e11459c06e2e0b20d10030ef791a4b
mao <mao>
1991-07-09 00:12:09 +0000

    this version works; changing postgres over to use the switch

--- src/backend/storage/smgr/md.c
+++ src/backend/storage/smgr/md.c
@@ -4,19 +4,64 @@
  *	This code manages relations that reside on magnetic disk.
  */
 
+#include <sys/file.h>
+
 #include "tmp/c.h"
 #include "tmp/postgres.h"
 
-#include "port/machine.h"
+#include "machine.h"
+#include "storage/smgr.h"
+#include "storage/block.h"
+#include "storage/fd.h"
+#include "utils/rel.h"
+
+RcsId("$Header$");
+
+/*
+ *  Need to keep track of open file descriptors under the magnetic disk
+ *  storage manager.
+ */
+
+static int	Nfds = 100;	/* must be same as in storage/file/fd.c */
+static int	*Md_fdvec;
+
+#define MDFD_CLEAN	0
+#define MDFD_DIRTY	1
+
+/*
+ *  mdinit() -- Initialize private state for magnetic disk storage manager.
+ *
+ *	We keep a private table of all file descriptors.  Whenever we do
+ *	a write to one, we mark it dirty in our table.  Whenever we force
+ *	changes to disk, we mark the file descriptor clean.  At transaction
+ *	commit, we force changes to disk for all dirty file descriptors.
+ *	This routine allocates and initializes the table.
+ *
+ *	Returns SM_SUCCESS or SM_FAIL with errno set as appropriate.
+ */
 
 int
-mdcreate(path)
-    char *path;
+mdinit()
+{
+    if ((Md_fdvec = (int *) malloc(Nfds * sizeof(int))) == (int *) NULL)
+	return (SM_FAIL);
+
+    (void) bzero(Md_fdvec, Nfds * sizeof(int));
+
+    return (SM_SUCCESS);
+}
+
+int
+mdcreate(reln)
+    Relation reln;
 {
     int fd;
     int tmp;
+    char *path;
+    extern char *relpath();
 
-    fd = open(path, O_RDWR|O_CREAT|O_EXCL, 0666);
+    path = relpath(reln->rd_rel->relname);
+    fd = FileNameOpenFile(path, O_RDWR|O_CREAT|O_EXCL, 0666);
 
     /*
      *  If the file already exists and is empty, we pretend that the
@@ -26,15 +71,28 @@ mdcreate(path)
      */
 
     if (fd < 0) {
-	if (fd = open(path, O_RDWR, 0666) >= 0) {
+	if (fd = FileNameOpenFile(path, O_RDWR, 0666) >= 0) {
 	    if (read(fd, &tmp, sizeof(tmp)) != 0) {
-		close(fd);
+		FileClose(fd);
 		return (-1);
 	    }
 	}
     }
 
     return (fd);
+}
+
+/*
+ *  mdunlink() -- Unlink a relation.
+ */
+
+int
+mdunlink(reln)
+    Relation reln;
+{
+    FileUnlink(RelationGetFile(reln));
+
+    return (SM_SUCCESS);
 }
 
 /*
@@ -51,15 +109,322 @@ mdextend(reln, buffer)
 {
     long pos;
     File vfd;
-    BufferDesc bufhdr;
 
     vfd = RelationGetFile(reln);
 
-    if ((pos = lseek(vfd, 0L, L_XTND)) < 0)
+    if ((pos = FileSeek(vfd, 0L, L_XTND)) < 0)
 	return (SM_FAIL);
 
-    if (write(fd, buffer, BLCKSZ) != BLCKSZ)
+    if (FileWrite(vfd, buffer, BLCKSZ) != BLCKSZ)
 	return (SM_FAIL);
 
+    /* remember that we did a write, so we can sync at xact commit */
+    Md_fdvec[vfd] = MDFD_DIRTY;
+
+    return (SM_SUCCESS);
+}
+
+/*
+ *  mdopen() -- Open the specified relation.
+ *
+ *	The magnetic disk storage manager uses one file descriptor per open
+ *	relation.  This routine returns the open file descriptor.
+ */
+
+int
+mdopen(reln)
+    Relation reln;
+{
+    char *path;
+    int fd;
+    extern char *relpath();
+
+    path = relpath(reln->rd_rel->relname);
+
+    fd = FileNameOpenFile(path, O_RDWR, 0666);
+
+    return (fd);
+}
+
+/*
+ *  mdclose() -- Close the specified relation.
+ *
+ *	Returns SM_SUCCESS or SM_FAIL with errno set as appropriate.
+ */
+
+int
+mdclose(reln)
+    Relation reln;
+{
+    File vfd;
+    int status;
+
+    /* maybe it's already closed... */
+    if ((vfd = RelationGetFile(reln)) < 0)
+	return (SM_SUCCESS);
+
+    /*
+     *  Need to do it here.  We sync the file descriptor so that we don't
+     *  need to reopen it at transaction commit to force changes to disk.
+     */
+
+    FileSync(vfd);
+    FileClose(vfd);
+
+    /* mark this file descriptor as clean in our private table */
+    Md_fdvec[vfd] = MDFD_CLEAN;
+
+    return (SM_SUCCESS);
+}
+
+/*
+ *  mdread() -- Read the specified block from a relation.
+ *
+ *	Returns SM_SUCCESS or SM_FAIL.
+ */
+
+int
+mdread(reln, blocknum, buffer)
+    Relation reln;
+    BlockNumber blocknum;
+    char *buffer;
+{
+    int status;
+    File vfd;
+    long seekpos;
+    int nbytes;
+
+    if ((vfd = RelationGetFile(reln)) < 0) {
+	if ((vfd = mdopen(reln)) < 0)
+	    return (SM_FAIL);
+	reln->rd_fd = vfd;
+    }
+
+    seekpos = (long) (BLCKSZ * blocknum);
+    if (FileSeek(vfd, seekpos, L_SET) != seekpos) {
+	return (SM_FAIL);
+    }
+
+    status = SM_SUCCESS;
+    if ((nbytes = FileRead(vfd, buffer, BLCKSZ)) != BLCKSZ) {
+	if (nbytes == 0) {
+	    (void) bzero(buffer, BLCKSZ);
+	} else {
+	    status = SM_FAIL;
+	}
+    }
+
+    return (status);
+}
+
+/*
+ *  mdwrite() -- Write the supplied block at the appropriate location.
+ *
+ *	Returns SM_SUCCESS or SM_FAIL.
+ */
+
+int
+mdwrite(reln, blocknum, buffer)
+    Relation reln;
+    BlockNumber blocknum;
+    char *buffer;
+{
+    int status;
+    bool found;
+    File vfd;
+    long seekpos;
+
+    found = true;
+    if ((vfd = RelationGetFile(reln)) < 0) {
+	found = false;
+	if ((vfd = mdopen(reln)) < 0)
+	    return (SM_FAIL);
+    }
+
+    seekpos = (long) (BLCKSZ * blocknum);
+    if (FileSeek(vfd, seekpos, L_SET) != seekpos) {
+	if (!found)
+	    (void) FileClose(vfd);
+
+	return (SM_FAIL);
+    }
+
+    status = SM_SUCCESS;
+    if (FileWrite(vfd, buffer, BLCKSZ) != BLCKSZ)
+	status = SM_FAIL;
+
+    /*
+     *  If we opened this file descriptor especially to write this one block,
+     *  force the change to disk and mark the descriptor as clean.  If we
+     *  had an open descriptor for the file already, mark it as dirty so
+     *  we'll flush it at commit time.
+     */
+
+    if (!found) {
+	FileSync(vfd);
+	FileClose(vfd);
+	Md_fdvec[vfd] = MDFD_CLEAN;
+    } else {
+	Md_fdvec[vfd] = MDFD_DIRTY;
+    }
+
+    return (status);
+}
+
+/*
+ *  mdflush() -- Synchronously write a block to disk.
+ *
+ *	This is exactly like mdwrite(), but doesn't return until the file
+ *	system buffer cache has been flushed.
+ */
+
+int
+mdflush(reln, blocknum, buffer)
+    Relation reln;
+    BlockNumber blocknum;
+    char *buffer;
+{
+    int status;
+    bool found;
+    File vfd;
+    long seekpos;
+
+    found = true;
+    if ((vfd = RelationGetFile(reln)) < 0) {
+	found = false;
+	if ((vfd = mdopen(reln)) < 0)
+	    return (SM_FAIL);
+    }
+
+    seekpos = (long) (BLCKSZ * blocknum);
+    if (FileSeek(vfd, seekpos, L_SET) != seekpos) {
+	if (!found)
+	    (void) FileClose(vfd);
+
+	return (SM_FAIL);
+    }
+
+    status = SM_SUCCESS;
+
+    /* write and sync the block */
+    if (FileWrite(vfd, buffer, BLCKSZ) != BLCKSZ || FileSync(vfd) < 0)
+	status = SM_FAIL;
+
+    /*
+     *  By here, the block is written and changes have been forced to stable
+     *  storage.  Mark the descriptor as clean until the next write, so we
+     *  don't sync it again unnecessarily at transaction commit.
+     */
+
+    Md_fdvec[vfd] = MDFD_CLEAN;
+
+    if (!found)
+	FileClose(vfd);
+
+    return (status);
+}
+
+/*
+ *  mdblindwrt() -- Write a block to disk blind.
+ *
+ *	We have to be able to do this using only the name and OID of
+ *	the database and relation in which the block belongs.  This
+ *	is a synchronous write.
+ */
+
+int
+mdblindwrt(dbstr, relstr, dbid, relid, blkno, buffer)
+    char *dbstr;
+    char *relstr;
+    OID dbid;
+    OID relid;
+    BlockNumber blkno;
+    char *buffer;
+{
+    int fd;
+    long seekpos;
+    int status;
+    char path[64];
+
+    /* construct the path to the file and open it */
+    sprintf(path, "../%s/%s", (dbid == (OID) 0 ? ".." : dbstr), relstr);
+    if ((fd = open(path, O_RDWR, 0666)) < 0)
+	return (SM_FAIL);
+
+    /* seek to the right spot */
+    seekpos = (long) (BLCKSZ * blkno);
+    if (lseek(fd, seekpos, L_SET) != seekpos) {
+	(void) close(fd);
+	return (SM_FAIL);
+    }
+
+    status = SM_SUCCESS;
+
+    /* write and sync the block */
+    if (write(fd, buffer, BLCKSZ) != BLCKSZ || fsync(fd) < 0)
+	status = SM_FAIL;
+
+    if (close(fd) < 0)
+	status = SM_FAIL;
+
+    return (status);
+}
+
+/*
+ *  mdnblocks() -- Get the number of blocks stored in a relation.
+ *
+ *	Returns # of blocks or -1 on error.
+ */
+
+int
+mdnblocks(reln)
+    Relation reln;
+{
+    File vfd;
+
+    vfd = RelationGetFile(reln);
+
+    return (FileGetNumberOfBlocks(vfd));
+}
+
+/*
+ *  mdcommit() -- Commit a transaction.
+ *
+ *	All changes to magnetic disk relations must be forced to stable
+ *	storage.  This routine makes a pass over the private table of
+ *	file descriptors.  Any descriptors to which we have done writes,
+ *	but not synced, are synced here.
+ *
+ *	Returns SM_SUCCESS or SM_FAIL with errno set as appropriate.
+ */
+
+int
+mdcommit()
+{
+    int i;
+
+    for (i = 0; i < Nfds; i++) {
+	if (Md_fdvec[i] == MDFD_DIRTY) {
+	    if (FileSync(Md_fdvec[i]) < 0)
+		return (SM_FAIL);
+
+	    Md_fdvec[i] = MDFD_CLEAN;
+	}
+    }
+
+    return (SM_SUCCESS);
+}
+
+/*
+ *  mdabort() -- Abort a transaction.
+ *
+ *	Changes need not be forced to disk at transaction abort.  We mark
+ *	all file descriptors as clean here.  Always returns SM_SUCCESS.
+ */
+
+int
+mdabort()
+{
+    bzero(Md_fdvec, Nfds * sizeof(int));
     return (SM_SUCCESS);
 }
--- src/backend/storage/smgr/smgr.c
+++ src/backend/storage/smgr/smgr.c
@@ -8,182 +8,377 @@
 #include "tmp/c.h"
 #include "tmp/postgres.h"
 
-#include "port/machine.h"
+#include "machine.h"
 #include "storage/smgr.h"
+#include "storage/block.h"
+#include "utils/rel.h"
+#include "utils/log.h"
+
+RcsId("$Header$");
 
 typedef struct f_smgr {
-    int		(*smgr_create))(),
-    int		(*smgr_unlink))(),
-    int		(*smgr_extend))(),
-    int		(*smgr_open))(),
-    int		(*smgr_close))(),
-    int		(*smgr_read))(),
-    int		(*smgr_write))(),
-    int		(*smgr_flush))(),
-    int		(*smgr_nblocks))(),
-    int		(*smgr_commit))(),
-    int		(*smgr_abort))()
+    int		(*smgr_init)();		/* may be NULL */
+    int		(*smgr_shutdown)();	/* may be NULL */
+    int		(*smgr_create)();
+    int		(*smgr_unlink)();
+    int		(*smgr_extend)();
+    int		(*smgr_open)();
+    int		(*smgr_close)();
+    int		(*smgr_read)();
+    int		(*smgr_write)();
+    int		(*smgr_flush)();
+    int		(*smgr_blindwrt)();
+    int		(*smgr_nblocks)();
+    int		(*smgr_commit)();	/* may be NULL */
+    int		(*smgr_abort)();	/* may be NULL */
 } f_smgr;
 
-extern int	mdcreate(), mdunlink();
+/* routines defined elsewhere */
+extern int	mdinit(), mdcreate(), mdunlink();
 extern int	mdextend(), mdopen(), mdclose(), mdread();
-extern int	mdwrite(), mdflush(), mdnblocks(), mdcommit(), mdabort();
+extern int	mdwrite(), mdflush(), mdblindwrt(), mdnblocks();
+extern int	mdcommit(), mdabort();
 
-extern int	sjcreate(), sjunlink();
+extern int	sjinit(), sjshutdown(), sjcreate(), sjunlink();
 extern int	sjextend(), sjopen(), sjclose(), sjread();
-extern int	sjwrite(), sjflush(), sjnblocks(), sjcommit(), sjabort();
+extern int	sjwrite(), sjflush(), sjblindwrt(), sjnblocks();
+extern int	sjcommit(), sjabort();
 
 static f_smgr smgrsw[] = {
 
     /* magnetic disk */
-    { mdcreate, mdunlink, mdextend, mdopen, mdclose, mdread, mdwrite,
-      mdflush, mdnblocks, mdcommit, mdabort },
+    { mdinit, NULL, mdcreate, mdunlink, mdextend, mdopen, mdclose,
+      mdread, mdwrite, mdflush, mdblindwrt, mdnblocks, mdcommit, mdabort },
 
     /* sony jukebox */
-    { sjcreate, sjunlink, sjextend, sjopen, sjclose, sjread, sjwrite,
-      sjflush, sjnblocks, sjcommit, sjabort }
+    { sjinit, sjshutdown, sjcreate, sjunlink, sjextend, sjopen, sjclose,
+      sjread, sjwrite, sjflush, sjblindwrt, sjnblocks, sjcommit, sjabort }
 };
 
 static int NSmgr = lengthof(smgrsw);
 
 /*
- *  smgrcreate() -- Create a new relation by name.
+ *  smgrinit(), smgrshutdown() -- Initialize or shut down all storage
+ *				  managers.
+ */
+
+int
+smgrinit()
+{
+    int i;
+    extern char *smgrout();
+
+    for (i = 0; i < NSmgr; i++) {
+	if (smgrsw[i].smgr_init) {
+	    if ((*(smgrsw[i].smgr_init))() == SM_FAIL)
+		elog(FATAL, "initialization failed on %s", smgrout(i));
+	}
+    }
+
+    /* register the shutdown proc */
+    on_exitpg(smgrshutdown, 0);
+
+    return (SM_SUCCESS);
+}
+
+void
+smgrshutdown(dummy)
+    int dummy;
+{
+    int i;
+    extern char *smgrout();
+
+    for (i = 0; i < NSmgr; i++) {
+	if (smgrsw[i].smgr_shutdown) {
+	    if ((*(smgrsw[i].smgr_shutdown))() == SM_FAIL)
+		elog(FATAL, "shutdown failed on %s", smgrout(i));
+	}
+    }
+}
+
+/*
+ *  smgrcreate() -- Create a new relation.
  *
- *	This routine creates the named object on the appropriate storage
+ *	This routine takes a reldesc, creates the relation on the appropriate
  *	device, and returns a file descriptor for it.
  */
 
 int
-smgrcreate(which, relname)
+smgrcreate(which, reln)
     int16 which;
-    char *relname;
+    Relation reln;
 {
     int fd;
 
-    Assert(which >= 0 && which < NSmgr);
-
-    if ((fd = (*(smgrsw[which].smgr_create))(relname)) < 0)
-	elog(WARN, "cannot open %s: %m", relname);
+    if ((fd = (*(smgrsw[which].smgr_create))(reln)) < 0)
+	elog(WARN, "cannot open %16s", reln->rd_rel->relname);
 
     return (fd);
 }
 
 /*
- *  smgrunlink() -- Unlink a relation by name.
+ *  smgrunlink() -- Unlink a relation.
  *
- *	The appropriate object is removed from the store.
+ *	The relation is removed from the store.
  */
 
 int
-smgrunlink(which, relname)
-    int16 which;
-    char *relname;
-{
-    int status;
-
-    Assert(which >= 0 && which < NSmgr);
-
-    if ((status = (*(smgrsw[which].smgr_unlink))(relname)) == SM_FAIL)
-	elog(WARN, "cannot unlink %s: %m", relname);
-
-    return (status);
-}
-int
-smgrextend(which, reln, bufhdr)
+smgrunlink(which, reln)
     int16 which;
     Relation reln;
-    BufferDesc bufhdr;
 {
     int status;
 
-    Assert(which >= 0 && which < NSmgr);
-
-    /* new blocks are zero-filled */
-    bzero((char *) MAKE_PTR(bufhdr->data), BLCKSZ);
-
-    status = (*(smgrsw[which].smgr_extend))(reln, MAKE_PTR(bufhdr->data));
-
-    if (status == SM_FAIL)
-	elog(WARN, "%16s: cannot extend: %m", reln->rd_rel->relname);
+    if ((status = (*(smgrsw[which].smgr_unlink))(reln)) == SM_FAIL)
+	elog(WARN, "cannot unlink %16s", reln->rd_rel->relname);
 
     return (status);
 }
 
-int
-smgropen(which, path)
-    int16 which;
-    char *path;
-{
-    Assert(which >= 0 && which < NSmgr);
+/*
+ *  smgrextend() -- Add a new block to a file.
+ *
+ *	Returns SM_SUCCESS on success; aborts the current transaction on
+ *	failure.
+ */
 
-    return ((*(smgrsw[which].smgr_open))(path));
+int
+smgrextend(which, reln, buffer)
+    int16 which;
+    Relation reln;
+    char *buffer;
+{
+    int status;
+
+    /* new blocks are zero-filled */
+    bzero((char *) buffer, BLCKSZ);
+
+    status = (*(smgrsw[which].smgr_extend))(reln, buffer);
+
+    if (status == SM_FAIL)
+	elog(WARN, "%16s: cannot extend", reln->rd_rel->relname);
+
+    return (status);
 }
 
-int
-smgrclose(which, file)
-    int16 which;
-    File file;
-{
-    Assert(which >= 0 && which < NSmgr);
+/*
+ *  smgropen() -- Open a relation using a particular storage manager.
+ *
+ *	Returns the fd for the open relation on success, aborts the
+ *	transaction on failure.
+ */
 
-    return ((*(smgrsw[which].smgr_close))(file));
+int
+smgropen(which, reln)
+    int16 which;
+    Relation reln;
+{
+    int fd;
+
+    if ((fd = (*(smgrsw[which].smgr_open))(reln)) < 0)
+	elog(WARN, "cannot open %16s", reln->rd_rel->relname);
+
+    return (fd);
 }
 
+/*
+ *  smgrclose() -- Close a relation.
+ *
+ *	Returns SM_SUCCESS on success, aborts on failure.
+ */
+
 int
-smgrread(which, reln, blocknum)
+smgrclose(which, reln)
+    int16 which;
+    Relation reln;
+{
+    if ((*(smgrsw[which].smgr_close))(reln) == SM_FAIL)
+	elog(WARN, "cannot close %16s", reln->rd_rel->relname);
+
+    return (SM_SUCCESS);
+}
+
+/*
+ *  smgrread() -- read a particular block from a relation into the supplied
+ *		  buffer.
+ *
+ *	This routine is called from the buffer manager in order to
+ *	instantiate pages in the shared buffer cache.  All storage managers
+ *	return pages in the format that POSTGRES expects.  This routine
+ *	dispatches the read.  On success, it returns SM_SUCCESS.  On failure,
+ *	the current transaction is aborted.
+ */
+
+int
+smgrread(which, reln, blocknum, buffer)
     int16 which;
     Relation reln;
     BlockNumber blocknum;
+    char *buffer;
 {
-    Assert(which >= 0 && which < NSmgr);
+    int status;
 
-    return ((*(smgrsw[which].smgr_read))(reln, blocknum));
+    status = (*(smgrsw[which].smgr_read))(reln, blocknum, buffer);
+
+    if (status == SM_FAIL)
+	elog(WARN, "cannot read block %d of %16s",
+	     blocknum, reln->rd_rel->relname);
+
+    return (status);
 }
+
+/*
+ *  smgrwrite() -- Write the supplied buffer out.
+ *
+ *	This is not a synchronous write -- the interface for that is
+ *	smgrflush().  The buffer is written out via the appropriate
+ *	storage manager.  This routine returns SM_SUCCESS or aborts
+ *	the current transaction.
+ */
 
 int
-smgrwrite(which, bufhdr)
+smgrwrite(which, reln, blocknum, buffer)
     int16 which;
-    BufferDesc bufhdr;
+    Relation reln;
+    BlockNumber blocknum;
+    char *buffer;
 {
-    Assert(which >= 0 && which < NSmgr);
+    int status;
 
-    return ((*(smgrsw[which].smgr_write))(bufhdr));
+    status = (*(smgrsw[which].smgr_write))(reln, blocknum, buffer);
+
+    if (status == SM_FAIL)
+	elog(WARN, "cannot write block %d of %16s",
+	     blocknum, reln->rd_rel->relname);
+
+    return (status);
 }
+
+/*
+ *  smgrflush() -- A synchronous smgrwrite().
+ */
 
 int
-smgrflush(which, bufhdr)
+smgrflush(which, reln, blocknum, buffer)
     int16 which;
-    BufferDesc bufhdr;
+    Relation reln;
+    BlockNumber blocknum;
+    char *buffer;
 {
-    Assert(which >= 0 && which < NSmgr);
+    int status;
 
-    return ((*(smgrsw[which].smgr_flush))(bufhdr));
+    status = (*(smgrsw[which].smgr_flush))(reln, blocknum, buffer);
+
+    if (status == SM_FAIL)
+	elog(WARN, "cannot flush block %d of %16s to stable store",
+	     blocknum, reln->rd_rel->relname);
+
+    return (status);
 }
+
+/*
+ *  smgrblindwrt() -- Write a page out blind.
+ *
+ *	In some cases, we may find a page in the buffer cache that we
+ *	can't make a reldesc for.  This happens, for example, when we
+ *	want to reuse a dirty page that was written by a transaction
+ *	that has not yet committed, which created a new relation.  In
+ *	this case, the buffer manager will call smgrblindwrt() with
+ *	the name and OID of the database and the relation to which the
+ *	buffer belongs.  Every storage manager must be able to force
+ *	this page down to stable storage in this circumstance.
+ */
+
+int
+smgrblindwrt(which, dbname, relname, dbid, relid, blkno, buffer)
+    int16 which;
+    Name dbname;
+    Name relname;
+    OID dbid;
+    OID relid;
+    BlockNumber blkno;
+    char *buffer;
+{
+    char *dbstr;
+    char *relstr;
+    int nbytes;
+    int status;
+
+    /* do the postgres name dance */
+    nbytes = sizeof(NameData) + 1;
+    dbstr = (char *) palloc(nbytes);
+    relstr = (char *) palloc(nbytes);
+    strncpy(dbstr, dbname, sizeof(NameData));
+    strncpy(relstr, relname, sizeof(NameData));
+    dbstr[nbytes - 1] = '\0';
+    relstr[nbytes - 1] = '\0';
+
+    status = (*(smgrsw[which].smgr_blindwrt))(dbstr, relstr, dbid, relid,
+					      blkno, buffer);
+
+    pfree(dbstr);
+    pfree(relstr);
+
+    if (status == SM_FAIL)
+	elog(WARN, "cannot write block %d of %s [%s] blind",
+	     blkno, relstr, dbstr);
+
+    return (status);
+}
+/*
+ *  smgrnblocks() -- Calculate the number of POSTGRES blocks in the
+ *		     supplied relation.
+ *
+ *	Returns the number of blocks on success, aborts the current
+ *	transaction on failure.
+ */
 
 int
 smgrnblocks(which, reln)
     int16 which;
     Relation reln;
 {
-    Assert(which >= 0 && which < NSmgr);
+    int nblocks;
 
-    return ((*(smgrsw[which].smgr_nblocks))(reln));
+    if ((nblocks = (*(smgrsw[which].smgr_nblocks))(reln)) < 0)
+	elog(WARN, "cannot count blocks for %16s", reln->rd_rel->relname);
+
+    return (nblocks);
+}
+
+/*
+ *  smgrcommit(), smgrabort() -- Commit or abort changes made during the
+ *				 current transaction.
+ */
+
+int
+smgrcommit()
+{
+    int i;
+    extern char *smgrout();
+
+    for (i = 0; i < NSmgr; i++) {
+	if (smgrsw[i].smgr_commit) {
+	    if ((*(smgrsw[i].smgr_commit))() == SM_FAIL)
+		elog(FATAL, "transaction commit failed on %s", smgrout(i));
+	}
+    }
+
+    return (SM_SUCCESS);
 }
 
 int
-smgrcommit(which)
-    int16 which;
+smgrabort()
 {
-    Assert(which >= 0 && which < NSmgr);
+    int i;
+    extern char *smgrout();
 
-    return ((*(smgrsw[which].smgr_commit))());
-}
+    for (i = 0; i < NSmgr; i++) {
+	if (smgrsw[i].smgr_abort) {
+	    if ((*(smgrsw[i].smgr_abort))() == SM_FAIL)
+		elog(FATAL, "transaction abort failed on %s", smgrout(i));
+	}
+    }
 
-int
-smgrabort(which)
-    int16 which;
-{
-    Assert(which >= 0 && which < NSmgr);
-
-    return ((*(smgrsw[which].smgr_abort))());
+    return (SM_SUCCESS);
 }
