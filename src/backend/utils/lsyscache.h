3ed4dcf309eb1e9eb00cba1e7ea10cc5febaf5b8
sp <sp>
1991-07-16 13:13:43 +0000

    new argument in 'get_relstub'

--- src/backend/utils/lsyscache.h
+++ src/backend/utils/lsyscache.h
@@ -15,7 +15,7 @@ extern RegProcedure get_oprjoin ARGS((ObjectId opid));
 extern ObjectId get_regproc ARGS((Name funname));
 extern AttributeNumber get_relnatts ARGS((ObjectId relid));
 extern Name get_rel_name ARGS((ObjectId relid));
-extern struct varlena * get_relstub ARGS((ObjectId relid));
+extern struct varlena * get_relstub ARGS((ObjectId relid, int no, bool *islast));
 extern ObjectId get_ruleid ARGS((Name rulename));
 extern ObjectId get_eventrelid ARGS((ObjectId ruleid));
 extern int16 get_typlen ARGS((ObjectId typid));
