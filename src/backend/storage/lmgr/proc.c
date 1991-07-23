e4da550e79f90f3a608a8d94de4847c1524de341
mer <mer>
1991-07-23 19:55:22 +0000

    move out structs for shmem size computation in lock.c

--- src/backend/storage/lmgr/proc.c
+++ src/backend/storage/lmgr/proc.c
@@ -56,47 +56,6 @@ SPINLOCK ProcStructLock;
  */
 Boolean	ProcInitialized = FALSE;
 
-/*
- * Each backend has:
- */
-typedef struct proc {
-
-  /* proc->links MUST BE THE FIRST ELEMENT OF STRUCT (see ProcWakeup()) */
-
-  SHM_QUEUE         links;	/* proc can be waiting for one event(lock) */
-  SEMA              sem;	/* ONE semaphore to sleep on */
-  int               errType; 	/* error code tells why we woke up */
-
-  int               procId;  	/* unique number for this structure
-			 	 * NOT unique per backend, these things
-				 * are reused after the backend dies.
-				 */
-
-  int               critSects;	/* If critSects > 0, we are in sensitive
-				 * routines that cannot be recovered when
-				 * the process fails.
-				 */
-
-  int               prio;	/* priority for sleep queue */
-
-  TransactionIdData xid;	/* transaction currently being executed
-				 * by this proc
-				 */
-
-  int               token;	/* info for proc wakeup routines */	
-  SHM_QUEUE	lockQueue;	/* locks associated with current transaction */
-} PROC;
-
-/*
- * Global variables for this module.
- *	This is only used for garbage collection.
- */
-typedef struct procglobal {
-  SHMEM_OFFSET	freeProcs;
-  int		numProcs;
-  IPCKey	currKey;
-} PROC_HDR;
-
 PROC_HDR *ProcGlobal = NULL;
 
 PROC 		*MyProc = NULL;
