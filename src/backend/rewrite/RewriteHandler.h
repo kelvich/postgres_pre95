154e24edd98b37058607c88cf14de74b57856d1f
glass <glass>
1991-07-30 07:09:34 +0000

    obsoleted goh's prototypes defined RewriteInfo structure used to pass around meta info about the current parsetree, rule, etc.

--- src/backend/rewrite/RewriteHandler.h
+++ src/backend/rewrite/RewriteHandler.h
@@ -3,6 +3,7 @@
  * $Header$
  */
 
+#ifdef OLD_REWRITE
 int ChangeTheseVars ARGS((int, short, struct _LispValue *, struct _LispValue *));
 int ReplaceVarWithMulti ARGS((int, short, struct _LispValue *, struct _LispValue *));
 int FixResdom ARGS((struct _LispValue *));
@@ -10,3 +11,18 @@ struct _LispValue *ModifyVarNodes ARGS((struct _LispValue *, int, int, struct Re
 struct _LispValue *ModifyUpdateNodes ARGS((struct _LispValue *, struct _LispValue *, char *, int));
 struct _LispValue *ProcessEntry ARGS((struct _LispValue *, struct RelationData *, struct _LispValue *, int, int, char *));
 struct _LispValue *QueryRewrite ARGS((struct _LispValue *));
+#endif OLD_REWRITE
+
+struct _rewrite_meta_knowledge {
+    List rt;
+    int rt_index;
+    int instead_flag;
+    int event;
+    int action;
+    int current_varno;
+    int new_varno;
+    List rule_action,rule_qual;
+    int nothing;
+};
+
+typedef struct _rewrite_meta_knowledge RewriteInfo;
