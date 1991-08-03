509df9e87d05f8efe77e5cbc9cfc786cdfc08514
glass <glass>
1991-08-03 16:49:37 +0000

    added new prototypes

--- src/backend/rewrite/RewriteHandler.h
+++ src/backend/rewrite/RewriteHandler.h
@@ -26,3 +26,15 @@ struct _rewrite_meta_knowledge {
 };
 
 typedef struct _rewrite_meta_knowledge RewriteInfo;
+
+RewriteInfo *GatherRewriteMeta ARGS((List parsetree, List rule_action, List rule_qual, int rt_index, int event, int *instead_flag));
+List OptimizeRIRRules ARGS((List locks));
+List OrderRules ARGS((List locks));
+List FireRetrieveRulesAtQuery ARGS((List parsetree, int rt_index, Relation relation,int *instead_flag));
+ApplyRetrieveRule ARGS((List parsetree, List rule, int rt_index,int relation_level, int attr_num,int *modified));
+List ProcessRetrieveQuery ARGS((List parsetree, List rt,int *instead_flag, int rule));
+List FireRules ARGS((List parsetree, int rt_index, int event, int *instead_flag, List locks));
+List ProcessUpdateNode ARGS((List parsetree, int rt_index, int event, int *instead_flag,RuleLock relation_locks));
+List RewriteQuery ARGS((List parsetree,int *instead_flag));
+List QueryRewrite ARGS(( List parsetree ));
+
