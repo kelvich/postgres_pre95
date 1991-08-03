4807293b6b8f6fc8a7a42090595b100fd5de82a9
glass <glass>
1991-08-03 16:50:33 +0000

    added prototypes

--- src/backend/rewrite/RewriteManip.h
+++ src/backend/rewrite/RewriteManip.h
@@ -11,3 +11,15 @@ void HandleVarNodes ARGS(( List, List, int, int, int ));
 void UpdateRangeTableOfViewParse ARGS((Name name, List parsetree));
 #endif  OLD_REWRITE
 
+
+void OffsetVarNodes ARGS(( List qual_or_tlist , int offset )) ;
+void ChangeVarNodes ARGS(( List qual_or_tlist , int old_varno, int new_varno));
+void AddQualARGS((List parsetree, List qual));
+void AddNotQualARGS((List parsetree, List qual));
+Const make_nullARGS((ObjectId type));
+List FindMatchingNew ARGS(( List user_tlist , int attno ));
+List FindMatchingTLEntry ARGS(( List user_tlist , Name e_attname));
+void ResolveNewARGS((RewriteInfo *info, List targetlist,List tree));
+void FixNewARGS((RewriteInfo *info, List parsetree));
+void HandleRIRAttributeRuleARGS((List parsetree, List rt, List tl, int rt_index, int attr_num,int *modified));
+void HandleViewRuleARGS((List parsetree, List rt,List tl, int rt_index,int *modified));
