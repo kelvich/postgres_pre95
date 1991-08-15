8b0e33d5aac292155aeb7493897fa3470e413f71
caetta <caetta>
1991-08-15 17:51:36 +0000

    more various aggregate revisions

--- src/backend/planner/planner.h
+++ src/backend/planner/planner.h
@@ -2,5 +2,6 @@
 
 extern Plan planner ARGS((LispValue parse));
 extern Plan make_sortplan ARGS((List tlist, List sortkeys, List sortops, List plannode));
+extern Plan make_aggplan ARGS((List agg_tlist, List tlist, List qual, List currentlevel, List sortkeys));
 extern Plan init_query_planner ARGS((LispValue root, LispValue tlist, LispValue qual));
 extern Existential make_existential ARGS((Plan left, Plan right));
