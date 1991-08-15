61a623608a93e0d2da15fceebe93ee6d86a32104
caetta <caetta>
1991-08-15 17:51:17 +0000

    various Agg revisions

--- src/backend/planner/planmain.h
+++ src/backend/planner/planmain.h
@@ -3,4 +3,4 @@
 extern Plan query_planner ARGS((LispValue command_type, LispValue tlist, LispValue qual, LispValue currentlevel, LispValue maxlevel));
 extern Plan subplanner ARGS((LispValue flat_tlist, LispValue original_tlist, LispValue qual, LispValue level, LispValue sortkeys));
 extern Result make_result ARGS(( List tlist, List resrellevelqual, Plan left, Plan right));
-
+extern Plan make_aggplan ARGS((List agg_tlist, List tlist, List qual, List currentlevel, List sortkeys));
