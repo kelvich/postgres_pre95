c2b8eb29ed76fe16702bab3df67914f29a8ae085
caetta <caetta>
1991-08-17 02:38:55 +0000

    to support the newer, cleaner changes...

--- src/backend/executor/x_aggregate.h
+++ src/backend/executor/x_aggregate.h
@@ -1,6 +1,4 @@
 extern void ExecEndAgg ARGS((Agg node));
 extern List ExecInitAgg ARGS((Agg node, EState estate, Plan parent));
 extern TupleTableSlot ExecAgg ARGS((Agg node));
-extern char *update_aggregate ARGS((char *Caggname, char *running_comp, Datum attr_data, ObjectId func1, ObjectId func2));
-extern char *finalize_aggregate ARGS((Datum *running_comp, ObjectId func));
 
