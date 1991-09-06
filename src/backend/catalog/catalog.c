afa36417d7918af34782d58d3bd17b113eb269b3
mer <mer>
1991-09-06 15:58:35 +0000

    Make issystem() a lot cleaner -- it simply checks for pg_ This also allowed a lot of cleaning up of the code.

--- src/backend/catalog/catalog.c
+++ src/backend/catalog/catalog.c
@@ -23,123 +23,41 @@ RcsId("$Header$");
  * XXX in lib/catalog/catname.c, someday.
  */
 
-char	AGGREGATE_R[16] = 	"pg_aggregate";
-char	AM_R[16] = 		"pg_am";
-char	AMOP_R[16] = 		"pg_amop";
-char	AMPROC_R[16] = 		"pg_amproc";
-char	ATTRIBUTE_R[16] = 	"pg_attribute";
-char	CLASS_R[16] = 		"pg_class";
 char	DATABASE_R[16] = 	"pg_database";
 char	DEFAULTS_R[16] = 	"pg_defaults";
 char	DEMON_R[16] = 		"pg_demon";
-char	INDEX_R[16] = 		"pg_index";
-char	INHERITPROC_R[16] = 	"pg_inheritproc";
-char	INHERITS_R[16] = 	"pg_inherits";
-char	IPL_R[16] = 		"pg_ipl";
-char	LANGUAGE_R[16] = 	"pg_language";
 char	LOG_R[16] = 		"pg_log";
 char	MAGIC_R[16] = 		"pg_magic";
-char	OPCLASS_R[16] = 	"pg_opclass";
-char	OPERATOR_R[16] = 	"pg_operator";
-char	PARG_R[16] = 		"pg_parg";
-char	PROC_R[16] = 		"pg_proc";
-char	PRS2PLANS_R[16] = 	"pg_prs2plans";
-char 	PRS2RULE_R[16] = 	"pg_prs2rule";
-char 	PRS2STUB_R[16] = 	"pg_prs2stub";
-char	RULE_R[16] = 		"pg_rule";
-char	RULEPLANS_R[16] = 	"pg_ruleplans";
 char	SERVER_R[16] = 		"pg_server";
-char	STATISTIC_R[16] = 	"pg_statistic";
 char	TIME_R[16] = 		"pg_time";
 char	TYPE_R[16] = 		"pg_type";
 char	USER_R[16] = 		"pg_user";
 char	VARIABLE_R[16] = 	"pg_variable";
-char	VERSION_R[16] = 	"pg_version";
 
 /* ----------------
  *	WARNING  WARNING  WARNING  WARNING  WARNING  WARNING
  *
- *	keep SystemRelname[] in SORTED order!  A binary search
+ *	keep SharedSysRelns[] in SORTED order!  A binary search
  *	is done on it!
  *
  *	XXX this is a serious hack which should be fixed -cim 1/26/90
  * ----------------
  */
-static char *SystemRelname[] = {
-    AGGREGATE_R,
-    AM_R,
-    AMOP_R,
-    AMPROC_R,
-    ATTRIBUTE_R,
-    CLASS_R,
+static char *SharedSysRelns[] = {
     DATABASE_R,
     DEFAULTS_R,
     DEMON_R,
-    INDEX_R,
-    INHERITPROC_R,
-    INHERITS_R,
-    IPL_R,
-    LANGUAGE_R,
     LOG_R,
     MAGIC_R,
-    OPCLASS_R,
-    OPERATOR_R,
-    PARG_R,
-    PROC_R, 
-    PRS2PLANS_R,
-    PRS2RULE_R,
-    PRS2STUB_R,
-    RULE_R,
-    RULEPLANS_R,
     SERVER_R,
-    STATISTIC_R,
     TIME_R,
-    TYPE_R,
     USER_R,
     VARIABLE_R,
-    VERSION_R,		
 };
-
-static	char	**Spp;			/* system relname pointer */
 
 #ifndef	MAXPATHLEN
 #define	MAXPATHLEN	80
 #endif
-
-static	char	IsDbdb[] = {
-    '\0',	    /* AGGREGATE_R, 	*/
-    '\0',	    /* AM_R,            */
-    '\0',	    /* AMOP_R,          */
-    '\0',	    /* AMPROC_R,        */
-    '\0',	    /* ATTRIBUTE_R,     */
-    '\0',	    /* CLASS_R,         */
-    '\001',	    /* DATABASE_R,      */
-    '\001',	    /* DEFAULTS_R,      */
-    '\001',	    /* DEMON_R,         */
-    '\0',	    /* INDEX_R,         */
-    '\0',	    /* INHERITPROC_R,   */
-    '\0',	    /* INHERITS_R,      */
-    '\0',	    /* IPL_R,           */
-    '\0',	    /* LANGUAGE_R,      */
-    '\001',	    /* LOG_R,           */
-    '\001',	    /* MAGIC_R,         */
-    '\0',	    /* OPCLASS_R,       */
-    '\0',	    /* OPERATOR_R,      */
-    '\0',	    /* PARG_R,          */
-    '\0',	    /* PROC_R,          */
-    '\0',	    /* PRS2PLANS_R,     */
-    '\0',	    /* PRS2RULE_R,      */
-    '\0',	    /* PRS2STUB_R,      */
-    '\0',	    /* RULE_R,          */
-    '\0',	    /* RULEPLANS_R,     */
-    '\001',	    /* SERVER_R,        */
-    '\0',	    /* STATISTIC_R,     */
-    '\001',	    /* TIME_R,          */
-    '\0',	    /* TYPE_R,          */
-    '\001',	    /* USER_R,          */
-    '\001',	    /* VARIABLE_R,      */
-    '\0',	    /* VERSION_R,       */
-};
 
 /*
  *	relpath		- path to the relation
@@ -159,7 +77,7 @@ char    relname[];
         static  char    path[1 + 6 + 16] = "../../";
         static  char    *appnd = path + 6;
 
-        if (issystem(relname) && IsDbdb[Spp - SystemRelname]) {
+        if (NameIsSharedSystemRelationName(relname)) {
                 strncpy(appnd, relname, 16);
                 return(path);
         }
@@ -169,49 +87,21 @@ char    relname[];
 /*
  *	issystem	- returns non-zero iff relname is a system catalog
  *
- *	Uses a binary search on the SystemRelname table.  Hashing would
- *	probably be more efficient since the relation names are static.
- *	Currently, leaves Spp pointing to the SystemRelname string.
+ *	We now make a new requirement where system catalog relns must begin
+ *	with pg_ while user relns are forbidden to do so.  Make the test
+ * 	trivial and instantaneous.
+ *
  */
-
-int
+bool
 issystem(relname)
 char	relname[];
 {
-	register char	**spp;
-	register char	**mpp;
-	int		len;
-	int		mid;
-	int		status;
-
-	spp = SystemRelname;
-	len = sizeof (SystemRelname) / sizeof (char *);
-	while (len > 2) {
-		mid = len >> 1;
-		mpp = spp + mid;
-		if ((status = strncmp(relname, *mpp, 16)) == 0) {
-			Spp = mpp;
-			return(1);
-		}
-		else if (status < 0)
-			len = mid;
-		else {
-			spp = mpp + 1;
-			len -= mid + 1;
-		}
-	}
-	if (len <= 0)
-		return(0);
-	if (len == 2) {
-		if ((status = strncmp(relname, *spp, 16)) == 0) {
-			Spp = spp;
-			return(1);
-		} else if (status < 0)
-			return(0);
-		spp++;
-	}
-	Spp = spp;
-	return(strncmp(relname, *spp, 16) == 0);
+	if (relname[0] && relname[1] && relname[2])
+		return (relname[0] == 'p' && 
+			relname[1] == 'g' && 
+			relname[2] == '_');
+	else
+		return FALSE;
 }
 
 bool
@@ -222,10 +112,54 @@ NameIsSystemRelationName(name)
 }
 
 bool
-NameIsSharedSystemRelationName(name)
-	Name	name;
+NameIsSharedSystemRelationName(relname)
+	Name	relname;
 {
-	return ((bool)(NameIsSystemRelationName(name) && IsDbdb[Spp - SystemRelname]));
+	register char	**spp;
+	register char	**mpp;
+	int		len;
+	int		mid;
+	int		status;
+
+	spp = SharedSysRelns;
+
+	/*
+	 * Quick out: if its not a system reln it can't be a shared system reln.
+	 */
+	if (!issystem(relname))
+		return FALSE;
+	/*
+	 * Total number of Shared System relations
+	 */
+	len = sizeof (SharedSysRelns) / sizeof (char *);
+
+	/*
+	 * While there's more than 2 candidates remaining, run the
+	 * binary search algorithm
+	 */
+	while (len > 2) {
+		mid = len >> 1;
+		mpp = spp + mid;
+		if ((status = strncmp(relname, *mpp, 16)) == 0) {
+			return(TRUE);
+		}
+		else if (status < 0)
+			len = mid;
+		else {
+			spp = mpp + 1;
+			len -= mid + 1;
+		}
+	}
+	if (len <= 0)
+		return(FALSE);
+	if (len == 2) {
+		if ((status = strncmp(relname, *spp, 16)) == 0) {
+			return(1);
+		} else if (status < 0)
+			return(FALSE);
+		spp++;
+	}
+	return(strncmp(relname, *spp, 16) == 0);
 }
 
 /*
