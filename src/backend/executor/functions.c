b1cb076bf2c242c265b11bf1d5e56ecba74ae8fe
glass <glass>
1991-10-06 22:10:20 +0000

    Initial revision

---
+++ src/backend/executor/functions.c
@@ -0,0 +1,18 @@
+/* ------------------------------------------------
+ *   FILE
+ *     functions.c
+ *
+ *   DESCRIPTION
+ *	Routines to handle functions called from the executor
+ *      Putting this stuff in fmgr makes the postmaster a mess....
+ *
+ *  NOTES  
+ *	this is a new file, jc
+ *
+ *   IDENTIFICATION
+ *   	$Header
+ * ------------------------------------------------
+ */
+
+
+
