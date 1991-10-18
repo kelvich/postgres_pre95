4f7f77351e2c29f13693fac6f6fdb170467703ab
mer <mer>
1991-10-18 08:22:21 +0000

    extend macro functionality and add a typedef for pointers

--- src/backend/access/funcindex.h
+++ src/backend/access/funcindex.h
@@ -3,6 +3,7 @@
 #ifndef _FUNC_INDEX_INCLUDED_
 #define _FUNC_INDEX_INCLUDED_
 
+#include "tmp/postgres.h"
 
 typedef struct {
 	int		nargs;
@@ -10,8 +11,24 @@ typedef struct {
 	NameData	funcName;
 } FuncIndexInfo;
 
+typedef FuncIndexInfo	*FuncIndexInfoPtr;
+
+/*
+ * some marginally useful macro definitions
+ */
 #define FIgetname(FINFO) (&((FINFO)->funcName.data[0]))
 #define FIgetnArgs(FINFO) (FINFO)->nargs
 #define FIgetProcOid(FINFO) (FINFO)->procOid
+
+#define FIsetname(FINFO,name) strncpy(&((FINFO)->funcName.data[0]), name, 16)
+#define FIsetnArgs(FINFO, numargs) ((FINFO)->nargs = numargs)
+#define FIsetProcOid(FINFO, id) ((FINFO)->procOid = id)
+
+#define FIisFunctionalIndex(FINFO) (FINFO->procOid != InvalidObjectId)
+
+#define FIcopyFuncInfo(TO, FROM) \
+	TO->nargs = FROM->nargs; \
+	TO->procOid = FROM->procOid; \
+	strncpy(&((TO)->funcName.data[0]), &((FROM)->funcName.data[0]), 16)
 
 #endif _FUNC_INDEX_INCLUDED_
