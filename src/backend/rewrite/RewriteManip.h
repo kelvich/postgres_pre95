4b97e072b177b217a0a52b72b6de807121f6920f
glass <glass>
1991-11-06 22:25:26 +0000

    new prototypes

--- src/backend/rewrite/RewriteManip.h
+++ src/backend/rewrite/RewriteManip.h
@@ -3,23 +3,14 @@
  * $Header$
  */
 
-#ifdef OLD_REWRITE
-void OffsetVarNodes ARGS(( List , int ));
-void FixRangeTable ARGS(( List , List ));
-void AddQualifications ARGS(( List , List , int ));
-void HandleVarNodes ARGS(( List, List, int, int, int ));
-void UpdateRangeTableOfViewParse ARGS((Name name, List parsetree));
-#endif  OLD_REWRITE
+/* RewriteManip.c */
+void OffsetVarNodes ARGS((List qual_or_tlist , int offset ));
+void ChangeVarNodes ARGS((List qual_or_tlist , int old_varno , int new_varno ));
+void AddQual ARGS((List parsetree , List qual ));
+void AddNotQual ARGS((List parsetree , List qual ));
+void FixResdomTypes ARGS((List user_tlist ));
+void ResolveNew ARGS((RewriteInfo *info , List targetlist , List tree ));
+void FixNew ARGS((RewriteInfo *info , List parsetree ));
+void HandleRIRAttributeRule ARGS((List parsetree , List rt , List tl , int rt_index , int attr_num , int *modified , int *badpostquel ));
+void HandleViewRule ARGS((List parsetree , List rt , List tl , int rt_index , int *modified ));
 
-
-void OffsetVarNodes ARGS(( List qual_or_tlist , int offset )) ;
-void ChangeVarNodes ARGS(( List qual_or_tlist , int old_varno, int new_varno));
-void AddQual ARGS((List parsetree, List qual));
-void AddNotQual ARGS((List parsetree, List qual));
-Const make_null ARGS((ObjectId type));
-List FindMatchingNew ARGS(( List user_tlist , int attno ));
-List FindMatchingTLEntry ARGS(( List user_tlist , Name e_attname));
-void ResolveNew ARGS((RewriteInfo *info, List targetlist,List tree));
-void FixNew ARGS((RewriteInfo *info, List parsetree));
-void HandleRIRAttributeRule ARGS((List parsetree, List rt, List tl, int rt_index, int attr_num,int *modified));
-void HandleViewRule ARGS((List parsetree, List rt,List tl, int rt_index,int *modified));
