0c986ae594575a690bc18bf224a0de6c8ab3576f
mer <mer>
1991-11-18 17:35:11 +0000

    prototyping - get rid of an extraneous prototype

--- src/backend/tmp/utilities.h
+++ src/backend/tmp/utilities.h
@@ -1,6 +1,5 @@
 /* $Header$ */
 extern LispValue vectori_long_to_list ARGS((int *vectori, int start, int n));
-extern LispValue collect ARGS((int pred, LispValue list));
 /* extern LispValue same ARGS((LispValue list1, LispValue list2)); */
 extern LispValue last_element ARGS((LispValue list));
 extern LispValue copy_seq_tree ARGS((LispValue seqtree));
