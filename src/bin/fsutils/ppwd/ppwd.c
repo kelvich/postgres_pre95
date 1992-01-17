c486b54fbe685329f1bf4fa7b89ed808ec0b445e
clarsen <clarsen>
1992-01-17 05:29:43 +0000

    port of pwd

--- src/bin/fsutils/ppwd/ppwd.c
+++ src/bin/fsutils/ppwd/ppwd.c
@@ -6,18 +6,22 @@ static	char sccsid[] = "@(#)pwd.c 1.1 90/03/23 SMI"; /* from UCB 4.4 83/01/05 */
  */
 #include <stdio.h>
 #include <sys/param.h>
+#include "tmp/libpq-fs.h"
 
 char *getwd();
 
 main()
 {
 	char pathname[MAXPATHLEN + 1];
+	
+	PQsetdb(getenv("USER"));
 
-	if (getwd(pathname) == NULL) {
-		fprintf(stderr, "pwd: %s\n", pathname);
+	if (p_getwd(pathname) == NULL) {
+		fprintf(stderr, "ppwd: %s\n", pathname);
 		exit(1);
 	}
 	printf("%s\n", pathname);
+	PQfinish();
 	exit(0);
 	/* NOTREACHED */
 }
