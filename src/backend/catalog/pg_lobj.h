180a57b045dfd9b87851951111646630d9c9fdad
clarsen <clarsen>
1992-01-17 04:34:05 +0000

    modify relation and maniuplators to store object type.

--- src/backend/catalog/pg_lobj.h
+++ src/backend/catalog/pg_lobj.h
@@ -29,6 +29,7 @@
 
 CATALOG(pg_large_object) {
     oid                 ourid;
+    int4                 objtype;
     bytea               object_descriptor; /* opaque cookie variable length structure */
 } FormData_pg_large_object;
 
@@ -37,22 +38,28 @@ CATALOG(pg_large_object) {
  * ----------------
  */
 #define Name_pg_large_object    "pg_large_object"
-#define Natts_pg_large_object                   2
+#define Natts_pg_large_object                   3
 #define Anum_pg_large_object_oid                1
-#define Anum_pg_large_object_object_descriptor  2
+#define Anum_pg_large_object_object_type        2
+#define Anum_pg_large_object_object_descriptor  3
+
+/* possible values for objtype */
+#define Inversion   0
+#define Unix        1
 
 #ifndef struct_large_object_Defined
 #define struct_large_object_Defined 1
 
 struct  large_object {
     OID                 ourid;
+    int32              objtype;
     /* variable length structure */
     struct varlena      object_descriptor;
 };
 #endif struct_large_object_Defined
 
-int LOputOIDandLargeObjDesc ARGS((oid objOID , LargeObject *desc ));
-int CreateLOBJTuple ARGS((oid objOID , LargeObject *desc ));
-LargeObject *LOassocOIDandLargeObjDesc ARGS((oid objOID ));
+int LOputOIDandLargeObjDesc ARGS((oid objOID, int objtype, struct varlena *desc ));
+int CreateLOBJTuple ARGS((oid objOID, int objtype, struct varlena *desc ));
+struct varlena *LOassocOIDandLargeObjDesc ARGS((int *objtype, oid objOID));
 
 #endif PgLargeObjectIncluded
