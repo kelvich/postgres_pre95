9c7032e295e61141b83bed3dd2824c68f8ccd1d4
clarsen <clarsen>
1992-01-17 05:15:48 +0000

    switch table for different file types.

--- src/backend/libpq/be-fsstubs.c
+++ src/backend/libpq/be-fsstubs.c
@@ -1,3 +1,4 @@
+/*#define FSDB 1*/
 /* ----------------------------------------------------------------
  *   FILE
  *	be-fsstubs.c
@@ -13,7 +14,7 @@
  * bytea LOread(int fd, int len);
  * int LOwrite(int fd, char *buf);
  * int LOlseek(int fd,int offset,int whence);
- * int LOcreat(char *path, int mode);
+ * int LOcreat(char *path, int mode,int objtype);
  * int LOtell(int fd);
  * int LOftruncate(int fd);
  * bytea LOstat(int fd);
@@ -50,7 +51,7 @@
  * bytea LOread(int fd, int len);
  * int LOwrite(int fd, char *buf);
  * int LOlseek(int fd,int offset,int whence);
- * int LOcreat(char *path, int mode);
+ * int LOcreat(char *path, int mode, int objtype);
  * int LOtell(int fd);
  * int LOftruncate(int fd);
  * bytea LOstat(int fd);
@@ -84,7 +85,7 @@
 #include "tmp/postgres.h"
 #include "tmp/libpq.h"
 #include "tmp/libpq-fs.h"
-#include "utils/large_object.h"
+/*#include "utils/large_object.h"*/
 #include "utils/mcxt.h"
 #include "catalog/pg_lobj.h"
 #include "catalog/pg_naming.h"
@@ -93,30 +94,95 @@
 
 static GlobalMemory fscxt = NULL;
 
+/* HAck protos.  move to .h later */
+/* lo_api.c */
+void *LOCreate ARGS((char *path , int open_mode ));
+void *LOOpen ARGS((struct varlena *object , int open_mode ));
+void LOClose ARGS((void *obj_desc ));
+int LOUnixStat ARGS((void *obj_desc , struct pgstat *stbuf ));
+int LOSeek ARGS((void *obj_desc , int offset , int whence ));
+int LOTell ARGS((void *obj_desc ));
+int LORead ARGS((void *obj_desc , char *buf , int n ));
+int LOWrite ARGS((void *obj_desc , char *buf , int n ));
+
+/* ftype.c */
+void  *f262open ARGS((ObjectId foid ));
+int f262seek ARGS((void *f , int loc ));
+int f262read ARGS((void *f , char *dbuf , int nbytes ));
+void f262close ARGS((void *f ));
+
+static int noaction() { return  -1; } /* error */
+static void *noactionnull() { return (void *) NULL; } /* error */
+/* These should be easy to code */
+static int f262seekwhence() {return -1;}
+static int f262tell() {return -1;}
+static int f262unixstat() {return -1;}
+
+/* This is ordered upon objtype values.  Make sure that entries are sorted
+   properly. */
+
+static struct {
+    int bigcookie; 
+#define SMALL_INT 0	 /* is integer wrapped in a varlena */
+#define BIG 1  /* must be passed as varlena*/
+    void *(*LOcreate)(char *,int); /* name,mode -> runtime-cookie */
+    void *(*LOopen)(void *,int); /* persistant-cookie,mode -> runtime-cookie */
+    void (*LOclose)(void *); /* runtime-cookie */
+    int (*LOread)(void *,char *,int); /* rt-cookie,buf,length -> bytecount*/
+    int (*LOwrite)(void *,char *,int); /*rt-cookie,buf,length -> bytecount*/
+    int (*LOseek)(void *,int,int); /*rt-cookie,offset,whence -> bytecount */
+    int (*LOtell)(void *); /*rt-cookie ->bytecount*/
+    int (*LOunixstat)(void *,struct pgstat *); /* rt-cookie,stat ->errorcode*/
+} LOprocs[] = {
+    /* Inversion */
+    { SMALL_INT,noactionnull, f262open, f262close, f262read, noaction,
+	f262seekwhence, f262tell, f262unixstat},
+    /* Unix */
+    { BIG, LOCreate, LOOpen, LOClose, LORead, LOWrite,
+	LOSeek, LOTell, LOUnixStat}
+};
+
+
 int LOopen(fname,mode)
      char *fname;
      int mode;
 {
     oid loOID;
-    LargeObject *lobjCookie;
-    LargeObjectDesc *lobjDesc;
+    struct varlena *lobjCookie;
+    char *lobjDesc;
     int fd;
+    int objtype;
     MemoryContext currentContext;
 
+#if FSDB
+    elog(NOTICE,"LOopen(%s,%d)",fname,mode);
+#endif
     loOID = FilenameToOID(fname);
 
     if (loOID == InvalidObjectId) { /* lookup failed */
 	if ((mode  & O_CREAT) != O_CREAT) {
+#if FSDB
+	elog(NOTICE,"LOopen lookup failed");
+#endif
 	    return -1;
 	} else {
-	    return LOcreat(fname,mode);
+	    /* Have to choose a particular type */
+	    objtype = Unix;
+	    return LOcreat(fname,mode,objtype);
 	}
     }
     
-    lobjCookie = LOassocOIDandLargeObjDesc(loOID);
+    lobjCookie = LOassocOIDandLargeObjDesc(&objtype,loOID);
     
     if (lobjCookie == NULL) { /* lookup failed, perhaps a dir? */
+#if FSDB
+	elog(NOTICE,"LOopen assoc failed");
+#endif
 	return -1;
+    } else {
+#if FSDB
+	elog(NOTICE,"LOopen assoc succeeded");
+#endif
     }
   
     /* temp debug testing */
@@ -133,14 +199,22 @@ int LOopen(fname,mode)
 	    currentContext = MemoryContextSwitchTo(curPort->portalcxt);
 	}
 #endif
-	/* Kemnitz large objects */
-	lobjDesc = LOOpen(lobjCookie,mode);
-
+	switch (LOprocs[objtype].bigcookie) {
+	  case SMALL_INT:
+	    lobjDesc =
+	      LOprocs[objtype].LOopen((void *) *((int *)VARDATA(lobjCookie)),mode);
+	    break;
+	  case BIG: 
+	    lobjDesc = LOprocs[objtype].LOopen(lobjCookie,mode);
+	    break;
+	}
+	    
 	if (lobjDesc == NULL) {
 	    MemoryContextSwitchTo(currentContext);
+	    elog(NOTICE,"LOopen objtype specific open failed");
 	    return -1;
 	}
-	fd = NewLOfd(lobjDesc);
+	fd = NewLOfd(lobjDesc,objtype);
 	/* switch context back to orig. */
 	MemoryContextSwitchTo(currentContext);
     }
@@ -150,8 +224,11 @@ int LOopen(fname,mode)
 #define MAX_LOBJ_FDS 256
 char  *cookies[MAX_LOBJ_FDS];
 int lotype[MAX_LOBJ_FDS];
+
+#if deadcode
 static int current_objaddr[MAX_LOBJ_FDS]; /* current address in LO's terms */
 static int current_objoffset[MAX_LOBJ_FDS]; /* offset in LO's terms */
+#endif
 
 int LOclose(fd)
      int fd;
@@ -163,16 +240,20 @@ int LOclose(fd)
 	return -2;
     }
     if (cookies[fd] == NULL) {
-	elog(WARN,"LOclose(%d)",fd);
+	elog(WARN,"LOclose(%d) invalid file descriptor",fd);
 	return -3;
     }
+#if FSDB
+	elog(NOTICE,"LOclose(%d)",fd);
+#endif
     /* Kemnitz large objects */
     {
 	MemoryContext currentContext;
 	Assert(fscxt != NULL);
 	currentContext = MemoryContextSwitchTo((MemoryContext)fscxt);
 	
-	LOClose((LargeObjectDesc  *)cookies[fd]);
+	LOprocs[lotype[fd]].LOclose(cookies[fd]);
+
 	MemoryContextSwitchTo(currentContext);
     }
     DeleteLOfd(fd);
@@ -183,17 +264,25 @@ int LOclose(fd)
  *  The mere complexity of the read/write functions dictate that they
  *  shouldn't work, or be entirely understandable.
  */
+/*
+ *  We assume the large object supports byte oriented reads and seeks so
+ *  that our work is easier.
+ */
 struct varlena * LOread(fd,len)
      int fd;
      int len;
 {
+#if 0
     char buf[LARGE_OBJECT_BLOCK];
+#endif
     struct varlena *retval;
     int bytestoread;
     int totalread = 0;
 #define min(x,y)  ( (x) < (y) ? (x) : (y) )
 
     retval = (struct varlena *)palloc(sizeof(int32) + len);
+    totalread = LOprocs[lotype[fd]].LOread(cookies[fd],VARDATA(retval),len);
+#if deadcode
     bytestoread = len;
 #if 0
     elog(NOTICE,"LORead: bytestoread %d current_offset %d current_block %d",
@@ -255,7 +344,7 @@ struct varlena * LOread(fd,len)
 	    LOBlockSeek((LargeObjectDesc *)cookies[fd],current_objaddr[fd],SEEK_SET);
 	}
     }
-
+#endif
     VARSIZE(retval) = totalread;
     return retval;
 }
@@ -267,13 +356,19 @@ int LOwrite(fd,wbuf)
 {
     int totalwritten = 0;
     int bytestowrite;
-    char buf[LARGE_OBJECT_BLOCK];
-
-    bytestowrite = VARSIZE(wbuf);
-
 #if 0
+    char buf[LARGE_OBJECT_BLOCK];
+#endif
+
+#if FSDB
     elog(NOTICE,"LOwrite(%d,%x) bytes %d",fd,wbuf,VARSIZE(wbuf));
 #endif
+    bytestowrite = VARSIZE(wbuf);
+    totalwritten = LOprocs[lotype[fd]].LOwrite(cookies[fd],VARDATA(wbuf),
+					       bytestowrite);
+    return totalwritten;
+
+#if deadcode
 
     /*
      * Logically, we can write on byte boundaries, but we actually write
@@ -325,21 +420,22 @@ int LOwrite(fd,wbuf)
 	if (current_objoffset[fd] != 0) /* back up over incomplete blocks */
 	  LOBlockSeek((LargeObjectDesc *)cookies[fd],current_objaddr[fd],SEEK_SET);
     }
-
     return totalwritten;
+#endif
 }
 
 int LOlseek(fd,offset,whence)
      int fd,offset,whence;
 {
+    int pos;
+
     if (fd >= MAX_LOBJ_FDS) {
 	elog(WARN,"LOSeek(%d) out of range",fd);
 	return -2;
     }
-    if (cookies[fd] == NULL) {
-	elog(WARN,"LOSeek(%d) cookie is NULL",fd);
-	return -3;
-    }
+    return LOprocs[lotype[fd]].LOseek(cookies[fd],offset,whence);
+
+#if deadcode
     /* Kemnitz large objects */
     current_objaddr[fd] =
       LOBlockSeek((LargeObjectDesc *)cookies[fd],offset/LARGE_OBJECT_BLOCK,whence);
@@ -349,16 +445,21 @@ int LOlseek(fd,offset,whence)
 	 current_objaddr[fd],current_objoffset[fd]);
 #endif
     return current_objaddr[fd]*LARGE_OBJECT_BLOCK + current_objoffset[fd];
+#endif
 }
 
-int LOcreat(path,mode)
+int LOcreat(path,mode,objtype)
      char *path;
      int mode;
+     int objtype;
 {
-    LargeObjectDesc *lobjDesc;
+    char *lobjDesc;
     int fd;
-
-    elog(NOTICE,"LOCreat(%s,%d)",path,mode);
+    /* prevent garbage code */
+    if (objtype != Inversion && objtype != Unix) objtype = Unix;
+#if FSDB
+    elog(NOTICE,"LOCreat(%s,%d,%d)",path,mode,objtype);
+#endif
     {
 	MemoryContext currentContext;
 	if (fscxt == NULL) {
@@ -373,14 +474,14 @@ int LOcreat(path,mode)
 	    currentContext = MemoryContextSwitchTo(curPort->portalcxt);
 	}
 #endif
-	/* Kemnitz large objects */
-	lobjDesc = LOCreate(path,mode);
+
+	lobjDesc = LOprocs[objtype].LOcreate(path,mode);
 
 	if (lobjDesc == NULL) {
 	    MemoryContextSwitchTo(currentContext);
 	    return -1;
 	}
-	fd = NewLOfd(lobjDesc);
+	fd = NewLOfd(lobjDesc,objtype);
 	/* switch context back to orig. */
 	MemoryContextSwitchTo(currentContext);
     }
@@ -398,7 +499,10 @@ int LOtell(fd)
 	elog(WARN,"LOtell(%d) cookie is NULL",fd);
 	return -3;
     }
+    return LOprocs[lotype[fd]].LOtell(cookies[fd]);
+#if deadcode
     return current_objaddr[fd]*LARGE_OBJECT_BLOCK + current_objoffset[fd];
+#endif 
 }
 
 int LOftruncate()
@@ -412,32 +516,40 @@ struct varlena *LOstat(path)
     struct pgstat *st;
     unsigned int nblocks, byte_offset;
     int len;
+    int pathOID;
     len = sizeof(struct pgstat);
     ret = (struct varlena *) palloc(len+sizeof(int32));
     VARSIZE(ret) = len;
     st = (struct pgstat *)VARDATA(ret);
     bzero(st,len);	/* default values of 0 */
 
-    if (!LOisdir(path)) {
-	/* kemnitz large objects */
+    pathOID =  FilenameToOID(path);
+    st->st_ino = pathOID;
+
+    if (!LOisdir(path) && pathOID != InvalidObjectId) {
 	int fd = LOopen(path,O_RDONLY);
-	LOUnixStat(cookies[fd],st);
-/*	elog(NOTICE,"LOstat: fd %d file %s size %d blocks %d",fd,
-	     path,st->st_size,st->st_blocks);*/
+	LOprocs[lotype[fd]].LOunixstat(cookies[fd],st);
+#if FSDB
+	elog(NOTICE,"LOstat: fd %d file %s size %d",fd,
+	     path,st->st_size);
+#endif
+#if 0
 	LOStat((LargeObjectDesc *)cookies[fd],&nblocks,&byte_offset);
+#endif
 	LOclose(fd);
 /*	st->st_size = nblocks*LARGE_OBJECT_BLOCK + byte_offset;
 	st->st_mode = S_IFREG;
 	st->st_mode |= S_IRUSR|S_IWUSR|S_IXUSR|
 	  S_IRGRP|S_IWGRP|S_IXGRP|
 	  S_IROTH|S_IWOTH|S_IXOTH;*/
-    } else if (FilenameToOID(path) != InvalidObjectId) {
+    } else if (pathOID != InvalidObjectId) { /* isdir */
 	st->st_mode = S_IFDIR;
 	/* our directoiries don't exist in the filesystem, so give them
 	   artificial permissions */
-	st->st_mode |= S_IRUSR|S_IWUSR|S_IXUSR|
-	  S_IRGRP|S_IWGRP|S_IXGRP|
-	    S_IROTH|S_IWOTH|S_IXOTH;
+	st->st_uid = getuid(); /* fake uid */
+	st->st_mode |= S_IRUSR|S_IWUSR|S_IXUSR;
+/*	  S_IRGRP|S_IWGRP|S_IXGRP|
+	    S_IROTH|S_IWOTH|S_IXOTH;*/
     } else {
 	VARSIZE(ret) = 5;
     }
@@ -471,8 +583,10 @@ int LOrmdir(path)
       return -1;
     else if (!LOisemptydir(path))
       return -1;
-    else
+    else {
       LOunlink(path);
+      return 0;
+    }
 }
 
 int LOunlink(path)
@@ -480,7 +594,10 @@ int LOunlink(path)
 {
     /* remove large object descriptor */
     /* remove naming entry */
-    LOunlinkOID(path);
+    oid deloid = LOunlinkOID(path);
+    if (deloid != InvalidObjectId) { /* remove associated file if any */
+	LOunassocOID(deloid);
+    }
 }
 
 /*
@@ -488,15 +605,19 @@ int LOunlink(path)
  * Support routines for this file
  * ------------------------------------
  */
-int NewLOfd(lobjCookie)
-     LargeObjectDesc *lobjCookie;
+int NewLOfd(lobjCookie,objtype)
+     char *lobjCookie;
+     int objtype;
 {
     int i;
     for (i = 0; i < MAX_LOBJ_FDS; i++) {
 	if (cookies[i] == NULL) {
-	    cookies[i] = (char *)lobjCookie;
+	    cookies[i] = lobjCookie;
+	    lotype[i] =  objtype;
+#if deadcode
 	    current_objaddr[i] = 0;
 	    current_objoffset[i] = 0;
+#endif
 	    return i;
 	}
     }
