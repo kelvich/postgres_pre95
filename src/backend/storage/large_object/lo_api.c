d9f8b14d02d700c7257360f85dc166413e339ffe
mao <mao>
1992-02-28 02:30:18 +0000

    interface changes to support inversion

--- src/backend/storage/large_object/lo_api.c
+++ src/backend/storage/large_object/lo_api.c
@@ -12,10 +12,14 @@
 
 #include <sys/file.h>
 #include "tmp/c.h"
-#include "utils/large_object.h"
+#include "tmp/libpq-fs.h"
+#include "access/relscan.h"
+#include "access/tupdesc.h"
 #include "catalog/pg_naming.h"
 #include "catalog/pg_lobj.h"
-#include "tmp/libpq-fs.h"
+#include "storage/itemptr.h"
+#include "utils/rel.h"
+#include "utils/large_object.h"
 #include "utils/log.h"
 
 LargeObject *NewLargeObject();
@@ -47,10 +51,7 @@ int open_mode;
     LargeObject *newobj;
     int fd;
     int obj_len, filename_len;
-
-#if FSDB
-    elog(NOTICE,"LOGregCreate(%s,%d)",path,open_mode);
-#endif
+    oid oidf;
 
     file_oid = newoid();
     sprintf(filename, "LO%d", file_oid);
@@ -62,20 +63,16 @@ int open_mode;
     newobj = (LargeObject *) NewLargeObject(filename, PURE_FILE);
 
     retval->object = newobj;
-    retval->fd = fd;
+    retval->ofs.u_fs.fd = fd;
 
     /* Log this instance of large object into directory table. */
-    {
-      oid oidf;
-      if ((oidf = FilenameToOID(path)) == InvalidObjectId) { /* new file */
-        oidf = LOcreatOID(path,0); /* enter it in system relation */
-        /* enter cookie into table */
-        if (LOputOIDandLargeObjDesc(oidf, Unix, (struct varlena *)newobj) < 0) {
-#if FSDB
-          elog(NOTICE,"LOputOIDandLargeObjDesc failed");
-#endif
-	  }
-      }
+    if ((oidf = FilenameToOID(path)) == InvalidObjectId) {
+
+	/* enter it in system relation */
+	oidf = LOcreatOID(path,0);
+
+	/* enter cookie into table */
+	(void) LOputOIDandLargeObjDesc(oidf, Unix, (struct varlena *) newobj);
     }
 
     return(retval);
@@ -83,10 +80,8 @@ int open_mode;
 
 LargeObjectDesc *
 LOOpen(object, open_mode)
-
-LargeObject *object;
-int open_mode;
-
+    LargeObject *object;
+    int open_mode;
 {
     LargeObjectDesc *retval;
     int fd;
@@ -100,7 +95,7 @@ int open_mode;
 
     retval = (LargeObjectDesc *) palloc(sizeof(LargeObjectDesc));
 
-    retval->fd = fd;
+    retval->ofs.u_fs.fd = fd;
     retval->object = object;
     return(retval);
 }
@@ -113,10 +108,8 @@ int open_mode;
 
 void
 LOStat(obj_desc, nblocks, byte_offset)
-
-LargeObjectDesc *obj_desc;
-unsigned int *nblocks, *byte_offset;
-
+    LargeObjectDesc *obj_desc;
+    unsigned int *nblocks, *byte_offset;
 {
     unsigned long nbytes;
     unsigned long pos, len;
@@ -127,15 +120,15 @@ unsigned int *nblocks, *byte_offset;
 
     /* see where we are now */
 
-    pos = FileTell(obj_desc->fd);
+    pos = FileTell(obj_desc->ofs.u_fs.fd);
 
     /* do a seek to find number of bytes */
 
-    len = FileSeek(obj_desc->fd, 0L, L_XTND);
+    len = FileSeek(obj_desc->ofs.u_fs.fd, 0L, L_XTND);
 
     /* seek back to original position */
 
-    FileSeek(obj_desc->fd, pos, L_SET);
+    FileSeek(obj_desc->ofs.u_fs.fd, pos, L_SET);
 
     *nblocks = (len < 0) ? 0 : len / LARGE_OBJECT_BLOCK;
     *byte_offset = len % LARGE_OBJECT_BLOCK;
@@ -147,11 +140,9 @@ unsigned int *nblocks, *byte_offset;
 
 int
 LOBlockRead(obj_desc, buf, nblocks)
-
-LargeObjectDesc *obj_desc;
-char *buf;
-unsigned long nblocks;
-
+    LargeObjectDesc *obj_desc;
+    char *buf;
+    unsigned long nblocks;
 {
     unsigned long nbytes, bytes_read;
 
@@ -160,75 +151,9 @@ unsigned long nblocks;
     Assert(obj_desc->object->lo_storage_type == PURE_FILE);
 
     nbytes = LARGE_OBJECT_BLOCK * nblocks;
-    bytes_read = FileRead(obj_desc->fd, buf, nbytes);
-    /* bogus because this assumes that all but one block is read. */
-/*
-    if (bytes_read == 0) return(0);
+    bytes_read = FileRead(obj_desc->ofs.u_fs.fd, buf, nbytes);
 
-    if (bytes_read % LARGE_OBJECT_BLOCK != 0)
-        return(bytes_read % LARGE_OBJECT_BLOCK);
-    else
-        return(LARGE_OBJECT_BLOCK);
-*/
     return bytes_read;
-}
-
-/*
- * here, bytes_at_end allows an incomplete block to be written.
- * This should ONLY be used to end a file, since we only allow block-oriented
- * seeking.  IF bytes_in
- */
-
-int
-LOBlockWrite(obj_desc, buf, n_whole_blocks, bytes_at_end)
-
-LargeObjectDesc *obj_desc;
-char *buf;
-unsigned long n_whole_blocks, bytes_at_end;
-
-{
-    unsigned long totalbytes;
-    int ret;
-
-    Assert(PointerIsValid(obj_desc));
-    Assert(PointerIsValid(obj_desc->object));
-    Assert(obj_desc->object->lo_storage_type == PURE_FILE);
-
-    totalbytes = LARGE_OBJECT_BLOCK * n_whole_blocks + bytes_at_end;
-
-    ret = FileWrite(obj_desc->fd, buf, totalbytes);
-    if (ret < 0) {
-#if FSDB
-	perror ("NOTICE: write");
-#endif
-        return ret;
-    } else {
-	return ret;
-    }
-}
-
-/*
- * Just like seek, although "offset" is the block offset, and the return
- * value is a block number.
- */
-
-unsigned long
-LOBlockSeek(obj_desc, offset, whence)
-
-LargeObjectDesc *obj_desc;
-unsigned long offset;
-int whence;
-
-{
-    unsigned long retval;
-
-    Assert(PointerIsValid(obj_desc));
-    Assert(PointerIsValid(obj_desc->object));
-    Assert(obj_desc->object->lo_storage_type == PURE_FILE);
-
-    retval = FileSeek(obj_desc->fd, offset * LARGE_OBJECT_BLOCK, whence);
-
-    return(retval / LARGE_OBJECT_BLOCK);
 }
 
 /*
@@ -237,15 +162,13 @@ int whence;
 
 void
 LOClose(obj_desc)
-
-LargeObjectDesc *obj_desc;
-
+    LargeObjectDesc *obj_desc;
 {
     Assert(PointerIsValid(obj_desc));
     Assert(PointerIsValid(obj_desc->object));
     Assert(obj_desc->object->lo_storage_type == PURE_FILE);
 
-    FileClose(obj_desc->fd);
+    FileClose(obj_desc->ofs.u_fs.fd);
     pfree(obj_desc);
 }
 
@@ -256,9 +179,7 @@ LargeObjectDesc *obj_desc;
 
 void
 LODestroy(object)
-
-LargeObject *object;
-
+    LargeObject *object;
 {
     Assert(PointerIsValid(object));
     Assert(object->lo_storage_type == PURE_FILE);
@@ -274,9 +195,7 @@ LargeObject *object;
 
 void
 LODestroyRef(object)
-
-LargeObject *object;
-
+    LargeObject *object;
 {
     Assert(PointerIsValid(object));
     Assert(object->lo_storage_type == PURE_FILE);
@@ -292,9 +211,7 @@ LargeObject *object;
 
 LargeObject *
 LODescToObject(obj_desc)
-
-LargeObjectDesc *obj_desc;
-
+    LargeObjectDesc *obj_desc;
 {
     LargeObject *retval;
 
@@ -311,9 +228,8 @@ LargeObjectDesc *obj_desc;
  */
 int
 LOUnixStat(obj_desc, stbuf)
-
-LargeObjectDesc *obj_desc;
-struct pgstat *stbuf;
+    LargeObjectDesc *obj_desc;
+    struct pgstat *stbuf;
 {
     int ret;
 
@@ -322,12 +238,7 @@ struct pgstat *stbuf;
     Assert(obj_desc->object->lo_storage_type == PURE_FILE);
     Assert(stbuf != NULL);
 
-    ret = FileStat(obj_desc->fd,stbuf);
-
-#if FSDB
-    elog(NOTICE,"LOUnixStat: fd %d, ret %d, size %d",obj_desc->fd,
-         ret, stbuf->st_size);
-#endif
+    ret = FileStat(obj_desc->ofs.u_fs.fd,stbuf);
 
     return ret;
 }
@@ -346,7 +257,7 @@ LOSeek(obj_desc,offset,whence)
     Assert(PointerIsValid(obj_desc->object));
     Assert(obj_desc->object->lo_storage_type == PURE_FILE);
 
-    ret = FileSeek(obj_desc->fd,offset,whence);
+    ret = FileSeek(obj_desc->ofs.u_fs.fd,offset,whence);
 
     return ret;
 }
@@ -364,7 +275,7 @@ LOTell(obj_desc)
     Assert(PointerIsValid(obj_desc->object));
     Assert(obj_desc->object->lo_storage_type == PURE_FILE);
 
-    ret = FileTell(obj_desc->fd);
+    ret = FileTell(obj_desc->ofs.u_fs.fd);
 
     return ret;
 }
@@ -384,10 +295,11 @@ int LORead(obj_desc,buf,n)
     Assert(obj_desc->object->lo_storage_type == PURE_FILE);
     Assert(buf != NULL);
 
-    ret = FileRead(obj_desc->fd,buf,n);
+    ret = FileRead(obj_desc->ofs.u_fs.fd,buf,n);
 
     return ret;
 }
+
 /* byte oriented write.
  * maybe do partial block writes
  */
@@ -403,8 +315,7 @@ int LOWrite (obj_desc,buf,n)
     Assert(obj_desc->object->lo_storage_type == PURE_FILE);
     Assert(buf != NULL);
 
-    ret = FileWrite(obj_desc->fd,buf,n);
+    ret = FileWrite(obj_desc->ofs.u_fs.fd,buf,n);
 
     return ret;
 }
-
