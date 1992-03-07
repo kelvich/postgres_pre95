b7410962cbf65bb55011224bdbaa5098ccbe6d4e
hong <hong>
1992-03-07 22:31:45 +0000

    fixed a prototyping mismatch

--- src/backend/tcop/pquery.h
+++ src/backend/tcop/pquery.h
@@ -4,7 +4,7 @@ List MakeQueryDesc ARGS((List operation , List parsetree , Plan plantree , List 
 List CreateQueryDesc ARGS((List parsetree , Plan plantree , CommandDest dest ));
 EState CreateExecutorState ARGS((void ));
 String CreateOperationTag ARGS((int operationType ));
-void ProcessPortal ARGS((String portalName , List parseTree , Plan plan , EState state , List attinfo , CommandDest dest ));
+void ProcessPortal ARGS((String portalName , int portalType, List parseTree , Plan plan , EState state , List attinfo , CommandDest dest ));
 void ProcessQueryDesc ARGS((List queryDesc ));
 void ProcessQuery ARGS((List parsertree , Plan plan , CommandDest dest ));
 void ParallelProcessQueries ARGS((List parsetree_list , List plan_list , CommandDest dest ));
