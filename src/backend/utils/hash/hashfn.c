b9429a89fe8547055c9c58f42c9559dd0070858b
mer <mer>
1992-03-17 15:44:09 +0000

    try to speed up the tag hash function with a switch

--- src/backend/utils/hash/hashfn.c
+++ src/backend/utils/hash/hashfn.c
@@ -36,36 +36,82 @@ tag_hash(key,keysize)
 int *	key;
 int	keysize;
 {
-	int h;
+	register int h = 0;
 
 	/*
-	 * Convert tag to integer;  Use four byte chunks so we can
-	 * go a little faster.
+	 * Convert tag to integer;  Use four byte chunks in a "jump table"
+	 * to go a little faster.  Currently the maximum keysize is 16
+	 * (mar 17 1992) I have put in cases for up to 24.  Bigger than
+	 * this will resort to the old behavior of the for loop. (see the
+	 * default case).
 	 */
-	for(h = 0;keysize > (sizeof(int)-1);keysize -= sizeof(int),key++) {
-	  h = h * PRIME1 ^ (*key);
-	}
-        /*
-         * now let's grab the last few bytes of the tag if the tag
-         * has (size % 4) != 0 (which it sometimes will on a sun3).
-         */
-        if (keysize) {
-          char *keytmp = (char *)key;
+	switch (keysize)
+	{
+	    case 6*sizeof(int):
+		h = h * PRIME1 ^ (*key);
+		key++;
+		keysize -= sizeof(int);
+		/* fall through */
 
-          switch (keysize) {
-          case 3:
-            h = h * PRIME1 ^ (*keytmp);
-            keytmp++;
-            /* fall through */
-          case 2:
-            h = h * PRIME1 ^ (*keytmp);
-            keytmp++;
-            /* fall through */
-          case 1:
-            h = h * PRIME1 ^ (*keytmp);
-            break;
-          }
-        }
+	    case 5*sizeof(int):
+		h = h * PRIME1 ^ (*key);
+		key++;
+		keysize -= sizeof(int);
+		/* fall through */
+
+	    case 4*sizeof(int):
+		h = h * PRIME1 ^ (*key);
+		key++;
+		keysize -= sizeof(int);
+		/* fall through */
+
+	    case 3*sizeof(int):
+		h = h * PRIME1 ^ (*key);
+		key++;
+		keysize -= sizeof(int);
+		/* fall through */
+
+	    case 2*sizeof(int):
+		h = h * PRIME1 ^ (*key);
+		key++;
+		keysize -= sizeof(int);
+		/* fall through */
+
+	    case sizeof(int):
+		h = h * PRIME1 ^ (*key);
+		key++;
+		keysize -= sizeof(int);
+		break;
+
+	    default:
+		for(; keysize > (sizeof(int)-1); keysize -= sizeof(int), key++)
+		    h = h * PRIME1 ^ (*key);
+		/*
+		 * now let's grab the last few bytes of the tag if the tag
+		 * has (size % 4) != 0 (which it sometimes will on a sun3).
+		 */
+		if (keysize)
+		{
+		    char *keytmp = (char *)key;
+
+		    switch (keysize)
+		    {
+			case 3:
+			    h = h * PRIME1 ^ (*keytmp);
+			    keytmp++;
+			    /* fall through */
+			case 2:
+			    h = h * PRIME1 ^ (*keytmp);
+			    keytmp++;
+			    /* fall through */
+			case 1:
+			    h = h * PRIME1 ^ (*keytmp);
+			    break;
+		    }
+		}
+		break;
+	}
+
 	h %= PRIME2;
 	return (h);
 }
