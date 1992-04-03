a3ccf14411f82b89250749d03483b912861825fd
mer <mer>
1992-04-03 01:08:12 +0000

    Used to write varlena size twice and so needed to skip past one of the size values to get at the true varlena struct.  I have fixed this double write and now varlena's can be treated as plain non-byvalue attributes.

--- src/backend/access/tupmacs.h
+++ src/backend/access/tupmacs.h
@@ -10,11 +10,10 @@
 #define att_isnull(ATT, BITS) (!((BITS)[(ATT) >> 3] & (1 << ((ATT) & 0x07))))
 
 #define fetchatt(A, T) \
-((*(A))->attlen < 0 ? (char *) (LONGALIGN((T) + sizeof(long))) : \
  ((*(A))->attbyval ? \
   ((*(A))->attlen > sizeof(short) ? (char *) *(long *) (T) : \
    ((*(A))->attlen < sizeof(short) ? (char *) *(T) : \
-	(char *) * (short *) (T))) : (char *) (T)))
+	(char *) * (short *) (T))) : (char *) (T))
 	
 #endif
 
