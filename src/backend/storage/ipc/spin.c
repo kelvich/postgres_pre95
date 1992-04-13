3a6b023ccb3ab4da516c3880bfd34023c0581987
mer <mer>
1992-04-13 18:58:27 +0000

    initialize OidGenLockId

--- src/backend/storage/ipc/spin.c
+++ src/backend/storage/ipc/spin.c
@@ -54,6 +54,7 @@ IPCKey key;
   extern SPINLOCK LockMgrLock;
   extern SPINLOCK ProcStructLock;
   extern SPINLOCK SInvalLock;
+  extern SPINLOCK OidGenLockId;
 
 #ifdef SONY_JUKEBOX
   extern SPINLOCK SJCacheLock;
@@ -71,6 +72,7 @@ IPCKey key;
   LockMgrLock = (SPINLOCK) LOCKMGRLOCKID;
   ProcStructLock = (SPINLOCK) PROCSTRUCTLOCKID;
   SInvalLock = (SPINLOCK) SINVALLOCKID;
+  OidGenLockId = (SPINLOCK) OIDGENLOCKID;
 
 #ifdef SONY_JUKEBOX
   SJCacheLock = (SPINLOCK) SJCACHELOCKID;
