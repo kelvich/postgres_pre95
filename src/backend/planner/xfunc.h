ffe3f534deeea50d296611532081a35bc4afbe62
joey <joey>
1992-06-11 23:38:38 +0000

    prototype for xfunc_width()

--- src/backend/planner/xfunc.h
+++ src/backend/planner/xfunc.h
@@ -6,14 +6,10 @@
 extern int XfuncMode;  /* defined in tcop/postgres.c */
 
 /* defaults for function attributes used for expensive function calculations */
-#define ARCH_PCT 100
-#define DISK_PCT 100
 #define BYTE_PCT 100
-#define PERBYTE_CPU 10
-#define PERCALL_CPU 100
+#define PERBYTE_CPU 0
+#define PERCALL_CPU 0
 #define OUTIN_RATIO 1
-#define DISK_FRACTION 0
-#define ARCH_FRACTION 0
 #define DEFAULT_WIDTH 1
 
 /* function prototypes from planner/path/xfunc.c */
@@ -23,3 +19,4 @@ extern int xfunc_clause_compare ARGS((void *arg1 , void *arg2 ));
 extern double xfunc_expense ARGS((LispValue clause ));
 extern void xfunc_disjunct_sort ARGS((LispValue clause_list ));
 extern int xfunc_disjunct_compare ARGS((void *arg1 , void *arg2 ));
+extern int xfunc_width ARGS((LispValue clause));
