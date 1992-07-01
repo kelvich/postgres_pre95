e7f837d9a8e081cc5106c04abc4ffc8387b7429d
mer <mer>
1992-07-01 05:11:16 +0000

    add some fields to hold state for more general functions

--- src/backend/utils/fcache.h
+++ src/backend/utils/fcache.h
@@ -12,11 +12,17 @@
 
 typedef struct
 {
-	func_ptr func;
-	int typlen;
-	int nargs;
-	int typbyval;
-	ObjectId foid;
+	int      typlen;      /* length of the return type */
+	int      typbyval;    /* true if return type is pass by value */
+	func_ptr func;	      /* address of function to call (for c funcs) */
+	ObjectId foid;	      /* oid of the function in pg_proc */
+	ObjectId language;    /* oid of the language in pg_language */
+	int      nargs;	      /* number of arguments */
+	ObjectId *argOidVect; /* oids of all the arguments */
+	bool     *nullVect;   /* keep track of null arguments */
+	char     *src;	      /* source code of the function */
+	char     *bin;	      /* binary object code ?? */
+	char     *func_state; /* fuction_state struct for execution */
 }
 FunctionCache, *FunctionCachePtr;
 
