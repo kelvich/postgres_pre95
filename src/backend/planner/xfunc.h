a07095b8300e4ca823aa9eaddc734abce34871cb
joey <joey>
1992-07-12 11:01:14 +0000

    protos for new functions, and remove unnecessary externs

--- src/backend/planner/xfunc.h
+++ src/backend/planner/xfunc.h
@@ -23,20 +23,23 @@ extern int XfuncMode;  /* defined in tcop/postgres.c */
 #define VARLEN_DEFAULT 128;
 
 /* function prototypes from planner/path/xfunc.c */
-extern int xfunc_cinfo_compare ARGS((void *arg1 , void *arg2 ));
+int xfunc_cinfo_compare ARGS((void *arg1 , void *arg2 ));
 extern int xfunc_clause_compare ARGS((void *arg1 , void *arg2 ));
-extern double xfunc_expense ARGS((LispValue clause ));
-extern double xfunc_measure ARGS((LispValue clause));
+double xfunc_expense ARGS((LispValue clause ));
+double xfunc_func_expense ARGS((LispValue node, LispValue args));
+double xfunc_measure ARGS((LispValue clause));
 extern void xfunc_disjunct_sort ARGS((LispValue clause_list ));
-extern int xfunc_disjunct_compare ARGS((void *arg1 , void *arg2 ));
-extern int xfunc_width ARGS((LispValue clause));
+int xfunc_disjunct_compare ARGS((void *arg1 , void *arg2 ));
+int xfunc_width ARGS((LispValue clause));
 extern void xfunc_trypullup ARGS((Rel rel));
-extern int xfunc_shouldpull ARGS((Path childpath, JoinPath parentpath, CInfo *maxclausept, int whichchild));
-extern void xfunc_pullup ARGS((Path childpath, JoinPath parentpath, LispValue clause, int whichrel, int clausetype));
-extern void xfunc_fixvars ARGS((LispValue clause, Path path, int varno));
-extern CInfo xfunc_primary_join ARGS((LispValue joinclauselist));
+int xfunc_shouldpull ARGS((Path childpath, JoinPath parentpath, CInfo *maxclausept, int whichchild));
+void xfunc_pullup ARGS((Path childpath, JoinPath parentpath, LispValue clause, int whichrel, int clausetype));
+void xfunc_fixvars ARGS((LispValue clause, Path path, int varno));
+LispValue xfunc_primary_join ARGS((JoinPath pathnode));
 extern int xfunc_get_path_cost ARGS((Path pathnode));
-extern bool xfunc_copyrel ARGS((Rel from, Rel *to));
-extern int xfunc_tuple_width ARGS((Relation rd));
-extern int xfunc_total_path_cost ARGS((Path pathnode));
-extern double xfunc_cost_per_tuple ARGS((JoinPath joinnode, int whichrel));
+bool xfunc_copyrel ARGS((Rel from, Rel *to));
+LispValue xfunc_LispRemove ARGS((LispValue foo, List bar));
+int xfunc_func_width ARGS((regproc funcid, LispValue args));
+int xfunc_tuple_width ARGS((Relation rd));
+int xfunc_total_path_cost ARGS((Path pathnode));
+double xfunc_cost_per_tuple ARGS((JoinPath joinnode, int whichrel));
