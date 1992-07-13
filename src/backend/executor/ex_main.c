675f94e020e4060f9229ade2d383f27c958a1141
hong <hong>
1992-07-13 16:25:29 +0000

    to prevent ExecMain's from unpinning each other's buffer

--- src/backend/executor/ex_main.c
+++ src/backend/executor/ex_main.c
@@ -112,6 +112,7 @@ ExecMain(queryDesc, estate, feature)
     List 	result;
     CommandDest dest;
     void	(*destination)();
+    extern int	ExecMainLevel;  /* defined in storage/buffer/bufmgr.c */
     
     /* ----------------
      *	sanity checks
@@ -160,6 +161,20 @@ ExecMain(queryDesc, estate, feature)
 			  parseTree,
 			  plan,
 			  estate);
+	ExecMainLevel++; /* increment ExecMain nesting level */
+	if (ExecMainLevel > 1)
+	    /* if this is not the first time ExecMain is called
+	     * reset buffer refcount.  the current refcounts
+	     * are saved and will be restored at the end of
+	     * this ExecMain cycle.
+	     *
+	     * this makes sure that when ExecMain's are
+	     * called recursively as for tuple level rules
+	     * or postquel functions, the buffers pinned
+	     * by one ExecMain will not be unpinned by another
+	     * ExecMain.
+	     */
+	    BufferRefCountReset(get_es_refcount(estate));
 	break;
 		
     case EXEC_RUN:
@@ -176,6 +191,12 @@ ExecMain(queryDesc, estate, feature)
     case EXEC_END:
 	/* result is a tuple or LispNil */
 	result = EndPlan(plan, estate);
+	if (ExecMainLevel > 1)
+	    /* if this is not the first time ExecMain is called,
+	     * restore saved refcounts.
+	     */
+	    BufferRefCountRestore(get_es_refcount(estate));
+	ExecMainLevel--; /* decrement ExecMain nesting level */
 	break;
 
 	/* ----------------
