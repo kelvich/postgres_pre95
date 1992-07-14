7a3bc2864345fdd8f145f71a4f6e3de2215aed99
hong <hong>
1992-07-14 21:36:44 +0000

    use Cost instead of double

--- src/backend/planner/clausesel.h
+++ src/backend/planner/clausesel.h
@@ -1,8 +1,8 @@
 /* $Header$ */
 extern void set_clause_selectivities ARGS((LispValue clauseinfo_list, Cost new_selectivity));
-extern double product_selec ARGS((LispValue clauseinfo_list));
+extern Cost product_selec ARGS((LispValue clauseinfo_list));
 extern void set_rest_relselec ARGS((LispValue rel_list));
 extern void set_rest_selec ARGS((LispValue clauseinfo_list));
-extern double compute_clause_selec ARGS((LispValue clause, LispValue or_selectivities));
-extern double compute_selec ARGS((LispValue clauses, LispValue or_selectivities));
+extern Cost compute_clause_selec ARGS((LispValue clause, LispValue or_selectivities));
+extern Cost compute_selec ARGS((LispValue clauses, LispValue or_selectivities));
 extern LispValue translate_relid ARGS((LispValue relid));
--- src/backend/planner/orindxpath.h
+++ src/backend/planner/orindxpath.h
@@ -1,5 +1,5 @@
 /* $Header$ */
 
 extern LispValue create_or_index_paths ARGS((Rel rel, LispValue clauses));
-extern LispValue best_or_subclause_indices ARGS((Rel rel, LispValue subclauses, LispValue indices, LispValue examined_indexids, double subcost, LispValue selectivities));
+extern LispValue best_or_subclause_indices ARGS((Rel rel, LispValue subclauses, LispValue indices, LispValue examined_indexids, Cost subcost, LispValue selectivities));
 extern LispValue best_or_subclause_index ARGS((Rel rel, LispValue subclause, LispValue indices));
