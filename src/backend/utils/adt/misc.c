13df4835515a006f36181ab5685484c5e9c3eb37
mao <mao>
1992-07-29 16:00:32 +0000

    restore NullValue, NonNullValue

--- src/backend/utils/adt/misc.c
+++ src/backend/utils/adt/misc.c
@@ -2,6 +2,42 @@
 
 #include <sys/file.h>
 #include "tmp/c.h"
+#include "tmp/postgres.h"
+#include "tmp/datum.h"
+#include "catalog/pg_type.h"
+
+/*-------------------------------------------------------------------------
+ * Check if data is Null 
+ */
+
+
+bool NullValue(value, isNull)
+Datum value;
+Boolean *isNull;
+{
+	if (*isNull) {
+		*isNull = false;
+		return(true);
+	}
+	return(false);
+	
+}
+
+/*----------------------------------------------------------------------*
+ *     check if data is not Null                                        *
+ *--------------------------------------------------------------------- */
+
+bool NonNullValue(value, isNull)
+Datum value;
+Boolean *isNull;
+{
+	if (*isNull) {
+		*isNull = false;
+		return(false);
+	}
+	return(true);
+		
+}
 
 int32
 userfntest(i)
