88c4ab39f75869ac100e8e8da366b83761fb1d45
mao <mao>
1992-09-01 01:52:08 +0000

    now copy directory trees if requested to do so; add options to control this.

--- src/bin/fsutils/icopy/icopy.c
+++ src/bin/fsutils/icopy/icopy.c
@@ -4,7 +4,10 @@
  *	Icopy moves files between the Unix file system and Inversion.
  */
 
+#include <sys/types.h>
 #include <sys/file.h>
+#include <sys/stat.h>
+#include <sys/dir.h>
 #include <stdio.h>
 
 #include "tmp/c.h"
@@ -14,40 +17,65 @@
 
 RcsId("$Header$");
 
-#define	DIR_IN	0
-#define	DIR_OUT	1
-#define IBUFSIZ	8092
+#define	DIR_IN		0
+#define	DIR_OUT		1
+#define IBUFSIZ		8092
+#define	DEF_PORT	"4321"
 
 #ifndef TRUE
 #define	TRUE	1
 #define	FALSE	0
 #endif /* ndef TRUE */
 
+typedef struct SLIST {
+    char	s_id;
+    char	*s_name;
+} SLIST;
+
+SLIST SmgrList[] = {
+    { 'd',	"magnetic disk" },
+#ifdef SONY_JUKEBOX
+    { 's',	"sony jukebox" },
+#endif
+#ifdef MAIN_MEMORY
+    { 'r',	"volatile RAM" },
+#endif
+    { '\0',	(char *) NULL }
+};
+
+typedef struct COPYLIST {
+    char		*cl_src;
+    char		*cl_dest;
+    int			cl_isdir;
+    struct COPYLIST	*cl_next;
+} COPYLIST;
+
 char *ProgName;
 extern char *PQhost;
 extern char *PQport;
+int	CopyAll;		/* do dot files, too */
+int	Recurse;		/* traverse directory tree */
+int	Verbose;		/* report progress */
 
 /* routines declared here */
 extern char	*nextarg();
-extern void	icopy_in();
-extern void	icopy_out();
+extern int	icopy_in();
+extern int	icopy_out();
 extern void	isetup();
 extern void	ishutdown();
 extern void	usage();
+extern void	ibegin();
+extern void	icommit();
+extern int	docopy_in();
+extern int	docopy_out();
+extern COPYLIST *build_inlist();
+extern COPYLIST *build_outlist();
 
 /* routines declared elsewhere */
 extern char	*getenv();
 
-char *SmgrList[] = {
-    "magnetic disk",
-#ifdef SONY_JUKEBOX
-    "sony jukebox",
-#endif
-#ifdef MAIN_MEMORY
-    "main memory",
-#endif
-    (char *) NULL
-};
+extern char *optarg;
+extern int optind, opterr;
 
 main(argc, argv)
     int argc;
@@ -59,8 +87,10 @@ main(argc, argv)
     int smgrno;
     char *host, *port;
     char *srcfname, *destfname;
-    char *curarg;
+    int flag;
+    int status;
 
+    Verbose = CopyAll = Recurse = FALSE;
     host = port = dbname = smgr = srcfname = destfname = (char *) NULL;
     ProgName = *argv;
     smgrno = 0;
@@ -76,53 +106,47 @@ main(argc, argv)
     else
 	usage();
 
-    while (--argc) {
-	++argv;
-	if (**argv == '-') {
-	    curarg = *argv;
-	    switch (*++(*argv)) {
-	      case 'd':
-		dbname = nextarg(&argc, &argv);
-		break;
+    while ((flag = getopt(argc, argv, "ad:h:p:Rs:v")) != EOF) {
+	switch (flag) {
+	  case 'a':
+	    CopyAll = TRUE;
+	    break;
 
-	      case 'h':
-		host = nextarg(&argc, &argv);
-		break;
+	  case 'd':
+	    dbname = optarg;
+	    break;
 
-	      case 'p':
-		port = nextarg(&argc, &argv);
-		break;
+	  case 'h':
+	    host = optarg;
+	    break;
 
-	      case 's':
-		smgr = nextarg(&argc, &argv);
-		break;
+	  case 'p':
+	    port = optarg;
+	    break;
 
-	      /*
-	       *  This case handles "-" as a filename (meaning stdout
-	       *  or stdin).
-	       */
-	      case '\0':
-		if (srcfname == (char *) NULL)
-		    srcfname = curarg;
-		else if (destfname == (char *) NULL)
-		    destfname = curarg;
-		else
-		    usage();
-		break;
+	  case 'R':
+	    Recurse = TRUE;
+	    break;
 
-	      default:
-		usage();
-	    }
-	} else if (srcfname == (char *) NULL)
-	    srcfname = *argv;
-	else if (destfname == (char *) NULL)
-	    destfname = *argv;
-	else
+	  case 's':
+	    smgr = optarg;
+	    break;
+
+	  case 'v':
+	    Verbose = TRUE;
+	    break;
+
+	  default:
 	    usage();
+	    break;
+	}
     }
 
-    if (srcfname == (char *) NULL || destfname == (char *) NULL)
+    if (argc - optind != 2)
 	usage();
+
+    srcfname = argv[optind];
+    destfname = argv[optind + 1];
 
     if (host == (char *) NULL) {
 	if ((host = getenv("PGHOST")) == (char *) NULL) {
@@ -132,10 +156,8 @@ main(argc, argv)
     }
 
     if (port == (char *) NULL) {
-	if ((port = getenv("PGPORT")) == (char *) NULL) {
-	    fprintf(stderr, "no port specified and PGPORT undefined.\n");
-	    usage();
-	}
+	if ((port = getenv("PGPORT")) == (char *) NULL)
+	    port = DEF_PORT;
     }
 
     if (dbname == (char *) NULL)
@@ -148,17 +170,395 @@ main(argc, argv)
     isetup(host, port, dbname);
 
     if (dir == DIR_IN)
-	icopy_in(srcfname, destfname, smgrno);
+	status = icopy_in(srcfname, destfname, smgrno);
     else
-	icopy_out(srcfname, destfname, smgrno);
+	status = icopy_out(srcfname, destfname, smgrno);
 
     ishutdown();
 
-    exit (0);
+    exit (status);
 }
 
-void
+int
 icopy_in(srcfname, destfname, smgrno)
+    char *srcfname;
+    char *destfname;
+    int smgrno;
+{
+    char *tail;
+    char *newdest;
+    int src_is_dir, dest_is_dir;
+    COPYLIST *cplist;
+    int errs;
+    struct stat stbuf;
+    struct pgstat pgstbuf;
+
+    errs = 0;
+
+    /* eliminate trailing slashes */
+    tail = &srcfname[strlen(srcfname)-1];
+    while (tail > srcfname && *tail == '/')
+	*tail-- = '\0';
+
+    tail = &destfname[strlen(destfname)-1];
+    while (tail > destfname && *tail == '/')
+	*tail-- = '\0';
+
+    /* start a transaction */
+    ibegin();
+
+    /* verify that the source exists */
+    if (stat(srcfname, &stbuf) < 0) {
+	fprintf(stderr, "%s: ", ProgName);
+	perror(srcfname);
+	exit (1);
+    }
+
+    if ((stbuf.st_mode & S_IFMT) == S_IFDIR)
+	src_is_dir = TRUE;
+    else
+	src_is_dir = FALSE;
+
+    /* icopy of directories only makes sense if -R is supplied */
+    if (src_is_dir & !Recurse) {
+	fprintf(stderr, "%s: '%s' is a directory; use pmkdir or %s -R\n",
+		ProgName, srcfname, ProgName);
+	fflush(stderr);
+	exit (1);
+    }
+
+    /*
+     *  If the destination exists, then it must be a directory; we'll
+     *  copy the source into that directory.
+     */
+
+    dest_is_dir = FALSE;
+    if (p_stat(destfname, &pgstbuf) >= 0) {
+	if (!((pgstbuf.st_mode & S_IFMT) == S_IFDIR)) {
+	    fprintf(stderr, "%s: file '%s' already exists\n",
+		    ProgName, destfname);
+	    fflush(stderr);
+	    exit (1);
+	} else
+	    dest_is_dir = TRUE;
+    }
+
+    if (!dest_is_dir) {
+	/* the directory containing the destination must exist */
+	tail = rindex(destfname, '/');
+	if (tail == (char *) NULL) {
+	    fprintf(stderr, "%s: '%s' is not a fully-qualified pathname\n",
+		    ProgName, destfname);
+	    fflush(stderr);
+	    exit (1);
+	}
+
+	/* if the target isn't /, stat it */
+	if (tail != destfname) {
+	    *tail = '\0';
+	    if (p_stat(destfname, &pgstbuf) < 0) {
+		fprintf(stderr, "%s: %s: no such file or directory\n",
+			ProgName, destfname);
+		fflush(stderr);
+		exit (1);
+	    }
+	    *tail = '/';
+	}
+    }
+
+    /* done with verifying validity of arguments */
+    icommit();
+
+    /* set up destination file name correctly if supplied name is a dir */
+    if (dest_is_dir) {
+	tail = rindex(srcfname, '/');
+	if (tail == (char *) NULL)
+	    tail = srcfname;
+	else
+	    tail++;
+
+	if (strcmp(destfname, "/") == 0) {
+	    newdest = (char *) palloc(strlen(tail) + 2);
+	    sprintf(newdest, "/%s", tail);
+	} else {
+	    newdest = palloc(strlen(destfname) + strlen(tail) + 2);
+	    sprintf(newdest, "%s/%s", destfname, tail);
+	}
+
+	destfname = newdest;
+    }
+
+    cplist = build_inlist(srcfname, destfname);
+
+    /* do it */
+    while (cplist != (COPYLIST *) NULL) {
+	if (cplist->cl_isdir) {
+	    if (Verbose)
+		printf("mkdir %s\n", cplist->cl_dest);
+	    ibegin();
+	    if (p_mkdir(cplist->cl_dest) < 0)
+		errs++;
+	    icommit();
+	} else {
+	    if (Verbose)
+		printf("%s -> %s\n", cplist->cl_src, cplist->cl_dest);
+
+	    if (docopy_in(cplist->cl_src, cplist->cl_dest, smgrno) < 0)
+		errs++;
+	}
+
+	cplist = cplist->cl_next;
+    }
+
+    return (errs);
+}
+
+int
+icopy_out(srcfname, destfname, smgrno)
+    char *srcfname;
+    char *destfname;
+    int smgrno;
+{
+    char *tail;
+    char *newdest;
+    int src_is_dir, dest_is_dir;
+    COPYLIST *cplist;
+    int errs;
+    struct stat stbuf;
+    struct pgstat pgstbuf;
+
+    errs = 0;
+
+    /* eliminate trailing slashes */
+    tail = &srcfname[strlen(srcfname)-1];
+    while (tail > srcfname && *tail == '/')
+	*tail-- = '\0';
+
+    tail = &destfname[strlen(destfname)-1];
+    while (tail > destfname && *tail == '/')
+	*tail-- = '\0';
+
+    /* start a transaction */
+    ibegin();
+
+    /* verify that the source exists */
+    if (p_stat(srcfname, &pgstbuf) < 0) {
+	fprintf(stderr, "%s: ", ProgName);
+	perror(srcfname);
+	exit (1);
+    }
+
+    if ((pgstbuf.st_mode & S_IFMT) == S_IFDIR)
+	src_is_dir = TRUE;
+    else
+	src_is_dir = FALSE;
+
+    /* icopy of directories only makes sense if -R is supplied */
+    if (src_is_dir & !Recurse) {
+	fprintf(stderr, "%s: '%s' is a directory; use pmkdir or %s -R\n",
+		ProgName, srcfname, ProgName);
+	fflush(stderr);
+	exit (1);
+    }
+
+    /*
+     *  If the destination exists, then it must be a directory; we'll
+     *  copy the source into that directory.
+     */
+
+    dest_is_dir = FALSE;
+    if (stat(destfname, &stbuf) >= 0) {
+	if (!((stbuf.st_mode & S_IFMT) == S_IFDIR)) {
+	    fprintf(stderr, "%s: file '%s' already exists\n",
+		    ProgName, destfname);
+	    fflush(stderr);
+	    exit (1);
+	} else
+	    dest_is_dir = TRUE;
+    }
+
+    if (!dest_is_dir) {
+	/* the directory containing the destination must exist */
+	tail = rindex(destfname, '/');
+	if (tail != (char *) NULL) {
+
+	    /* if the target isn't /, stat it */
+	    if (tail != destfname) {
+		*tail = '\0';
+		if (stat(destfname, &stbuf) < 0) {
+		    fprintf(stderr, "%s: ", ProgName);
+		    perror(destfname);
+		    fflush(stderr);
+		    exit (1);
+		}
+		*tail = '/';
+	    }
+	}
+    }
+
+    /* done with verifying validity of arguments */
+    icommit();
+
+    /* set up destination file name correctly if supplied name is a dir */
+    if (dest_is_dir) {
+	tail = rindex(srcfname, '/');
+	if (tail == (char *) NULL)
+	    tail = srcfname;
+	else
+	    tail++;
+
+	if (strcmp(destfname, "/") == 0) {
+	    newdest = (char *) palloc(strlen(tail) + 2);
+	    sprintf(newdest, "/%s", tail);
+	} else {
+	    newdest = palloc(strlen(destfname) + strlen(tail) + 2);
+	    sprintf(newdest, "%s/%s", destfname, tail);
+	}
+
+	destfname = newdest;
+    }
+
+    ibegin();
+    cplist = build_outlist(srcfname, destfname);
+    icommit();
+
+    /* do it */
+    while (cplist != (COPYLIST *) NULL) {
+	if (cplist->cl_isdir) {
+	    if (Verbose)
+		printf("mkdir %s\n", cplist->cl_dest);
+	    if (mkdir(cplist->cl_dest) < 0)
+		errs++;
+	} else {
+	    if (Verbose)
+		printf("%s -> %s\n", cplist->cl_src, cplist->cl_dest);
+
+	    if (docopy_out(cplist->cl_src, cplist->cl_dest, smgrno) < 0)
+		errs++;
+	}
+
+	cplist = cplist->cl_next;
+    }
+
+    return (errs);
+}
+
+COPYLIST *
+build_inlist(srcfname, destfname)
+    char *srcfname;
+    char *destfname;
+{
+    COPYLIST *head, *tail;
+    DIR *dirp;
+    struct dirent *entry;
+    char *newsrc, *newdest;
+    struct stat stbuf;
+
+    if (stat(srcfname, &stbuf) < 0) {
+	fprintf(stderr, "%s: ", ProgName);
+	perror(srcfname);
+	exit (1);
+    }
+
+    /* don't copy symbolic links */
+    if ((stbuf.st_mode & S_IFMT) == S_IFLNK) {
+	fprintf(stderr, "%s: %s not copied (symbolic link)\n",
+		ProgName, srcfname);
+	fflush(stderr);
+	return ((COPYLIST *) NULL);
+    }
+
+    head = (COPYLIST *) palloc(sizeof(COPYLIST));
+    head->cl_src = srcfname;
+    head->cl_dest = destfname;
+    head->cl_next = (COPYLIST *) NULL;
+
+    head->cl_isdir = ((stbuf.st_mode & S_IFMT) == S_IFDIR);
+    if (head->cl_isdir && Recurse) {
+	if ((dirp = opendir(srcfname)) == (DIR *) NULL) {
+	    fprintf(stderr, "%s: ", ProgName);
+	    perror(srcfname);
+	    fflush(stderr);
+	    return (head);
+	}
+
+	tail = head;
+
+	while ((entry = readdir(dirp)) != (struct dirent *) NULL) {
+	    if (entry->d_name[0] == '.' && !CopyAll)
+		continue;
+	    if (strcmp(entry->d_name, ".") == 0
+		|| strcmp(entry->d_name, "..") == 0)
+		continue;
+	    newsrc = (char *) palloc(strlen(srcfname) + entry->d_namlen + 2);
+	    sprintf(newsrc, "%s/%s", srcfname, entry->d_name);
+	    newdest = (char *) palloc(strlen(destfname) + entry->d_namlen + 2);
+	    sprintf(newdest, "%s/%s", destfname, entry->d_name);
+	    tail->cl_next = build_inlist(newsrc, newdest);
+	    while (tail->cl_next != (COPYLIST *) NULL)
+		tail = tail->cl_next;
+	}
+	(void) closedir(dirp);
+    }
+
+    return (head);
+}
+
+COPYLIST *
+build_outlist(srcfname, destfname)
+    char *srcfname;
+    char *destfname;
+{
+    COPYLIST *head, *tail;
+    PDIR *dirp;
+    struct pgdirent *entry;
+    char *newsrc, *newdest;
+    struct pgstat stbuf;
+
+    if (p_stat(srcfname, &stbuf) < 0) {
+	fprintf(stderr, "%s: ", ProgName);
+	perror(srcfname);
+	exit (1);
+    }
+
+    head = (COPYLIST *) palloc(sizeof(COPYLIST));
+    head->cl_src = srcfname;
+    head->cl_dest = destfname;
+    head->cl_next = (COPYLIST *) NULL;
+
+    head->cl_isdir = ((stbuf.st_mode & S_IFMT) == S_IFDIR);
+    if (head->cl_isdir && Recurse) {
+	if ((dirp = p_opendir(srcfname)) == (PDIR *) NULL) {
+	    fprintf(stderr, "%s: ", ProgName);
+	    perror(srcfname);
+	    fflush(stderr);
+	    return (head);
+	}
+
+	tail = head;
+
+	while ((entry = p_readdir(dirp)) != (struct pgdirent *) NULL) {
+	    if (entry->d_name[0] == '.' && !CopyAll)
+		continue;
+	    if (strcmp(entry->d_name, ".") == 0
+		|| strcmp(entry->d_name, "..") == 0)
+		continue;
+	    newsrc = (char *) palloc(strlen(srcfname) + entry->d_namlen + 2);
+	    sprintf(newsrc, "%s/%s", srcfname, entry->d_name);
+	    newdest = (char *) palloc(strlen(destfname) + entry->d_namlen + 2);
+	    sprintf(newdest, "%s/%s", destfname, entry->d_name);
+	    tail->cl_next = build_outlist(newsrc, newdest);
+	    while (tail->cl_next != (COPYLIST *) NULL)
+		tail = tail->cl_next;
+	}
+	(void) p_closedir(dirp);
+    }
+
+    return (head);
+}
+
+int
+docopy_in(srcfname, destfname, smgrno)
     char *srcfname;
     char *destfname;
     int smgrno;
@@ -168,27 +568,22 @@ icopy_in(srcfname, destfname, smgrno)
     int nread, nwrite, totread, nbytes;
     int done;
 
-    /* copy in cannot go to stdout */
-    if (strcmp(destfname, "-") == 0)
-	usage();
-
-    if (strcmp(srcfname, "-") == 0)
-	srcfd = fileno(stdin);
-    else if ((srcfd = open(srcfname, O_RDONLY, 0600)) < 0) {
-	perror(ProgName);
-	exit (1);
+    if ((srcfd = open(srcfname, O_RDONLY, 0600)) < 0) {
+	fprintf(stderr, "%s: ", ProgName);
+	perror(srcfname);
+	return (-1);
     }
 
     if ((destfd = p_creat(destfname, INV_WRITE|smgrno, Inversion)) < 0) {
 	fprintf(stderr, "Cannot create Inversion file %s\n", destfname);
 	fflush(stderr);
-	exit (1);
+	return (-1);
     }
 
     if ((buf = (char *) malloc(IBUFSIZ)) == (char *) NULL) {
 	fprintf(stderr, "cannot allocate %d bytes for copy buffer\n", IBUFSIZ);
 	fflush(stderr);
-	exit (1);
+	return (-1);
     }
 
     done = FALSE;
@@ -200,8 +595,9 @@ icopy_in(srcfname, destfname, smgrno)
 	/* read one inversion file system buffer's worth of data */
 	while (nbytes > 0) {
 	    if ((nread = read(srcfd, lbuf, nbytes)) < 0) {
-		perror(ProgName);
-		exit (1);
+		fprintf(stderr, "%s: ", ProgName);
+		perror(read);
+		return (-1);
 	    } else if (nread == 0) {
 		nbytes = 0;
 		done = TRUE;
@@ -219,7 +615,7 @@ icopy_in(srcfname, destfname, smgrno)
 		fprintf(stderr, "%s: write failed to Inversion file %s\n",
 			ProgName, destfname);
 		fflush(stderr);
-		exit (1);
+		return (-1);
 	    }
 	    totread -= nwrite;
 	    lbuf += nwrite;
@@ -228,10 +624,12 @@ icopy_in(srcfname, destfname, smgrno)
 
     (void) close(srcfd);
     (void) p_close(destfd);
+
+    return (0);
 }
 
-void
-icopy_out(srcfname, destfname, smgrno)
+int
+docopy_out(srcfname, destfname, smgrno)
     char *srcfname;
     char *destfname;
     int smgrno;
@@ -249,14 +647,13 @@ icopy_out(srcfname, destfname, smgrno)
 	fprintf(stderr, "%s: cannot open Inversion file %s\n",
 		ProgName, srcfname);
 	fflush(stderr);
-	exit (1);
+	return (-1);
     }
 
-    if (strcmp(destfname, "-") == 0)
-	destfd = fileno(stdout);
-    else if ((destfd = open(destfname, O_WRONLY|O_CREAT|O_TRUNC, 0600)) < 0) {
-	perror(ProgName);
-	exit (1);
+    if ((destfd = open(destfname, O_WRONLY|O_CREAT|O_TRUNC, 0600)) < 0) {
+	fprintf(stderr, "%s: ", ProgName);
+	perror(destfname);
+	return (-1);
     }
 
     if ((buf = (char *) malloc(IBUFSIZ)) == (char *) NULL) {
@@ -277,7 +674,7 @@ icopy_out(srcfname, destfname, smgrno)
 		fprintf(stderr, "%s: read failed from inversion file %s\n",
 			ProgName, srcfname);
 		fflush(stderr);
-		exit (1);
+		return (-1);
 	    } else if (nread == 0) {
 		nbytes = 0;
 		done = TRUE;
@@ -292,8 +689,9 @@ icopy_out(srcfname, destfname, smgrno)
 	lbuf = buf;
 	while (totread > 0) {
 	    if ((nwrite = write(destfd, lbuf, totread)) <= 0) {
-		perror(ProgName);
-		exit (1);
+		fprintf(stderr, "%s: ", ProgName);
+		perror(write);
+		return (-1);
 	    }
 	    totread -= nwrite;
 	    lbuf += nwrite;
@@ -310,11 +708,15 @@ isetup(host, port, dbname)
     char *port;
     char *dbname;
 {
-    char *res;
-
     PQport = port;
     PQhost = host;
     PQsetdb(dbname);
+}
+
+void
+ibegin()
+{
+    char *res;
 
     res = PQexec("begin");
     if (*res == 'E') {
@@ -326,6 +728,12 @@ isetup(host, port, dbname)
 
 void
 ishutdown()
+{
+    PQfinish();
+}
+
+void
+icommit()
 {
     char *res;
 
@@ -373,8 +781,8 @@ smgrlookup(smgr)
 {
     int i;
 
-    for (i = 0; SmgrList[i] != (char *) NULL; i++)
-	if (strcmp(smgr, SmgrList[i]) == 0)
+    for (i = 0; SmgrList[i].s_id != '\0'; i++)
+	if (*smgr == SmgrList[i].s_id)
 	    return (i);
 
     return (-1);
@@ -386,18 +794,14 @@ usage()
     int i;
 
     fprintf(stderr, "usage: %s {in|out} ", ProgName);
-    fprintf(stderr, "-h host -p port -d db -s smgr from_file to_file\n\n");
+    fprintf(stderr, "[-h host] [-p port] -d db -s smgr [-R] [-v] [-a] from to\n\n");
 
     fprintf(stderr, "    'in' copies to Inversion, and 'out' copies from Inversion.\n\n");
 
     fprintf(stderr, "    smgr may be:\n");
-    for (i = 0; SmgrList[i] != (char *) NULL; i++)
-	fprintf(stderr, "\t%s\n", SmgrList[i]);
-    fprintf(stderr, "    smgr is ignored for %s out.\n", ProgName);
-
-    fprintf(stderr, "\n    from_file or to_file may be '-', for stdin and stdout, respectively.\n");
-    fprintf(stderr, "    stdin may not be used with %s out.\n", ProgName);
-    fprintf(stderr, "    stdout may not be used with %s in.\n", ProgName);
+    for (i = 0; SmgrList[i].s_id != '\0'; i++)
+	fprintf(stderr, "\t%c   (%s)\n", SmgrList[i].s_id, SmgrList[i].s_name);
+    fprintf(stderr, "    smgr is not required for %s out, and will be ignored if it is supplied.\n", ProgName);
 
     fflush(stderr);
     exit (1);
