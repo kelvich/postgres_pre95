1d99973230500509d8ede89c1af82a3d04e6017a
mao <mao>
1992-10-14 05:59:42 +0000

    new mag disk storage manager.  very large relations are now split across up to 8192 2GByte files, assuming space exists; this means that the max relation size on the mag disk storage manager is now 17.59TBytes.

--- src/backend/storage/smgr/md.c
+++ src/backend/storage/smgr/md.c
@@ -13,23 +13,46 @@
 #include "storage/smgr.h"
 #include "storage/block.h"
 #include "storage/fd.h"
+#include "utils/mcxt.h"
 #include "utils/rel.h"
+#include "utils/log.h"
 
 RcsId("$Header$");
 
+#undef DIAGNOSTIC
+
 /*
- *  Need to keep track of open file descriptors under the magnetic disk
- *  storage manager.
+ *  The magnetic disk storage manager keeps track of open file descriptors
+ *  in its own descriptor pool.  This happens for two reasons.  First, at
+ *  transaction boundaries, we walk the list of descriptors and flush
+ *  anything that we've dirtied in the current transaction.  Second, we
+ *  have to support relations of > 4GBytes.  In order to do this, we break
+ *  relations up into chunks of < 2GBytes and store one chunk in each of
+ *  several files that represent the relation.
  */
 
-static int	Nfds = 100;	/* must be same as in storage/file/fd.c */
-static char	*Md_fdvec;
+typedef struct _MdfdVec {
+    int			mdfd_vfd;		/* fd number in vfd pool */
+    uint16		mdfd_flags;		/* clean, dirty */
+    int			mdfd_lstbcnt;		/* most recent block count */
+    struct _MdfdVec	*mdfd_chain;		/* for large relations */
+} MdfdVec;
+
+static int	Nfds = 100;
+static MdfdVec	*Md_fdvec = (MdfdVec *) NULL;
+static int	CurFd = 0;
+MemoryContext	MdCxt;
 
 /* globals defined elsewhere */
 extern char		*DataDir;
 
-#define MDFD_CLEAN	(char) 0
-#define MDFD_DIRTY	(char) 1
+#define MDFD_DIRTY	(uint16) 0x01
+
+#define	RELSEG_SIZE	2147483688	/* 2 ** 31 */
+
+/* routines declared here */
+extern MdfdVec	*_mdfd_openseg();
+extern MdfdVec	*_mdfd_getseg();
 
 /*
  *  mdinit() -- Initialize private state for magnetic disk storage manager.
@@ -46,10 +69,20 @@ extern char		*DataDir;
 int
 mdinit()
 {
-    if ((Md_fdvec = (char *) malloc(Nfds)) == (char *) NULL)
+    MemoryContext oldcxt;
+
+    MdCxt = (MemoryContext) CreateGlobalMemory("MdSmgr");
+    if (MdCxt == (MemoryContext) NULL)
 	return (SM_FAIL);
 
-    (void) bzero(Md_fdvec, Nfds);
+    oldcxt = MemoryContextSwitchTo(MdCxt);
+    Md_fdvec = (MdfdVec *) palloc(Nfds * sizeof(MdfdVec));
+    (void) MemoryContextSwitchTo(oldcxt);
+
+    if (Md_fdvec == (MdfdVec *) NULL)
+	return (SM_FAIL);
+
+    (void) bzero(Md_fdvec, Nfds * sizeof(MdfdVec));
 
     return (SM_SUCCESS);
 }
@@ -58,7 +91,7 @@ int
 mdcreate(reln)
     Relation reln;
 {
-    int fd;
+    int fd, vfd;
     int tmp;
     char *path;
     extern char *relpath();
@@ -84,11 +117,19 @@ mdcreate(reln)
 	}
     }
 
-    if (fd >= Nfds)
-	if (_fdvec_ext(fd) == SM_FAIL)
+    if (CurFd >= Nfds) {
+	if (_fdvec_ext() == SM_FAIL)
 	    return (-1);
+    }
 
-    return (fd);
+    Md_fdvec[CurFd].mdfd_vfd = fd;
+    Md_fdvec[CurFd].mdfd_flags = (uint16) 0;
+    Md_fdvec[CurFd].mdfd_chain = (MdfdVec *) NULL;
+    Md_fdvec[CurFd].mdfd_lstbcnt = 0;
+
+    vfd = CurFd++;
+
+    return (vfd);
 }
 
 /*
@@ -99,7 +140,39 @@ int
 mdunlink(reln)
     Relation reln;
 {
-    FileUnlink(RelationGetFile(reln));
+    int fd;
+    int i;
+    MdfdVec *v, *ov;
+    MemoryContext oldcxt;
+    char fname[20];	/* XXX should have NAMESIZE defined */
+    char tname[20];
+
+    bzero(fname, 20);
+    strncpy(fname, RelationGetRelationName(reln), 16);
+
+    if (FileNameUnlink(fname) < 0)
+	return (SM_FAIL);
+
+    /* unlink all the overflow files for large relations */
+    for (i = 1; ; i++) {
+	sprintf(tname, "%s.%d", fname, i);
+	if (FileNameUnlink(tname) < 0)
+	    break;
+    }
+
+    /* finally, clean out the mdfd vector */
+    fd = RelationGetFile(reln);
+    Md_fdvec[fd].mdfd_flags = (uint16) 0;
+
+    oldcxt = MemoryContextSwitchTo(MdCxt);
+    for (v = &Md_fdvec[fd]; v != (MdfdVec *) NULL; ) {
+	ov = v;
+	v = v->mdfd_chain;
+	if (ov != &Md_fdvec[fd])
+	    pfree(ov);
+    }
+    Md_fdvec[fd].mdfd_chain = (MdfdVec *) NULL;
+    (void) MemoryContextSwitchTo(oldcxt);
 
     return (SM_SUCCESS);
 }
@@ -117,27 +190,36 @@ mdextend(reln, buffer)
     char *buffer;
 {
     long pos;
-    File vfd;
+    int nblocks;
+    MdfdVec *v;
 
-    vfd = RelationGetFile(reln);
+    nblocks = mdnblocks(reln);
+    v = _mdfd_getseg(reln, nblocks, O_CREAT);
 
-    if ((pos = FileSeek(vfd, 0L, L_XTND)) < 0)
+    if ((pos = FileSeek(v->mdfd_vfd, 0L, L_XTND)) < 0)
 	return (SM_FAIL);
 
-    if (FileWrite(vfd, buffer, BLCKSZ) != BLCKSZ)
+    if (FileWrite(v->mdfd_vfd, buffer, BLCKSZ) != BLCKSZ)
 	return (SM_FAIL);
 
     /* remember that we did a write, so we can sync at xact commit */
-    Md_fdvec[vfd] = MDFD_DIRTY;
+    v->mdfd_flags |= MDFD_DIRTY;
+
+    /* try to keep the last block count current, though it's just a hint */
+    if ((v->mdfd_lstbcnt = (++nblocks % RELSEG_SIZE)) == 0)
+	v->mdfd_lstbcnt = RELSEG_SIZE;
+
+#ifdef DIAGNOSTIC
+    if (FileGetNumberOfBlocks(v->mdfd_vfd) > RELSEG_SIZE
+	|| v->mdfd_lstbcnt > RELSEG_SIZE)
+	elog(FATAL, "segment too big!");
+#endif
 
     return (SM_SUCCESS);
 }
 
 /*
  *  mdopen() -- Open the specified relation.
- *
- *	The magnetic disk storage manager uses one file descriptor per open
- *	relation.  This routine returns the open file descriptor.
  */
 
 int
@@ -146,7 +228,13 @@ mdopen(reln)
 {
     char *path;
     int fd;
+    int vfd;
     extern char *relpath();
+
+    if (CurFd >= Nfds) {
+	if (_fdvec_ext() == SM_FAIL)
+	    return (-1);
+    }
 
     path = relpath(&(reln->rd_rel->relname.data[0]));
 
@@ -156,11 +244,19 @@ mdopen(reln)
     if (fd < 0)
 	fd = FileNameOpenFile(path, O_RDWR|O_CREAT|O_EXCL, 0600);
 
-    if (fd >= Nfds)
-	if (_fdvec_ext(fd) == SM_FAIL)
-	    return (-1);
+    Md_fdvec[CurFd].mdfd_vfd = fd;
+    Md_fdvec[CurFd].mdfd_flags = (uint16) 0;
+    Md_fdvec[CurFd].mdfd_chain = (MdfdVec *) NULL;
+    Md_fdvec[CurFd].mdfd_lstbcnt = FileGetNumberOfBlocks(fd);
 
-    return (fd);
+#ifdef DIAGNOSTIC
+    if (Md_fdvec[CurFd].mdfd_lstbcnt > RELSEG_SIZE)
+	elog(FATAL, "segment too big on relopen!");
+#endif
+
+    vfd = CurFd++;
+
+    return (vfd);
 }
 
 /*
@@ -173,23 +269,29 @@ int
 mdclose(reln)
     Relation reln;
 {
-    File vfd;
+    int fd;
+    MdfdVec *v;
     int status;
 
-    /* maybe it's already closed... */
-    if ((vfd = RelationGetFile(reln)) < 0)
-	return (SM_SUCCESS);
+    fd = RelationGetFile(reln);
 
-    /*
-     *  Need to do it here.  We sync the file descriptor so that we don't
-     *  need to reopen it at transaction commit to force changes to disk.
-     */
+    for (v = &Md_fdvec[fd]; v != (MdfdVec *) NULL; v = v->mdfd_chain) {
 
-    FileSync(vfd);
-    FileClose(vfd);
+	/* may be closed already */
+	if (v->mdfd_vfd < 0)
+	    continue;
 
-    /* mark this file descriptor as clean in our private table */
-    Md_fdvec[vfd] = MDFD_CLEAN;
+	/*
+	 *  We sync the file descriptor so that we don't need to reopen it at
+	 *  transaction commit to force changes to disk.
+	 */
+
+	FileSync(v->mdfd_vfd);
+	FileClose(v->mdfd_vfd);
+
+	/* mark this file descriptor as clean in our private table */
+	v->mdfd_flags &= ~MDFD_DIRTY;
+    }
 
     return (SM_SUCCESS);
 }
@@ -207,23 +309,25 @@ mdread(reln, blocknum, buffer)
     char *buffer;
 {
     int status;
-    File vfd;
     long seekpos;
     int nbytes;
+    MdfdVec *v;
 
-    if ((vfd = RelationGetFile(reln)) < 0) {
-	if ((vfd = mdopen(reln)) < 0)
-	    return (SM_FAIL);
-	reln->rd_fd = vfd;
-    }
+    v = _mdfd_getseg(reln, blocknum, 0);
 
-    seekpos = (long) (BLCKSZ * blocknum);
-    if (FileSeek(vfd, seekpos, L_SET) != seekpos) {
+    seekpos = (long) (BLCKSZ * (blocknum % RELSEG_SIZE));
+
+#ifdef DIAGNOSTIC
+    if (seekpos >= BLCKSZ * RELSEG_SIZE)
+	elog(FATAL, "seekpos too big!");
+#endif
+
+    if (FileSeek(v->mdfd_vfd, seekpos, L_SET) != seekpos) {
 	return (SM_FAIL);
     }
 
     status = SM_SUCCESS;
-    if ((nbytes = FileRead(vfd, buffer, BLCKSZ)) != BLCKSZ) {
+    if ((nbytes = FileRead(v->mdfd_vfd, buffer, BLCKSZ)) != BLCKSZ) {
 	if (nbytes == 0) {
 	    (void) bzero(buffer, BLCKSZ);
 	} else {
@@ -247,43 +351,26 @@ mdwrite(reln, blocknum, buffer)
     char *buffer;
 {
     int status;
-    bool found;
-    File vfd;
     long seekpos;
+    MdfdVec *v;
 
-    found = true;
-    if ((vfd = RelationGetFile(reln)) < 0) {
-	found = false;
-	if ((vfd = mdopen(reln)) < 0)
-	    return (SM_FAIL);
-    }
+    v = _mdfd_getseg(reln, blocknum, 0);
 
-    seekpos = (long) (BLCKSZ * blocknum);
-    if (FileSeek(vfd, seekpos, L_SET) != seekpos) {
-	if (!found)
-	    (void) FileClose(vfd);
+    seekpos = (long) (BLCKSZ * (blocknum % RELSEG_SIZE));
+#ifdef DIAGNOSTIC
+    if (seekpos >= BLCKSZ * RELSEG_SIZE)
+	elog(FATAL, "seekpos too big!");
+#endif
 
+    if (FileSeek(v->mdfd_vfd, seekpos, L_SET) != seekpos) {
 	return (SM_FAIL);
     }
 
     status = SM_SUCCESS;
-    if (FileWrite(vfd, buffer, BLCKSZ) != BLCKSZ)
+    if (FileWrite(v->mdfd_vfd, buffer, BLCKSZ) != BLCKSZ)
 	status = SM_FAIL;
 
-    /*
-     *  If we opened this file descriptor especially to write this one block,
-     *  force the change to disk and mark the descriptor as clean.  If we
-     *  had an open descriptor for the file already, mark it as dirty so
-     *  we'll flush it at commit time.
-     */
-
-    if (!found) {
-	FileSync(vfd);
-	FileClose(vfd);
-	Md_fdvec[vfd] = MDFD_CLEAN;
-    } else {
-	Md_fdvec[vfd] = MDFD_DIRTY;
-    }
+    v->mdfd_flags |= MDFD_DIRTY;
 
     return (status);
 }
@@ -302,29 +389,25 @@ mdflush(reln, blocknum, buffer)
     char *buffer;
 {
     int status;
-    bool found;
-    File vfd;
     long seekpos;
+    MdfdVec *v;
 
-    found = true;
-    if ((vfd = RelationGetFile(reln)) < 0) {
-	found = false;
-	if ((vfd = mdopen(reln)) < 0)
-	    return (SM_FAIL);
-    }
+    v = _mdfd_getseg(reln, blocknum, 0);
 
-    seekpos = (long) (BLCKSZ * blocknum);
-    if (FileSeek(vfd, seekpos, L_SET) != seekpos) {
-	if (!found)
-	    (void) FileClose(vfd);
+    seekpos = (long) (BLCKSZ * (blocknum % RELSEG_SIZE));
+#ifdef DIAGNOSTIC
+    if (seekpos >= BLCKSZ * RELSEG_SIZE)
+	elog(FATAL, "seekpos too big!");
+#endif
 
+    if (FileSeek(v->mdfd_vfd, seekpos, L_SET) != seekpos) {
 	return (SM_FAIL);
     }
 
-    status = SM_SUCCESS;
-
     /* write and sync the block */
-    if (FileWrite(vfd, buffer, BLCKSZ) != BLCKSZ || FileSync(vfd) < 0)
+    status = SM_SUCCESS;
+    if (FileWrite(v->mdfd_vfd, buffer, BLCKSZ) != BLCKSZ
+	|| FileSync(v->mdfd_vfd) < 0)
 	status = SM_FAIL;
 
     /*
@@ -333,10 +416,7 @@ mdflush(reln, blocknum, buffer)
      *  don't sync it again unnecessarily at transaction commit.
      */
 
-    Md_fdvec[vfd] = MDFD_CLEAN;
-
-    if (!found)
-	FileClose(vfd);
+    v->mdfd_flags &= ~MDFD_DIRTY;
 
     return (status);
 }
@@ -359,25 +439,39 @@ mdblindwrt(dbstr, relstr, dbid, relid, blkno, buffer)
     char *buffer;
 {
     int fd;
+    int segno;
     long seekpos;
     int status;
     char *path;
+    int nchars;
+
+    /* be sure we have enough space for the '.segno', if any */
+    segno = blkno / RELSEG_SIZE;
+    if (segno > 0)
+	nchars = 10;
+    else
+	nchars = 0;
 
     /* construct the path to the file and open it */
     if (dbid == (OID) 0) {
-	path = (char *) palloc(strlen(DataDir) + sizeof(NameData) + 2);
-	sprintf(path, "%s/%s", DataDir, relstr);
+	path = (char *) palloc(strlen(DataDir) + sizeof(NameData) + 2 + nchars);
+	if (segno == 0)
+	    sprintf(path, "%s/%s", DataDir, relstr);
+	else
+	    sprintf(path, "%s/%s.%d", DataDir, relstr, segno);
     } else {
-	path = (char *) palloc(strlen(DataDir) + 2 * sizeof(NameData) + 3);
-	path = (char *) palloc(strlen(DataDir) + strlen("/base/") + 2 * sizeof(NameData) + 2);
-	sprintf(path, "%s/base/%s/%s", DataDir, dbstr, relstr);
+	path = (char *) palloc(strlen(DataDir) + strlen("/base/") + 2 * sizeof(NameData) + 2 + nchars);
+	if (segno == 0)
+	    sprintf(path, "%s/base/%s/%s", DataDir, dbstr, relstr);
+	else
+	    sprintf(path, "%s/base/%s/%s.%d", DataDir, dbstr, relstr, segno);
     }
 
     if ((fd = open(path, O_RDWR, 0600)) < 0)
 	return (SM_FAIL);
 
     /* seek to the right spot */
-    seekpos = (long) (BLCKSZ * blkno);
+    seekpos = (long) (BLCKSZ * (blkno % RELSEG_SIZE));
     if (lseek(fd, seekpos, L_SET) != seekpos) {
 	(void) close(fd);
 	return (SM_FAIL);
@@ -407,11 +501,39 @@ int
 mdnblocks(reln)
     Relation reln;
 {
-    File vfd;
+    int fd;
+    MdfdVec *v;
+    int nblocks;
+    int segno;
 
-    vfd = RelationGetFile(reln);
+    fd = RelationGetFile(reln);
+    v = &Md_fdvec[fd];
 
-    return (FileGetNumberOfBlocks(vfd));
+#ifdef DIAGNOSTIC
+    if (FileGetNumberOfBlocks(v->mdfd_vfd) > RELSEG_SIZE)
+	elog(FATAL, "segment too big in getseg!");
+#endif
+
+    segno = 0;
+    for (;;) {
+	if (v->mdfd_lstbcnt == RELSEG_SIZE
+	    || (nblocks = FileGetNumberOfBlocks(v->mdfd_vfd)) == RELSEG_SIZE) {
+
+	    v->mdfd_lstbcnt = RELSEG_SIZE;
+	    segno++;
+
+	    if (v->mdfd_chain == (MdfdVec *) NULL) {
+		v->mdfd_chain = _mdfd_openseg(reln, segno, O_CREAT);
+		if (v->mdfd_chain == (MdfdVec *) NULL)
+		    elog(WARN, "cannot count blocks for %.16s -- open failed",
+				RelationGetRelationName(reln));
+	    }
+
+	    v = v->mdfd_chain;
+	} else {
+	    return ((segno * RELSEG_SIZE) + nblocks);
+	}
+    }
 }
 
 /*
@@ -429,13 +551,16 @@ int
 mdcommit()
 {
     int i;
+    MdfdVec *v;
 
-    for (i = 0; i < Nfds; i++) {
-	if (Md_fdvec[i] == MDFD_DIRTY) {
-	    if (FileSync(i) < 0)
-		return (SM_FAIL);
+    for (i = 0; i < CurFd; i++) {
+	for (v = &Md_fdvec[i]; v != (MdfdVec *) NULL; v = v->mdfd_chain) {
+	    if (v->mdfd_flags & MDFD_DIRTY) {
+		if (FileSync(v->mdfd_vfd) < 0)
+		    return (SM_FAIL);
 
-	    Md_fdvec[i] = MDFD_CLEAN;
+		v->mdfd_flags &= ~MDFD_DIRTY;
+	    }
 	}
     }
 
@@ -452,7 +577,15 @@ mdcommit()
 int
 mdabort()
 {
-    bzero(Md_fdvec, Nfds * sizeof(int));
+    int i;
+    MdfdVec *v;
+
+    for (i = 0; i < CurFd; i++) {
+	for (v = &Md_fdvec[i]; v != (MdfdVec *) NULL; v = v->mdfd_chain) {
+	    v->mdfd_flags &= ~MDFD_DIRTY;
+	}
+    }
+
     return (SM_SUCCESS);
 }
 
@@ -464,27 +597,113 @@ mdabort()
  */
 
 int
-_fdvec_ext(fd)
-    int fd;
+_fdvec_ext()
 {
-    char *nvec;
+    MdfdVec *nvec;
     int orig;
+    MemoryContext oldcxt;
 
-    orig = Nfds;
+    Nfds *= 2;
 
-    do
-	Nfds *= 2;
-    while (Nfds <= fd);
+    oldcxt = MemoryContextSwitchTo(MdCxt);
 
-    if ((nvec = (char *) malloc(Nfds)) == (char *) NULL)
-	return (SM_FAIL);
+    nvec = (MdfdVec *) palloc(Nfds * sizeof(MdfdVec));
+    (void) bzero(nvec, Nfds * sizeof(MdfdVec));
+    (void) bcopy((char *) Md_fdvec, nvec, (Nfds / 2) * sizeof(MdfdVec));
+    pfree(Md_fdvec);
 
-    (void) bzero(nvec, Nfds);
-    (void) bcopy(Md_fdvec, nvec, orig);
-
-    free(Md_fdvec);
+    (void) MemoryContextSwitchTo(oldcxt);
 
     Md_fdvec = nvec;
 
     return (SM_SUCCESS);
+}
+
+MdfdVec *
+_mdfd_openseg(reln, segno, oflags)
+    Relation reln;
+    int segno;
+    int oflags;
+{
+    MemoryContext oldcxt;
+    MdfdVec *v;
+    int fd;
+    int status;
+    bool dofree;
+    char *path, *fullpath;
+
+    /* be sure we have enough space for the '.segno', if any */
+    path = relpath(RelationGetRelationName(reln));
+
+    dofree = false;
+    if (segno > 0) {
+	dofree = true;
+	fullpath = (char *) palloc(strlen(path) + 10);
+	sprintf(fullpath, "%s.%d", path, segno);
+    } else
+	fullpath = path;
+
+    /* open the file */
+    fd = PathNameOpenFile(fullpath, O_RDWR|oflags, 0600);
+
+    if (dofree)
+	pfree(fullpath);
+
+    if (fd < 0)
+	return ((MdfdVec *) NULL);
+
+    /* allocate an mdfdvec entry for it */
+    oldcxt = MemoryContextSwitchTo(MdCxt);
+    v = (MdfdVec *) palloc(sizeof(MdfdVec));
+    (void) MemoryContextSwitchTo(oldcxt);
+
+    /* fill the entry */
+    v->mdfd_vfd = fd;
+    v->mdfd_flags = (uint16) 0;
+    v->mdfd_chain = (MdfdVec *) NULL;
+    v->mdfd_lstbcnt = FileGetNumberOfBlocks(fd);
+
+#ifdef DIAGNOSTIC
+    if (v->mdfd_lstbcnt > RELSEG_SIZE)
+	elog(FATAL, "segment too big on open!");
+#endif
+
+    /* all done */
+    return (v);
+}
+
+MdfdVec *
+_mdfd_getseg(reln, blkno, oflag)
+    Relation reln;
+    int blkno;
+    int oflag;
+{
+    MdfdVec *v;
+    int segno;
+    int fd;
+    int i;
+
+    fd = RelationGetFile(reln);
+    if (fd < 0) {
+	if ((fd = mdopen(reln)) < 0)
+	    elog(WARN, "cannot open relation %.16s",
+			RelationGetRelationName(reln));
+	reln->rd_fd = fd;
+    }
+
+    for (v = &Md_fdvec[fd], segno = blkno / RELSEG_SIZE, i = 1;
+	 segno > 0;
+	 i++, segno--) {
+
+	if (v->mdfd_chain == (MdfdVec *) NULL) {
+	    v->mdfd_chain = _mdfd_openseg(reln, i, oflag);
+
+	    if (v->mdfd_chain == (MdfdVec *) NULL)
+		elog(WARN, "cannot open segment %d of relation %.16s",
+			    i, RelationGetRelationName(reln));
+	}
+	v = v->mdfd_chain;
+    }
+
+    return (v);
 }
