683acbe41114251c97ae8a14e3dd62138acbd1bc
mao <mao>
1992-11-03 23:25:38 +0000

    sunita's oid was wrong

--- src/backend/catalog/pg_user.h
+++ src/backend/catalog/pg_user.h
@@ -87,7 +87,7 @@ DATA(insert OID = 5059 ( caetta 5059 t t t t ));
 DATA(insert OID = 5209 ( joey 5209 t t t t ));
 DATA(insert OID = 5568 ( clarsen 5568 t t t t ));
 DATA(insert OID = 5443 ( jolly 5443 t t t t ));
-DATA(insert OID = 5443 ( sunita 6559 t t t t ));
+DATA(insert OID = 6559 ( sunita 6559 t t t t ));
 
 BKI_BEGIN
 #endif ALLOW_PG_GROUP
