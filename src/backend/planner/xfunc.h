cf13aff616c7ceb3a65bb35490fa7bf118c1b4a5
joey <joey>
1992-11-23 22:36:30 +0000

    added XFUNC_NOPM, XFUNC_NOPRUNE, macros get_grouprank and is_join, and protos for new functions in path/predmig.c

--- src/backend/planner/xfunc.h
+++ src/backend/planner/xfunc.h
@@ -6,10 +6,13 @@
 #define XFUNC_OFF -1
 #define XFUNC_NOR 2
 #define XFUNC_NOPULL 4
+#define XFUNC_NOPM 8
+#define XFUNC_NOPRUNE 16
 
 /* constants for local and join predicates */
 #define XFUNC_LOCPRD 1
 #define XFUNC_JOINPRD 2
+#define XFUNC_UNKNOWN 0
 
 extern int XfuncMode;  /* defined in tcop/postgres.c */
 
@@ -22,10 +25,17 @@ extern int XfuncMode;  /* defined in tcop/postgres.c */
 /* default width assumed for variable length attributes */
 #define VARLEN_DEFAULT 128;
 
+/* Macro to get group rank out of group cost and group sel */
+#define get_grouprank(a) ((get_groupsel(a) - 1) / get_groupcost(a))
+
+/* Macro to see if a path node is actually a Join */
+#define is_join(pathnode) (length(get_relids(get_parent(pathnode))) > 1 ? 1 : 0)
+
 /* function prototypes from planner/path/xfunc.c */
 int xfunc_cinfo_compare ARGS((void *arg1 , void *arg2 ));
 extern int xfunc_clause_compare ARGS((void *arg1 , void *arg2 ));
 Cost xfunc_expense ARGS((LispValue clause ));
+Cost xfunc_join_expense ARGS((JoinPath path));
 Cost xfunc_local_expense ARGS((LispValue clause ));
 Cost xfunc_func_expense ARGS((LispValue node, LispValue args));
 List xfunc_find_references ARGS((LispValue clause));
@@ -35,7 +45,7 @@ int xfunc_disjunct_compare ARGS((void *arg1 , void *arg2 ));
 int xfunc_width ARGS((LispValue clause));
 extern void xfunc_trypullup ARGS((Rel rel));
 int xfunc_shouldpull ARGS((Path childpath, JoinPath parentpath, CInfo *maxclausept));
-void xfunc_pullup ARGS((Path childpath, JoinPath parentpath, LispValue clause, int whichrel, int clausetype));
+CInfo xfunc_pullup ARGS((Path childpath, JoinPath parentpath, LispValue clause, int whichrel, int clausetype));
 void xfunc_fixvars ARGS((LispValue clause, Path path, int varno));
 LispValue xfunc_primary_join ARGS((JoinPath pathnode));
 extern Cost xfunc_get_path_cost ARGS((Path pathnode));
@@ -46,3 +56,22 @@ int xfunc_tuple_width ARGS((Relation rd));
 Cost xfunc_total_path_cost ARGS((Path pathnode));
 Cost xfunc_expense_per_tuple ARGS((JoinPath joinnode));
 Count xfunc_card_unreferenced ARGS((LispValue clause));
+
+
+/* protos from planner/path/predmig.c */
+extern bool xfunc_do_predmig ARGS((Path root));
+bool xfunc_predmig ARGS((JoinPath pathnode, Stream streamroot, Stream laststream));
+bool xfunc_series_llel ARGS((Stream stream));
+bool xfunc_llel_chains ARGS((Stream root, Stream bottom));
+bool xfunc_prdmig_pullup ARGS((Stream origstream, Stream pullme, JoinPath joinpath));
+void xfunc_form_groups ARGS((Stream root, Stream bottom));
+Stream xfunc_add_clauses ARGS((Stream current));
+void xfunc_setup_group ARGS((Stream node, Stream bottom));
+Stream xfunc_streaminsert ARGS((CInfo clauseinfo, Stream current, int clausetype));
+int xfunc_num_relids ARGS((Stream node));
+StreamPtr xfunc_get_downjoin ARGS((Stream node));
+StreamPtr xfunc_get_upjoin ARGS((Stream node));
+Stream xfunc_stream_qsort ARGS((Stream root, Stream bottom));
+int xfunc_stream_compare ARGS((void *arg1, void *arg2));
+bool xfunc_check_stream ARGS((Stream node));
+bool xfunc_in_stream ARGS((Stream node,stream));
