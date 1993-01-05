376c4cbb8bd81c583624a1f66060a063b05ea7fc
aoki <aoki>
1993-01-05 02:31:00 +0000

    removed stuff that is now passed through Makefile variables.

--- src/backend/libpq/auth.h
+++ src/backend/libpq/auth.h
@@ -1,4 +1,4 @@
-/* ----------------------------------------------------------------
+/*----------------------------------------------------------------
  *   FILE
  *	auth.h
  *	
@@ -9,7 +9,7 @@
  *
  *   IDENTIFICATION
  *	$Header$
- * ----------------------------------------------------------------
+ *----------------------------------------------------------------
  */
 
 #ifndef AuthIncluded		/* Include this file only once */
@@ -18,51 +18,35 @@
 #include "tmp/c.h"
 #include "tmp/pqcomm.h"
 
-/* ----------------------------------------------------------------
+/*----------------------------------------------------------------
  * Common routines and definitions
- * ----------------------------------------------------------------
+ *----------------------------------------------------------------
  */
 
 /* what we call "no authentication system" */
 #define	UNAUTHNAME		"unauth"
 
 /* what a frontend uses by default */
+#if !defined(KRB4) && !defined(KRB5)
 #define	DEFAULT_CLIENT_AUTHSVC	UNAUTHNAME
+#else /* KRB4 || KRB5 */
+#define	DEFAULT_CLIENT_AUTHSVC	"kerberos"
+#endif /* KRB4 || KRB5 */
 
+extern		fe_sendauth ARGS((MsgType msgtype,
+				  Port *port,
+				  char *hostname));
 extern char    *fe_getauthname ARGS(());
 extern void	fe_setauthsvc ARGS((char *name));
 extern MsgType	fe_getauthsvc ARGS(());
 
+extern		be_recvauth ARGS((Msgtype msgtype,
+				  Port *port,
+				  char *username));
 extern void	be_setauthsvc ARGS((char *name));
 extern int	be_getauthsvc ARGS(());
 
-extern		pg_sendauth ARGS((MsgType msgtype, Port *port,
-				  char *hostname));
-extern		pg_recvauth ARGS((Msgtype msgtype, Port *port,
-				  char *username));
-
-/* ----------------------------------------------------------------
- * MIT Kerberos authentication system, protocol version 4
- *	Note that the srvtab you use must be readable by a non-root
- *	user, i.e., "postgres".
- * ----------------------------------------------------------------
- */
-#ifdef KRB4
-#define	PG_KRB4_SERVICE	"postgres_dbms"
 #define	PG_KRB4_VERSION	"PGVER4.1"	/* at most KRB_SENDAUTH_VLEN chars */
-#define	PG_KRB4_SRVTAB	"/etc/srvtab"
-#endif /* KRB4 */
-
-/* ----------------------------------------------------------------
- * MIT Kerberos authentication system, protocol version 5
- *	Note that the srvtab you use must be readable by a non-root
- *	user, i.e., "postgres".
- * ----------------------------------------------------------------
- */
-#ifdef KRB5
-#define	PG_KRB5_SERVICE	"postgres_dbms"
-#define	PG_KRB5_VERSION	"PGVER5.1"	/* at most KRB_SENDAUTH_VLEN chars */
-#define	PG_KRB5_SRVTAB	"/etc/srvtab"
-#endif /* KRB5 */
+#define	PG_KRB5_VERSION	"PGVER5.1"
 
 #endif /* !defined(AuthIncluded) */
