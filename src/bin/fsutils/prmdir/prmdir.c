e62e4f923c9fe8a01023c99360e59b339b949905
clarsen <clarsen>
1993-01-24 04:59:53 +0000

    add -H,-D,-P flags BSD net-2 release

--- src/bin/fsutils/prmdir/prmdir.c
+++ src/bin/fsutils/prmdir/prmdir.c
@@ -19,36 +19,65 @@ char copyright[] =
 
 extern char *getenv();
 
+void usage();
+
 main(argc,argv)
 	int argc;
 	char **argv;
 {
 	int errors = 0;
+	int dflag, ch;
 	char *dbname;
+	extern int optind;
+	extern char *optarg;
+	extern char *PQhost, *PQport;
 
-	if ((dbname = getenv("DATABASE")) == (char *) NULL) {
-	    fprintf(stderr, "no database specified in env var DATABASE\n");
-	    fflush(stderr);
-	    exit (1);
+	dflag = 0;
+	while ((ch = getopt(argc, argv, "H:P:D:")) != EOF)
+	  switch(ch) {
+	    case 'H':
+	      PQhost = optarg;
+	      break;
+	    case 'P':
+	      PQport = optarg;
+	      break;
+	    case 'D':
+	      PQsetdb(optarg);
+	      dflag = 1;
+	      break;
+	    case '?':
+	    default:
+	      usage();
+	  }
+
+	if (!dflag) {
+	    if ((dbname = getenv("DATABASE")) == (char *) NULL) {
+		fprintf(stderr, "no database specified in env var DATABASE\n");
+		fflush(stderr);
+		exit (1);
+	    }
+	    PQsetdb(dbname);
 	}
-
-	PQsetdb(dbname);
+	{
+	    extern int p_attr_caching;
+	    p_attr_caching = 0;
+	}
 
 	(void) PQexec("begin");
 
-	if (argc < 2) {
-		fprintf(stderr, "usage: %s directory ...\n", argv[0]);
+	if ((argc-optind) < 1) {
 		PQfinish();
-		exit(1);
+		usage();
 	}
 
-	while (--argc) {
-		if (p_rmdir(*++argv) < 0) {
-			fprintf(stderr, "p_rmdir: ");
-			perror(*argv);;
-			errors++;
-		}
-	}
+	do {
+	    if (p_rmdir(argv[optind]) < 0) {
+		fprintf(stderr, "p_rmdir: ");
+		perror(argv[optind]);;
+		errors++;
+	    }
+	    optind++;
+	} while (optind < argc);
 
 	if (errors == 0)
 		(void) PQexec("end");
@@ -59,4 +88,11 @@ main(argc,argv)
 
 	exit(errors != 0);
 	/* NOTREACHED */
+}
+
+void
+usage()
+{
+    fprintf(stderr, "usage: prmdir directory ...\n");
+    exit(1);
 }
