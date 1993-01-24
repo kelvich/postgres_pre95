1953283ddbdcbfc89d2f32f40bd625b53eae0357
clarsen <clarsen>
1993-01-24 04:53:51 +0000

    add -D,-H,-P flags

--- src/bin/fsutils/ppwd/ppwd.c
+++ src/bin/fsutils/ppwd/ppwd.c
@@ -1,35 +1,65 @@
-#ifndef lint
-static	char sccsid[] = "@(#)pwd.c 1.1 90/03/23 SMI"; /* from UCB 4.4 83/01/05 */
-#endif
 /*
- * pwd
+ * Copyright (c) 1991 Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
+
+#ifndef lint
+char copyright[] =
+"@(#) Copyright (c) 1991 Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)pwd.c	5.4 (Berkeley) 2/20/91";
+#endif /* not lint */
+
+#include <unistd.h>
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/param.h>
 #include "tmp/libpq-fs.h"
 
-char *getwd();
-extern char *getenv();
+extern int p_errno;
 
 main()
 {
-	char pathname[MAXPATHLEN + 1];
-	char *dbname;
+	char *p;
+	char pathname[MAXPATHLEN+1];
 
-	if ((dbname = getenv("DATABASE")) == (char *) NULL) {
-	    fprintf(stderr, "no database specified in env var DATABASE\n");
-	    fflush(stderr);
-	    exit (1);
+	p = p_getwd(pathname);
+	if (p) {
+		(void)printf("%s\n", p);
+		exit(0);
 	}
-
-	PQsetdb(dbname);
-	
-	if (p_getwd(pathname) == NULL) {
-		fprintf(stderr, "ppwd: %s\n", pathname);
-		exit(1);
-	}
-	printf("%s\n", pathname);
-	PQfinish();
-	exit(0);
-	/* NOTREACHED */
+	(void)fprintf(stderr, "ppwd: %s\n", strerror(p_errno));
+	exit(1);
 }
