ceed13dc2307e1bb2a603bac6644bde191ca774b
marc <marc>
1993-02-18 23:55:41 +0000

    support optional username

--- src/bin/pg_id/pg_id.c
+++ src/bin/pg_id/pg_id.c
@@ -1,10 +1,46 @@
 /*
  * $Header$
  */
-main()
 
+/*
+ * Print the user ID for the login name passed as argument,
+ * or the real user ID of the caller if no argument.  If the
+ * login name doesn't exist, print "NOUSER" and exit 1.
+ */
+#include <sys/types.h>
+#include <pwd.h>
+#include <stdio.h>
+
+main(argc, argv)
+	char **argv;
 {
-	extern int getuid();
+	struct passwd *pw;
+	int ch;
+	extern int optind;
 
-	printf("%d", getuid());
+	while ((ch = getopt(argc, argv, "")) != EOF)
+		switch (ch) {
+		case '?':
+		default:
+			fprintf(stderr, "usage: pg_id [login]\n");
+			exit(1);
+		}
+	argc -= optind;
+	argv += optind;
+
+	if (argc > 0) {
+		if (argc > 1) {
+                        fprintf(stderr, "usage: pg_id [login]\n");
+                        exit(1);
+		}
+		if ((pw = getpwnam(argv[0])) == NULL) {
+			printf("NOUSER\n");
+			exit(1);
+		}
+		printf("%d\n", pw->pw_uid);
+	} else {
+		printf("%d\n", getuid());
+	}
+
+	exit(0);
 }
