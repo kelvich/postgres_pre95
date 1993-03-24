8437c5e28aab930caca8ee83b5497508e9cbef86
aoki <aoki>
1993-03-24 20:00:09 +0000

    fixed the goddamn thing to have the same command-line options as createdb/destroydb et al.

--- src/bin/vacuum/vacuum.sh
+++ src/bin/vacuum/vacuum.sh
@@ -1,9 +1,50 @@
 #!/bin/sh
-# $Header$
+# ----------------------------------------------------------------
+#   FILE
+# 	vacuum
 #
-#	Vacuum postgres databases
+#   DESCRIPTION
+#	This program runs the monitor with the "-c" option to vacuum
+#	the requested database.
 #
 #	We expect that this script will be run via cron nightly.  This
 #	replaces vacuumd and vcontrol in earlier releases of postgres.
 #
-_POSTGRESBIN_/monitor -c vacuum $*
+#   IDENTIFICATION
+#     $Header$
+# ----------------------------------------------------------------
+
+# ----------------
+#       Set paths from environment or default values.
+#       The _fUnKy_..._sTuFf_ gets set when the script is installed
+#       from the default value for this build.
+#	Currently the only thing we look for from the environment is
+#	PGDATA, PGHOST, and PGPORT
+# ----------------
+[ -z "$PGPORT" ] && PGPORT=4321
+[ -z "$PGHOST" ] && PGHOST=localhost
+BINDIR=_fUnKy_BINDIR_sTuFf_
+PATH=$BINDIR:$PATH
+
+dbname=$USER
+
+while test -n "$1"
+do
+    case $1 in
+	-a) AUTHSYS=$2; shift;;
+        -h) PGHOST=$2; shift;;
+        -p) PGPORT=$2; shift;;
+         *) dbname=$1;;
+    esac
+    shift;
+done
+
+AUTHOPT="-a $AUTHSYS"
+[ -z "$AUTHSYS" ] && AUTHOPT=""
+
+monitor -TN $AUTHOPT -h $PGHOST -p $PGPORT -c "vacuum" $dbname || {
+    echo "$0: database vacuum failed on $dbname."
+    exit 1
+}
+
+exit 0
