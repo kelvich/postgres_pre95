f314dee71c8c3de4201324313735b44c04903f26
avi <avi>
1993-05-16 19:07:15 +0000

    oidint2 declarations added

--- src/backend/utils/oidcompos.h
+++ src/backend/utils/oidcompos.h
@@ -18,6 +18,18 @@ bool oidint4ne ARGS((OidInt4 o1 , OidInt4 o2 ));
 bool oidint4cmp ARGS((OidInt4 o1 , OidInt4 o2 ));
 OidInt4 mkoidint4 ARGS((ObjectId v_oid , uint32 v_int4 ));
 
+/* oidint2.c */
+OidInt2 oidint2in ARGS((char *o ));
+char *oidint2out ARGS((OidInt2 o ));
+bool oidint2lt ARGS((OidInt2 o1 , OidInt2 o2 ));
+bool oidint2le ARGS((OidInt2 o1 , OidInt2 o2 ));
+bool oidint2eq ARGS((OidInt2 o1 , OidInt2 o2 ));
+bool oidint2ge ARGS((OidInt2 o1 , OidInt2 o2 ));
+bool oidint2gt ARGS((OidInt2 o1 , OidInt2 o2 ));
+bool oidint2ne ARGS((OidInt2 o1 , OidInt2 o2 ));
+bool oidint2cmp ARGS((OidInt2 o1 , OidInt2 o2 ));
+OidInt2 mkoidint2 ARGS((ObjectId v_oid , uint16 v_int2 ));
+
 /* oidchar16.c */
 OidChar16 oidchar16in ARGS((char *inStr ));
 char *oidchar16out ARGS((OidChar16 oidname ));
