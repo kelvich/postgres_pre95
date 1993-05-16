016388519ed65ab5c02ce1f9c5475e43e84e4fcf
avi <avi>
1993-05-16 19:01:08 +0000

    F_OID8EQ added as the EQPROC for oid8s

--- src/backend/utils/cache/catcache.c
+++ src/backend/utils/cache/catcache.c
@@ -68,7 +68,7 @@ static int	DisableCache;
 static long	eqproc[] = {
     F_BOOLEQ, 0l, F_CHAREQ, F_CHAR16EQ, 0l,
     F_INT2EQ, F_KEYFIRSTEQ, F_INT4EQ, 0l, F_TEXTEQ,
-    F_OIDEQ, 0l, 0l, 0l, 0l
+    F_OIDEQ, 0l, 0l, 0l, F_OID8EQ
 };
 
 #define	EQPROC(SYSTEMTYPEOID)	eqproc[(SYSTEMTYPEOID)-16]
