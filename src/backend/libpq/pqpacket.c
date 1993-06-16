8b67dea4f235e56c969ddc4927d1226d43a5ea6b
aoki <aoki>
1993-06-16 05:05:24 +0000

    now sets PQerrormsg in a couple places where it should

--- src/backend/libpq/pqpacket.c
+++ src/backend/libpq/pqpacket.c
@@ -5,8 +5,8 @@
  * $Header$
  *
  * This is the module that understands the lowest-level part
- * of the communication protocol.  All of the trickyness in
- * this module is for making sure that non-blocking IO in
+ * of the communication protocol.  All of the trickiness in
+ * this module is for making sure that non-blocking I/O in
  * the Postmaster works correctly.   Check the notes in PacketRecv
  * on non-blocking I/O.
  *
@@ -23,8 +23,6 @@
  *	conversion for headers.  Data conversion is someone elses
  *	responsibility.
  *
- * Memory Allocation:  is not handled in this module
- *
  * IMPORTANT: these routines are called by backends, clients, and
  *	the Postmaster.
  *
@@ -32,14 +30,8 @@
  *	header structs become longs, must switch to htonl().
  * XXX say what??
  *
- * XXX this fake-o UDP shit has got to go.  none of these routines are
+ * XXX this fake-o UDP cruft has got to go.  none of these routines are
  * called more than once, anywhere.
- *
- * XXX i can't believe no one has noticed that there aren't any ntohl's in
- * this code -- just htonl's.  (it just happens to work because no one can
- * ever actually run libpq between byte-swapped machines -- the fact that
- * we send raw host-order integers everywhere would kill you.)  but if we
- * fix this code, then startup packets from old binaries become invalid.  feh.
  */
 #include <stdio.h>
 #include <sys/types.h>
@@ -47,16 +39,16 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <fcntl.h>
+#include <errno.h>
 
 #include "tmp/postgres.h"
 #include "utils/log.h"
 #include "storage/ipci.h"
 #include "tmp/pqcomm.h"
-
-extern char PQerrormsg[];
+#include "tmp/libpq.h"
 
 /*
- * PacketRecieve -- receive a packet on a port.
+ * PacketReceive -- receive a packet on a port.
  *
  * RETURNS: connection id of the packet sender, if one
  * is available.
@@ -64,114 +56,111 @@ extern char PQerrormsg[];
  * XXX this code is called in exactly one place
  */
 PacketReceive(port, buf, nonBlocking, connIdP)
-Port		*port;	/* receive port */
-PacketBuf	*buf;	/* MAX_PACKET_SIZE-worth of buffer space */
-Boolean		nonBlocking; /* NON_BLOCKING or BLOCKING i/o */
-ConnId		*connIdP;    /* sender's connection (seqpack) */
+    Port	*port;	/* receive port */
+    PacketBuf	*buf;	/* MAX_PACKET_SIZE-worth of buffer space */
+    Boolean	nonBlocking; /* NON_BLOCKING or BLOCKING i/o */
+    ConnId	*connIdP;    /* sender's connection (seqpack) */
 {
-  int		status;
-  PacketLen	cc;	    	/* character count -- bytes recvd */
-  PacketLen	packetLen; 	/* remaining packet chars to read */
-  Addr	tmp;	   	/* curr recv buf pointer */
-  PacketHdr	*hdr = (PacketHdr *) buf;  /* incoming hdr information */
-  int		addrLen = sizeof(struct sockaddr_in);
-  int		flag;
-  int		decr;
+    int		status;
+    PacketLen	cc;	    	/* character count -- bytes recvd */
+    PacketLen	packetLen; 	/* remaining packet chars to read */
+    Addr	tmp;	   	/* curr recv buf pointer */
+    PacketHdr	*hdr = (PacketHdr *) buf;  /* incoming hdr information */
+    int		addrLen = sizeof(struct sockaddr_in);
+    int		flag;
+    int		decr;
 
-  if (nonBlocking) {
-    flag = MSG_PEEK;
-    decr = 0;
-  } else {
-    flag = 0;
-    decr = sizeof(PacketHdr);
-  }
-  /*
-   * Assume port->nBytes is zero unless we were interrupted during
-   * non-blocking I/O.  This first recvfrom() is to get the hdr
-   * information so we know how many bytes to read.  Life would
-   * be very complicated if we read too much data (buffering).
-   */
-  tmp = ((Addr)buf) + port->nBytes;
-  if (port->nBytes >= sizeof(PacketHdr)) {
-#if 0
-    packetLen = htonl(hdr->len) + sizeof(PacketHdr);
-#endif
-    packetLen = htonl(hdr->len) - port->nBytes;
-  } else {
-    cc = recvfrom(port->sock, tmp, sizeof(PacketHdr), flag, 
-		  &(port->raddr), &addrLen);
-    if ( cc < sizeof(PacketHdr)) {
-      /* if cc is negative, the system call failed */
-      if (cc < 0) {
-	return(STATUS_ERROR);
-      }
-      /* 
-       * cc == 0 means the connection was broken at the 
-       * other end.
-       */
-      else if (! cc) {
-	return(STATUS_INVALID);
-
-      } else {
-	/*
-	 * Worst case.  We didn't even read in enough data to
-	 * get the header.  IN UDP (seq-pack) THIS IS IMPOSSIBLE.
-	 * Messages are delivered in full packets.  In the stream
-	 * protocol, it probably won't happen unless the client
-	 * is malicious (so its even harder to test).
-	 *
-	 * Don't save the number of bytes we've read so far.
-	 * Since we only peeked at the incoming message, the
-	 * kernel is going to keep it for us.
-	 */
-
-	*connIdP = INVALID_ID;
-	return(STATUS_NOT_DONE);
-      }
+    if (nonBlocking) {
+	flag = MSG_PEEK;
+	decr = 0;
     } else {
-
-      /* great. got the header. now get the true length (including
-       * header size).
-       */
-      packetLen = htonl(hdr->len);
-      packetLen -= decr;
-      tmp += decr - port->nBytes;
+	flag = 0;
+	decr = sizeof(PacketHdr);
     }
-  }
-
-  /*
-   * Now that we know how big it is, read the packet.  We read
-   * the entire packet, since the last call was just a peek.
-   */
-  while (packetLen) { 
-    cc = recvfrom(port->sock, tmp, packetLen, NULL, 
-		  &(port->raddr), &addrLen);
-    if (cc < 0)
-      return(STATUS_ERROR);
-
-    /* 
-     * cc == 0 means the connection was broken at the 
-     * other end.
+    /*
+     * Assume port->nBytes is zero unless we were interrupted during
+     * non-blocking I/O.  This first recvfrom() is to get the hdr
+     * information so we know how many bytes to read.  Life would
+     * be very complicated if we read too much data (buffering).
      */
-    else if (! cc) 
-      return(STATUS_INVALID);
+    tmp = ((Addr)buf) + port->nBytes;
+    if (port->nBytes >= sizeof(PacketHdr)) {
+#if 0
+	packetLen = htonl(hdr->len) + sizeof(PacketHdr);
+#endif
+	packetLen = htonl(hdr->len) - port->nBytes;
+    } else {
+	cc = recvfrom(port->sock, tmp, sizeof(PacketHdr), flag, 
+		      &(port->raddr), &addrLen);
+	if (cc < sizeof(PacketHdr)) {
+	    /* if cc is negative, the system call failed */
+	    if (cc < 0) {
+		return(STATUS_ERROR);
+	    }
+	    /* 
+	     * cc == 0 means the connection was broken at the 
+	     * other end.
+	     */
+	    else if (! cc) {
+		return(STATUS_INVALID);
 
-    tmp += cc;
-    packetLen -= cc;
-
-
-    /* if non-blocking, we're done. */
-    if (nonBlocking && packetLen) {
-      port->nBytes += cc;
-      return(STATUS_NOT_DONE);
+	    } else {
+		/*
+		 * Worst case.  We didn't even read in enough data to
+		 * get the header.  IN UDP (seq-pack) THIS IS IMPOSSIBLE.
+		 * Messages are delivered in full packets.  In the stream
+		 * protocol, it probably won't happen unless the client
+		 * is malicious (so its even harder to test).
+		 *
+		 * Don't save the number of bytes we've read so far.
+		 * Since we only peeked at the incoming message, the
+		 * kernel is going to keep it for us.
+		 */
+		*connIdP = INVALID_ID;
+		return(STATUS_NOT_DONE);
+	    }
+	} else {
+	    /*
+	     * great. got the header. now get the true length (including
+	     * header size).
+	     */
+	    packetLen = htonl(hdr->len);
+	    packetLen -= decr;
+	    tmp += decr - port->nBytes;
+	}
     }
-  }
 
-  *connIdP = htonl(hdr->connId);
-  port->nBytes = 0;
-  return(STATUS_OK);
+    /*
+     * Now that we know how big it is, read the packet.  We read
+     * the entire packet, since the last call was just a peek.
+     */
+    while (packetLen) { 
+	cc = recvfrom(port->sock, tmp, packetLen, NULL, 
+		      &(port->raddr), &addrLen);
+	if (cc < 0)
+	    return(STATUS_ERROR);
+	/* 
+	 * cc == 0 means the connection was broken at the 
+	 * other end.
+	 */
+	else if (! cc) 
+	    return(STATUS_INVALID);
+
+	tmp += cc;
+	packetLen -= cc;
+
+
+	/* if non-blocking, we're done. */
+	if (nonBlocking && packetLen) {
+	    port->nBytes += cc;
+	    return(STATUS_NOT_DONE);
+	}
+    }
+
+    *connIdP = htonl(hdr->connId);
+    port->nBytes = 0;
+    return(STATUS_OK);
 }
-
 
 /*
  * PacketSend -- send a single-packet message.
@@ -184,38 +173,40 @@ ConnId		*connIdP;    /* sender's connection (seqpack) */
  * XXX this code is called in exactly one place
  */
 PacketSend(port, conn, buf, type, len, nonBlocking)
-Port		*port;
-Connection	*conn;
-PacketBuf	*buf;
-MsgType		type;
-PacketLen	len;
-Boolean		nonBlocking;
+    Port	*port;
+    Connection	*conn;
+    PacketBuf	*buf;
+    MsgType	type;
+    PacketLen	len;
+    Boolean	nonBlocking;
 {
-  int			status;
-  PacketLen		totalLen;
-  PacketHdr		*hdr = (PacketHdr *) buf;
-  int			addrLen = sizeof (struct sockaddr_in);
+    int		status;
+    PacketLen	totalLen;
+    PacketHdr	*hdr = (PacketHdr *) buf;
+    int		addrLen = sizeof(struct sockaddr_in);
 
-  Assert( ! nonBlocking );
-  Assert( buf );
+    Assert(!nonBlocking);
+    Assert(buf);
 
-  totalLen = len;
+    totalLen = len;
 
-  hdr->seqno = htonl(conn->seqno++);
-  hdr->connId = htonl(conn->id);
-  hdr->type = (MsgType)htonl(type);
-  hdr->len = htonl(len);
+    hdr->seqno = htonl(conn->seqno++);
+    hdr->connId = htonl(conn->id);
+    hdr->type = (MsgType)htonl(type);
+    hdr->len = htonl(len);
 
-  len = sendto(port->sock, (Addr) buf, totalLen, /* flags */ 0,
-	       &(port->raddr), addrLen);
+    len = sendto(port->sock, (Addr) buf, totalLen, /* flags */ 0,
+		 &(port->raddr), addrLen);
 
-  if (len < totalLen) {
-      strcpy(PQerrormsg,"PacketSend: couldn't send complete packet\n");
-    fprintf(stderr,PQerrormsg);
-    return(STATUS_ERROR);
-  }
+    if (len < totalLen) {
+	(void) sprintf(PQerrormsg,
+		       "FATAL: PacketSend: couldn't send complete packet: errno=%d\n",
+		       errno);
+	fputs(PQerrormsg, stderr);
+	return(STATUS_ERROR);
+    }
 
-  return(STATUS_OK);
+    return(STATUS_OK);
 }
 
 /*
@@ -229,74 +220,23 @@ Boolean		nonBlocking;
  * XXX this code is called in exactly one place
  */
 PacketData(buf, dataP, bufSizeP, msgTypeP, seqnoP)
-Addr		buf;
-Addr 	*dataP;
-int  		*bufSizeP;
-MsgType		*msgTypeP;
-SeqNo		*seqnoP;
+    Addr	buf;
+    Addr 	*dataP;
+    int  	*bufSizeP;
+    MsgType	*msgTypeP;
+    SeqNo	*seqnoP;
 {
-  PacketHdr	*hdr = (PacketHdr *) buf;
+    PacketHdr	*hdr = (PacketHdr *) buf;
 
-  /* seq pack protocol only */
-  if (htonl(hdr->seqno) != *seqnoP)
-    *msgTypeP = DUPLICATE_MSG;
-  else
-    *seqnoP++;
+    /* seq pack protocol only */
+    if (htonl(hdr->seqno) != *seqnoP)
+	*msgTypeP = DUPLICATE_MSG;
+    else
+	*seqnoP++;
 
-  *msgTypeP = (MsgType)htonl(hdr->type);
-  *bufSizeP = htonl(hdr->len);
+    *msgTypeP = (MsgType)htonl(hdr->type);
+    *bufSizeP = htonl(hdr->len);
 
-  /* data part starts right after the header */
-  *dataP = (Addr) (hdr+1);
+    /* data part starts right after the header */
+    *dataP = (Addr) (hdr+1);
 }
-
-
-/*
- * PacketRetransmit -- resend a buffer.  
- *
- * Assume that the header is already initialized.  We're
- * just going to resend the same message.
- *
- * XXX dead code
- */
-PacketRetransmit(buf,port,addr)
-Addr		buf;
-Port		*port;
-Addr		addr;
-{
-  PacketHdr 	*hdr = (PacketHdr *) buf;
-  PacketLen	len,totalLen;
-  int		addrLen = sizeof (struct sockaddr_in);
-
-  len = totalLen = htonl(hdr->len);
-  len = sendto(port->sock, (Addr) buf, totalLen, /* flags */ 0,
-	       (struct sockaddr *)addr, addrLen);
-
-  if (len < totalLen) {
-    strcpy(PQerrormsg,"PacketWrite: couldn't send complete packet\n");
-    fprintf(stderr,PQerrormsg);
-    return(STATUS_ERROR);
-  }
-
-  return(STATUS_OK);
-}
-
-
-/*
- * PacketBuf -- remove the header from a packet buf for
- * 	a client.
- *
- * NOTE: not sure this is necessary.  Some clients are going
- *	to have to know how big the thing is in order to do
- *	memory allocation.
- *
- * XXX dead code
- */
-PacketBufInit(bufP, bufSizeP)
-Addr 		*bufP;
-PacketLen  	*bufSizeP;
-{
-  *bufP  += sizeof(PacketHdr);
-  *bufSizeP -= sizeof(PacketHdr);
-}
-
