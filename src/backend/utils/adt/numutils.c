cc46e70c835044f3531c2ec81a29e59c3eedad35
aoki <aoki>
1993-06-24 03:58:30 +0000

    added protos for itoa, ltoa, ftoa, atof1 fixed up protos for int2larger/smaller, itoi (now two routines), removed dead protos for inteq, etc. added ltoa->pg_ltoa mapping #define for hpux (hpux already has ltoa)

--- src/backend/utils/adt/numutils.c
+++ src/backend/utils/adt/numutils.c
@@ -7,14 +7,9 @@
  */
 
 #include "tmp/postgres.h"
+#include "utils/builtins.h"
 
 RcsId("$Header$");
-
-int itoa ARGS((int i , char *a ));
-int ltoa ARGS((long l , char *a ));
-int ftoa ARGS((double value, char *ascii, int width, int prec1, char format ));
-int atof1 ARGS((char *str , double *val ));
-
 
 /*
  *	itoa		- converts a short int to its string represention
