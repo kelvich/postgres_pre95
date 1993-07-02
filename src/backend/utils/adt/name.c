d3005aca07b58000e217e13db08822ef4be6446e
aoki <aoki>
1993-07-02 03:09:56 +0000

    added a bunch of routines that take a Name and a char * and do Name-safe operations on them.

--- src/backend/utils/adt/name.c
+++ src/backend/utils/adt/name.c
@@ -18,19 +18,55 @@ RcsId("$Header$");
 
 	 /* (see char.c for comparison/operation routines) */
 
+namestrcpy(name, str)
+    Name name;
+    char *str;
+{
+    if (!name || !str)
+	return(-1);
+    bzero(name->data, sizeof(NameData));
+    (void) strncpy(name->data, str, NAMEDATALEN);
+    return(0);
+}
 
+namestrcat(name, str)
+    Name name;
+    char *str;
+{
+    int i;
+    char *p, *q;
+
+    if (!name || !str)
+	return(-1);
+    for (i = 0, p = name->data; i < NAMEDATALEN && *p; ++i, ++p)
+	;
+    for (q = str; i < NAMEDATALEN; ++i, ++p, ++q) {
+	*p = *q;
+	if (!*q)
+	    break;
+    }
+    return(0);
+}
+
+namestrcmp(name, str)
+    Name name;
+    char *str;
+{
+    if (!name && !str)
+	return(0);
+    if (!name)
+	return(-1);	/* NULL < anything */
+    if (!str)
+	return(1);	/* NULL < anything */
+    return(strncmp(name->data, str, NAMEDATALEN));
+}
 
 	     /* ========== PRIVATE ROUTINES ========== */
-
-/* name.c */
-bool NameIsEqual ARGS((Name name1 , Name name2 ));
-uint32 NameComputeLength ARGS((Name name ));
 
 /*
  * Note:
  *	This is the same code as char16eq.
  *	Assumes that "xy\0\0a" should be equal to "xy\0b".
- *	If not, can do the comparison backwards for efficiency???
  */
 bool
 NameIsEqual(name1, name2)
@@ -40,7 +76,7 @@ NameIsEqual(name1, name2)
 	if (! PointerIsValid(name1) || ! PointerIsValid(name2)) {
 		return(false);
 	}
-	return((bool)(strncmp(name1, name2, 16) == 0));
+	return((bool)(strncmp(name1, name2, NAMEDATALEN) == 0));
 }
 
 uint32
@@ -52,9 +88,10 @@ NameComputeLength(name)
 
 	Assert(NameIsValid(name));
 
-	for (length = 0, charP = (char *)name; length < 16 && *charP != '\0';
-			length++, charP++) {
-		;
+	for (length = 0, charP = name->data;
+	     length < NAMEDATALEN && *charP != '\0';
+	     length++, charP++) {
+	    ;
 	}
 	return (uint32)length;
 }
