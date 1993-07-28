871b9d2310fe4648a2407fdfac9da5074b33a9c3
aoki <aoki>
1993-07-28 23:43:56 +0000

    MAKEOBJDIR changes

--- src/tools/bmake/mk-proto/postgres.global.mk
+++ src/tools/bmake/mk-proto/postgres.global.mk
@@ -1,11 +1,19 @@
 #
-# Pull in Makefile.global's from this and any parent dirs.
-# If a Makefile.global also exists in the obj/ directory,
-# include that afterwards (so it can override any previous
-# settings).  We use "./..." to disable make from doing a path
-# search to locate files as this gets *very* confusing on
-# a large system like this.
+# postgres.global.mk
 #
+# $Header$
+#
+
+#
+# Pull in Makefile.global's from this and any parent dirs.  If a
+# Makefile.global also exists in the obj/ directory, include that
+# afterwards (so it can override any previous settings).  We use
+# "./..." to disable make from doing a path search to locate files as
+# this gets *very* confusing on a large system like this.
+#
+
+MAKEOBJDIR?=obj
+
 .if !defined(NOINCLUDE)
 .if exists(./Makefile.local)
 .    include "./Makefile.local"
@@ -15,23 +23,23 @@
 .	if exists(${.CURDIR}/../../../Makefile.global)
 .	    if exists(${.CURDIR}/../../../../Makefile.global)
 .		include "${.CURDIR}/../../../../Makefile.global"
-.		if exists(${.CURDIR}/../../../../obj/Makefile.global)
-.		    include "${.CURDIR}/../../../../obj/Makefile.global"
+.		if exists(${.CURDIR}/../../../../${MAKEOBJDIR}/Makefile.global)
+.		    include "${.CURDIR}/../../../../${MAKEOBJDIR}/Makefile.global"
 .		endif
 .	    endif
 .	    include "${.CURDIR}/../../../Makefile.global"
-.	    if exists(${.CURDIR}/../../../obj/Makefile.global)
-.		include "${.CURDIR}/../../../obj/Makefile.global"
+.	    if exists(${.CURDIR}/../../../${MAKEOBJDIR}/Makefile.global)
+.		include "${.CURDIR}/../../../${MAKEOBJDIR}/Makefile.global"
 .	    endif
 .	endif
 .	include "${.CURDIR}/../../Makefile.global"
-.	if exists(${.CURDIR}/../../obj/Makefile.global)
-.	    include "${.CURDIR}/../../obj/Makefile.global"
+.	if exists(${.CURDIR}/../../${MAKEOBJDIR}/Makefile.global)
+.	    include "${.CURDIR}/../../${MAKEOBJDIR}/Makefile.global"
 .	endif
 .    endif
 .    include "${.CURDIR}/../Makefile.global"
-.    if exists(${.CURDIR}/../obj/Makefile.global)
-.	include "${.CURDIR}/../obj/Makefile.global"
+.    if exists(${.CURDIR}/../${MAKEOBJDIR}/Makefile.global)
+.	include "${.CURDIR}/../${MAKEOBJDIR}/Makefile.global"
 .    endif
 .endif
 
@@ -39,7 +47,7 @@
 .    include "${.CURDIR}/Makefile.global"
 .endif
 
-.if exists(${.CURDIR}/obj/Makefile.global)
-.    include "${.CURDIR}/obj/Makefile.global"
+.if exists(${.CURDIR}/${MAKEOBJDIR}/Makefile.global)
+.    include "${.CURDIR}/${MAKEOBJDIR}/Makefile.global"
 .endif
 .endif
