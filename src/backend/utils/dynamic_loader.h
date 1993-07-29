8ad6938883dcb77d03cad5cd5fcfeb073e722496
andrew <andrew>
1993-07-29 00:16:25 +0000

    dynloader interface restructured. (Gotten rid of the HPUX ifdef's) DynamicFileList now has a "handle" field used by the pg_dl* layer. (The pg_dl* layer consists of pg_dlopen for opening a file, pg_dlsym for retrieving a symbol and pg_dlclose for closing a file.)

--- src/backend/utils/dynamic_loader.h
+++ src/backend/utils/dynamic_loader.h
@@ -14,9 +14,6 @@
 
 #include <sys/param.h>			/* for MAXPATHLEN */
 #include <sys/types.h>			/* for dev_t, ino_t, etc. */
-#ifdef PORTNAME_hpux
-#include <dl.h>				/* for shl_t */
-#endif /* PORTNAME_hpux */
 
 typedef struct {
     func_ptr    func;
@@ -31,8 +28,9 @@ extern FList ExtSyms[];
  *
  */
 
+
 /*
- * Dynamically loaded function list.
+ * Dynamically loaded function list. 
  */
 
 typedef struct df_list {
@@ -40,6 +38,7 @@ typedef struct df_list {
     func_ptr func;			/* Function address */
     struct df_list *next;
 } DynamicFunctionList;
+
 
 /*
  * List of dynamically loaded files.
@@ -49,14 +48,12 @@ typedef struct df_files {
     char filename[MAXPATHLEN];		/* Full pathname of file */
     dev_t device;			/* Device file is on */
     ino_t inode;			/* Inode number of file */
-    DynamicFunctionList *func_list;	/* List of functions */
-    char *address;			/* Memory allocated for file */
-    long size;				/* Size of memory allocated for file */
+    void *handle;			/* a handle for pg_dl* functions */
     struct df_files *next;
-#ifdef PORTNAME_hpux
-    shl_t handle;			/* Handle for shared library */
-    char shlib[MAXPATHLEN];		/* File of dynamic loader */
-#endif /* PORTNAME_hpux */
 } DynamicFileList;
+
+void *pg_dlopen ARGS((char *filename, char **errmsg));
+func_ptr pg_dlsym  ARGS((void *handle, char *funcname));
+void pg_dlclose ARGS((void *handle));
 
 #endif Dynamic_loaderHIncluded
