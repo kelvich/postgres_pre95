5caf3b6b1d35524b1c7b25c7f7bbab5f7f074020
andrew <andrew>
1993-07-29 04:09:16 +0000

    new dynloader. Uses libdl-1.0's dl_open/dl_sym/dl_close.

--- src/backend/port/ultrix4/dynloader.c
+++ src/backend/port/ultrix4/dynloader.c
@@ -3,314 +3,61 @@
  *  $Header$
  */
 
-
 /*
  * New dynamic loader.
  *
- * How does this work?  Glad you asked :-)
- *
- * In the DEC dynamic loader, we have to have done the following in order
- * for it to work:
- *
- * 1. Make sure that we stay near & etext (the highest base text address)
- *    so that we do not try to jump into the data area.  The data area starts
- *    by default at 0x1000000.
- *
- * 2. Make sure everything is linked with the -N option, so that "ld -A" will
- *    do the right thing.
- *
- * 3. Make sure loaded objects are compiled with "-G 0"
- *
- * 4. Make sure loaded objects are loaded ONCE AND ONLY ONCE.
- *
- * The algorithm is as follows:
- *
- * 1.  Find out how much text/data space will be required.  This is done
- *     by reading the header of the ".o" to be loaded.
- *
- * 2.  Execute the "ld -A" with a an address equal to some memory we malloc'ed.
- *     "ld -A" will do all the relocation, etc. for us.
- *
- * 3.  Using the output of "ld -A", read the text and data area into a valid
- *     text area.  (The DEC 3100 allows data to be read in the text area, but
- *     not vice versa.)
- * 
- * 4.  Determine which functions are defined by the object file we are loading,
- *     and using the address we loaded the output of "ld -A" into, find the
- *     addresses of those functions.  In object files, the symbol table (and 
- *     the output of nm) will give the offsets for functions.  Adding the
- *     function offset to the base text address gives the function's true
- *     address.  (This could also be done by reading the symbol table of the
- *     output of "ld -A", but it is so massive that this is VERY wasteful.)
- * 
- *     (In this case, we cheat rather hugely and use the output of nm because
- *     the symbol table format for MIPSEL/DS3100 is not well-documented).
+ * This dynamic loader uses Andrew Yu's libdl-1.0 package for Ultrix 4.x.
+ * (Note that pg_dlsym and pg_dlclose are actually macros defined in
+ * "port-protos.h".)
  */ 
 
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/param.h>
-#include <sys/stat.h>
-#include <sys/file.h>
-
-#include <a.out.h>
-#include <symconst.h>
-#include <mips/cachectl.h>
-
+#include "dl.h"
 #include "tmp/c.h"
 #include "fmgr.h"
 #include "port-protos.h"
+#include "utils/log.h"
 
 extern char pg_pathname[];
-
-static char *load_address = NULL;
-static char *temp_file_name = NULL;
-static char *path = "/usr/tmp/postgresXXXXXX";
-
-#define PAGE_ROUND(X) ((X) % 512 == 0 ? (X) : (X) - (X) % 512 + 512)
 
 void *pg_dlopen( filename, err )
      char *filename; char **err;
 {
-	extern etext, edata, end;
-	extern char *mktemp();
+    static int dl_initialized= 0;
+    void *handle;
 
-	int nread;
-	char command[256];
-	unsigned long image_size, true_image_size;
-	FILE *temp_file = NULL;
-	DynamicFunctionList *func_list = NULL, *load_symbols();
-	struct filehdr obj_file_struct, ld_file_struct;
-	AOUTHDR obj_aout_hdr, ld_aout_hdr;
-	struct scnhdr scn_struct;
-	int size_text, size_data = 0, size_bss = 0, bss_offset;
-	int i, fd;
-	dfHandle *handle= NULL;
-
-	fd = open(filename, O_RDONLY);
-
-	read(fd, & obj_file_struct, sizeof(struct filehdr));
-	read(fd, & obj_aout_hdr, sizeof(AOUTHDR));
-
-	read(fd, & scn_struct, sizeof(struct scnhdr)); /* text hdr */
-	size_text = scn_struct.s_size;
-	if (obj_file_struct.f_nscns > 1)
-	{
-		read(fd, & scn_struct, sizeof(struct scnhdr)); /* data hdr */
-		size_data = scn_struct.s_size;
-	}
-
-	close(fd);
-
-/*
- * add 10000 for fudge factor to account for data areas that appear in
- * the linking process (yes, there are such beasts!).
- */
-
-	image_size = size_text + size_data + 20000;
-
-	if (temp_file_name == NULL)
-	{
-		temp_file_name = (char *)malloc(strlen(path) + 1);
-	}
-
-	strcpy(temp_file_name,path);
-	mktemp(temp_file_name);
-
-	load_address = (char *) valloc(image_size);
-
-	sprintf(command,"ld -x -N -A %s -T %lx -o %s  %s -lm_G0 -lc_G0",
-	    pg_pathname,
-	    load_address,
-	    temp_file_name,  filename);
-
-	if (system(command))
-	{
-		*err = "link failed!";
-		goto finish_up;
-	}
-
-	if(!(temp_file = fopen(temp_file_name,"r")))
-	{
-		*err = "unable to open tmp file";
-		goto finish_up;
-	}
-	fread(&ld_file_struct, sizeof(struct filehdr), 1, temp_file);
-	fread(&ld_aout_hdr, sizeof(AOUTHDR), 1, temp_file);
-
-	fread(&scn_struct, sizeof(struct scnhdr), 1, temp_file); /* text hdr */
-
-/*
- * Figure out how big the data areas (including the bss area) are,
- * and determine where the bss area is if there is one.
- */
-
-	true_image_size = scn_struct.s_size;
-	for (i = 1; i < ld_file_struct.f_nscns; i++)
-	{
-		fread(&scn_struct, sizeof(struct scnhdr), 1, temp_file);
-		true_image_size += scn_struct.s_size;
-		if (!strcmp(scn_struct.s_name, "bss"))
-		{
-			size_bss = scn_struct.s_size;
-			bss_offset = scn_struct.s_vaddr - (int) load_address;
-		}
-	}
-
-/*
- * Here we see if our "fudge guess" above was too small.  We do it this way
- * because loading is so ungodly expensive, and we want to avoid having to
- * create 3 megabyte files unnecessarily.
- */
-
-	if (true_image_size > image_size)
-	{
-		free(load_address);
-		fclose(temp_file);
-		unlink(temp_file_name);
-		load_address = (char *) valloc(true_image_size);
-		sprintf(command,"ld -x -N -A %s -T %lx -o %s  %s -lm_G0 -lc_G0",
-	    		pg_pathname,
-	    		load_address,
-	    		temp_file_name,  filename);
-		system(command);
-		temp_file = fopen(temp_file_name,"r");
-		fread(&ld_file_struct, sizeof(struct filehdr), 1, temp_file);
-		fread(&ld_aout_hdr, sizeof(AOUTHDR), 1, temp_file);
-	}
-
-	fseek(temp_file, N_TXTOFF(ld_file_struct, ld_aout_hdr), 0);
-
-	fread(load_address, true_image_size,1,temp_file);
-
-	/* zero the BSS segment */
-
-	if (size_bss != 0)
-	{
-		bzero(bss_offset + load_address, size_bss);
-	}
-
-	if (cachectl(load_address, PAGE_ROUND(true_image_size), UNCACHEABLE))
-	{
-		*err = "dynamic_file_load: Cachectl failed!";
-	}
-	else
-	{
-		func_list = load_symbols(temp_file_name, load_address);
-	}
-
-	handle= (dfHandle*)malloc(sizeof(dfHandle));
-	handle->func_list= func_list;
-	handle->load_address= load_address;
-	handle->size= true_image_size;
-
-finish_up:
-	fclose(temp_file);
-	unlink(temp_file_name);
-
-	return (void *)handle;
-}
-
-/*
- * Cheat massively because I can't figure out how to read the symbol table
- * properly, so use system("nm ...") to do it instead.
- */
-
-DynamicFunctionList *
-load_symbols(filename, entry_addr)
-
-char *filename;
-int entry_addr;
-
-{
-	char command[256];
-	char line[256];
-	char *tmp_file;
-	FILE *fp;
-	DynamicFunctionList *head, *scanner;
-	int entering = 1, func_addr;
-	char funcname[256];
-
-	/* create a temporary file name */
-	tmp_file = (char *) palloc(64);
-	sprintf(tmp_file, "/tmp/_pg_dyn%ld", getpid());
-
-	sprintf(command, "/usr/bin/nm %s | grep \' T \' > %s", filename, tmp_file);
-
-	if (system(command))
-		fprintf(stderr, "system() died\n");
-
-	fp = fopen(tmp_file, "r");
-
-	while (fgets(line, 255, fp) != NULL)
-	{
-		sscanf(line, "%lx T %s", &func_addr, funcname);
-		if (entering)
-		{
-			head = (DynamicFunctionList *)
-				   malloc(sizeof(DynamicFunctionList));
-			scanner = head;
-			entering = 0;
-		}
-		else
-		{
-			scanner->next = (DynamicFunctionList *)
-							malloc(sizeof(DynamicFunctionList));
-			scanner = scanner->next;
-		}
-
-		scanner->funcname= (char *)strcpy(malloc(strlen(funcname)+1),funcname);
-		scanner->func = (func_ptr) func_addr;
-		scanner->next = NULL;
-	}
-
-	fclose(fp);
-	unlink(tmp_file);
-	pfree(tmp_file);
-	return(head);
-}
-
-func_ptr pg_dlsym( handle, funcname )
-     void *handle; char *funcname;
-{
-    dfHandle *dfhandle= (dfHandle *)handle;
-    DynamicFunctionList *func_scanner;
-
-    if (funcname==NULL)
-	return (func_ptr)NULL;
-    func_scanner= dfhandle->func_list;
-    while(func_scanner) {
-	if (!strcmp(func_scanner->funcname, funcname)) 
-	    return func_scanner->func;
-	func_scanner= func_scanner->next;
-    }
-    return (func_ptr)NULL;
-}
-
-void pg_dlclose( handle )
-     void *handle;
-{
-    dfHandle *dfhandle= (dfHandle *)handle;
-    DynamicFunctionList *func_scanner, *throw_away;
-    
-    bzero(dfhandle->load_address, dfhandle->size);
-
-#if !defined(OLD_DEC) 
     /*
-     * Some dynamic loaders use brk/sbrk - DON'T TRY TO FREE THIS SPACE.
+     * initializes the dynamic loader with the executable's pathname.
+     * (only needs to do this the first time pg_dlopen is called.)
      */
-    free((char *) dfhandle->load_address);
-#endif /* !OLD_DEC */
-
-    func_scanner = dfhandle->func_list;
-    for (throw_away = func_scanner->next;
-	 throw_away != (DynamicFunctionList *) NULL;) {
-        throw_away = func_scanner->next;
-	free((char *) func_scanner->funcname);
-        free((char *) func_scanner);
-        func_scanner = throw_away;
+    if (!dl_initialized) {
+        if (!dl_init(pg_pathname)) {
+	    *err = dl_error();
+	    return NULL;
+	}
+	dl_initialized= 1;
     }
-    free((char *) dfhandle->func_list);
 
-    return;
+    /*
+     * open the file. We do the symbol resolution right away so that we
+     * will know if there are undefined symbols. (This is in fact the
+     * same semantics as "ld -A". ie. you cannot have undefined symbols.
+     */
+    if ((handle=dl_open(filename, DL_NOW))==NULL) {
+	int count;
+	char **list= dl_undefinedSymbols(&count);
+
+	/* list the undefined symbols, if any */
+	if(count) {
+	    elog(NOTICE, "dl: Undefined:");
+	    while(*list) {
+		elog(NOTICE, "  %s", *list);
+		list++;
+	    }
+	}
+	*err= dl_error();
+    }
+
+    return (void *)handle;
 }
+
