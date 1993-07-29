dc7dcfc407a5467ccf335b6d705bb8a3e37e0def
andrew <andrew>
1993-07-29 00:01:13 +0000

    dynloader interface restructured.

--- src/backend/port/sparc/port-protos.h
+++ src/backend/port/sparc/port-protos.h
@@ -5,10 +5,14 @@
 #include "utils/dynamic_loader.h"
 
 /* dynloader.c */
-DynamicFunctionList *dynamic_file_load ARGS((char **err , char *filename , char **address , long *size ));
 DynamicFunctionList *load_symbols ARGS((char *filename , struct exec *hdr , int entry_addr ));
 func_ptr dynamic_load ARGS((char **err ));
 int execld ARGS((char *address , char *tmp_file , char *filename ));
+
+typedef struct dfHandle {
+    DynamicFunctionList *func_list;
+    char		*load_address;
+} dfHandle;
 
 /* port.c */
 
