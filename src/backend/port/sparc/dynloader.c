ba1ba2b62ac3f3e894b5b0999bdd9bcdcb9ea22e
aoki <aoki>
1993-08-04 11:02:54 +0000

    blew away ld -A loader in favor of dlopen(3) loader. (sparc dl fix)

--- src/backend/port/sparc/dynloader.c
+++ src/backend/port/sparc/dynloader.c
@@ -1,260 +1,54 @@
 /*
+ *   FILE
+ *	dynloader.c
+ *
+ *   DESCRIPTION
+ *	dynamic loader for SunOS 4 using the shared library mechanism
+ *
+ *   INTERFACE ROUTINES
+ *	pg_dlopen
+ *	pg_dlsym
+ *	pg_dlclose
+ *
+ *   NOTES
+ *	pg_dlsym and pg_dlclose are actually macros, defined in 
+ *	port-protos.h.
+ *
+ *   IDENTIFICATION
+ *	$Header$
+ */
 
-!!!! Be sure that we close all open files up exit on the Sequent - the ld !!!!
-     fails otherwise 
-
-	 $Header$
-*/
-
-#include <pwd.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/param.h>
-#include <sys/stat.h>
-#include <sys/file.h>
+#include <dlfcn.h>
 
-extern char pg_pathname[];
-
-#include <a.out.h>
-
-#include "tmp/c.h"
-#include "fmgr.h"
 #include "port-protos.h"
 
+RcsId("$Header$");
+
 /*
- * Allow extra space for "overruns" caused by the link.
+ * Dynamic Loader on SunOS 4.
+ *
+ * this dynamic loader uses the system dynamic loading interface for shared 
+ * libraries (ie. dlopen/dlsym/dlclose). The user must specify a shared
+ * library as the file to be dynamically loaded.
+ *
+ * Note that only pg_dlopen is defined here. pg_dlsym and pg_dlclose
+ * are actually macros.
  */
 
-#define FUDGE 10000
-
-static char *temp_file_name = NULL;
-static char *path = "/usr/tmp/postgres";
-
-void *pg_dlopen( filename, err )
-     char *filename; char **err;
-{
-	extern char *valloc();
-
-	int nread;
-	struct exec ld_header, header;
-	unsigned long image_size, true_image_size;
-	char *load_address = NULL;
-	FILE *temp_file = NULL;
-	DynamicFunctionList *func_list= NULL, *load_symbols();
-	int fd;
-	char foo[10];
-	dfHandle *handle= NULL;
-
-	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
-	{
-		*err = "error opening file";
-		goto finish_up;
-	}
-
-	read(fd, &ld_header, sizeof(struct exec));
-
-	image_size = ld_header.a_text + ld_header.a_data + ld_header.a_bss + FUDGE;
-
-	close(fd); /* don't open it until the load is finished. */
-
-	if (!(load_address = valloc(image_size)))
-	{
-		*err = "unable to allocate memory";
-		goto finish_up;
-	}
-
-	if (temp_file_name == NULL)
-	{
-		sprintf(foo, "%d", getpid());
-		temp_file_name = (char *)malloc(strlen(path) + strlen(foo) + 2);
-		strcpy(temp_file_name, path);
-		strcat(temp_file_name, foo);
-	}
-
-	if(execld(load_address, temp_file_name, filename))
-	{
-		*err = "link failed!";
-		goto finish_up;
-	}
-
-	if(!(temp_file = fopen(temp_file_name,"r")))
-	{
-		*err = "unable to open tmp file";
-		goto finish_up;
-	}
-	nread = fread(&header, sizeof(header), 1, temp_file);
-	true_image_size = header.a_text + header.a_data + header.a_bss;
-
-	if (true_image_size > image_size)
-	{
-		fclose(temp_file);
-		free(load_address);
-		load_address = valloc(true_image_size);
-
-		if (execld(load_address, temp_file_name, filename))
-		{
-			*err = "ld failed!";
-			goto finish_up;
-		}
-		temp_file = fopen(temp_file_name,"r");
-		nread = fread(&header, sizeof(header), 1, temp_file);
-	}
-
-	fseek(temp_file, N_TXTOFF(header), 0);
-	nread = fread(load_address, true_image_size,1,temp_file);
-
-	func_list = load_symbols(filename, &ld_header, load_address);
-
-	fclose(temp_file);
-	unlink(temp_file_name);
-
-	handle= (dfHandle*)malloc(sizeof(dfHandle));
-	handle->func_list= func_list;
-	handle->load_address= load_address;
-
-	temp_file = NULL;
-	load_address = NULL;
-
-finish_up:
-	if (temp_file != NULL) fclose(temp_file);
-	if (load_address != NULL) free(load_address);
-	return (void *)handle;
-}
-
-DynamicFunctionList *
-load_symbols(filename, hdr, entry_addr)
-
-char *filename;
-struct exec *hdr;
-int entry_addr;
-
-{
-	int fd;
-	char *strings, *symb_table, *p, *q;
-	int symtab_offset, string_offset, string_size, nsyms, i;
-	struct nlist *table_entry;
-	int entering = 1;
-	DynamicFunctionList *head, *scanner;
-
-	symtab_offset = N_SYMOFF(*hdr);
-	string_offset = N_STROFF(*hdr);
-
-	fd = open(filename, O_RDONLY);
-
-	lseek(fd, string_offset, 0);
-	read(fd, &string_size, sizeof(string_size));
-	strings = (char *) malloc(string_size - 4);
-	read(fd, strings, string_size - 4);
-	nsyms = hdr->a_syms / sizeof(struct nlist);
-	lseek(fd, symtab_offset, 0);
-	symb_table = (char *) malloc(hdr->a_syms);
-	read(fd, symb_table, hdr->a_syms);
-
-	p = symb_table;
-	for (i = 0; i < nsyms; i++)
-	{
-		table_entry = (struct nlist *) p;
-		p += sizeof(struct nlist);
-	    if (! ((table_entry->n_type & N_EXT) == 0
-			|| (table_entry->n_type & N_TYPE) != N_TEXT))
-		{
-			if (entering)
-			{
-				head = (DynamicFunctionList *)
-					   malloc(sizeof(DynamicFunctionList));
-				scanner = head;
-				entering = 0;
-			}
-			else
-			{
-				scanner->next = (DynamicFunctionList *)
-								malloc(sizeof(DynamicFunctionList));
-				scanner = scanner->next;
-			}
-			/*
-			 * Add one for "_", ie
-			 * overpaid() will be _overpaid
-			 */
-
-			q = strings + (table_entry->n_un.n_strx - 4) + 1;
-
-			scanner->funcname= (char *)strcpy(malloc(strlen(q)+1),q);
-			scanner->func = (func_ptr) (table_entry->n_value + entry_addr);
-			scanner->next = NULL;
-		}
-	}
-
-	free(symb_table);
-	free(strings);
-	close(fd);
-	return(head);
-}
-
-func_ptr
-dynamic_load(err)
-
-char **err;
-
-{
-	*err = "Dynamic load: Should not be here!";
-	return(NULL);
-}
-
-/* 
- *   ld -N -x -A SYMBOL_TABLE -T ADDR -o TEMPFILE FUNC -lc
+/*
+ * pg_dlopen--
+ *	attempts to dynamically loaded in filename and returns error in
+ *	err, if any. 
  */
-
-execld(address, tmp_file, filename)
-
-char *address, *tmp_file, *filename;
-
+void *
+pg_dlopen(filename, err)
+    char *filename; char **err;
 {
-	char command[256];
-	int retval;
+    void *handle;
 
-	sprintf(command,"ld -N -x -A %s -T %lx -o %s  %s -lc -lm -ll",
-	    	pg_pathname,
-	    	address,
-	    	tmp_file,  filename);
-	retval = system(command);
-	return(retval);
-}
-
-func_ptr pg_dlsym( handle, funcname )
-     void *handle; char *funcname;
-{
-    dfHandle *dfhandle= (dfHandle *)handle;
-    DynamicFunctionList *func_scanner;
-
-    if (funcname==NULL)
-	return (func_ptr)NULL;
-    func_scanner= dfhandle->func_list;
-    while(func_scanner) {
-	if (!strcmp(func_scanner->funcname, funcname)) 
-	    return func_scanner->func;
-	func_scanner= func_scanner->next;
+    if ((handle = dlopen(filename, 1)) == (void *) NULL) {
+	*err = dlerror();
     }
-    return (func_ptr)NULL;
-}
-
-void pg_dlclose( handle )
-     void *handle;
-{
-    dfHandle *dfhandle= (dfHandle *)handle;
-    DynamicFunctionList *func_scanner, *throw_away;
-    
-    free((char *) dfhandle->load_address);
-    func_scanner = dfhandle->func_list;
-    for (throw_away = func_scanner->next;
-	 throw_away != (DynamicFunctionList *) NULL;) {
-        throw_away = func_scanner->next;
-	free((char *) func_scanner->funcname);
-        free((char *) func_scanner);
-        func_scanner = throw_away;
-    }
-    free((char *) dfhandle->func_list);
-
-    return;
+    return((void *) handle);
 }
--- src/backend/port/sparc/port-protos.h
+++ src/backend/port/sparc/port-protos.h
@@ -1,19 +1,30 @@
 /*
- * $Header$
+ *   FILE
+ *	port-protos.h
+ *
+ *   DESCRIPTION
+ *	port-specific prototypes for SunOS 4
+ *
+ *   NOTES
+ *
+ *   IDENTIFICATION
+ *	$Header$
  */
 
+#ifndef PortProtos_H			/* include this file only once */
+#define PortProtos_H 1
+
+#include <dlfcn.h>
+#include "fmgr.h"			/* for func_ptr */
 #include "utils/dynamic_loader.h"
 
 /* dynloader.c */
-DynamicFunctionList *load_symbols ARGS((char *filename , struct exec *hdr , int entry_addr ));
-func_ptr dynamic_load ARGS((char **err ));
-int execld ARGS((char *address , char *tmp_file , char *filename ));
 
-typedef struct dfHandle {
-    DynamicFunctionList *func_list;
-    char		*load_address;
-} dfHandle;
+#define	pg_dlsym	dlsym
+#define	pg_dlclose	dlclose
 
 /* port.c */
 
 void sparc_bug_set_outerjoincost ARGS((char *p, long val));
+
+#endif /* PortProtos_H */
