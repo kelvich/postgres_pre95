cf2aa493246b6d0a6cd1ee0142c9389fbd056c83
aoki <aoki>
1993-08-04 11:02:54 +0000

    cleanups for removal of old loader

--- src/backend/port/hpux/port-protos.h
+++ src/backend/port/hpux/port-protos.h
@@ -3,35 +3,26 @@
  *	port-protos.h
  *
  *  DESCRIPTION
- *
- *  INTERFACE ROUTINES
+ *	port-specific prototypes for HP-UX
  *
  *  NOTES
- *	Based on:
- *	$Header: hpux port-protos.h Rik Tue Jan 19 17:11:46 GMT 1993 
  *
  *  IDENTIFICATION
  *	$Header$
  */
 
-#include "fmgr.h"
-#include "utils/dynamic_loader.h"
+#ifndef PortProtos_H			/* include this file only once */
+#define PortProtos_H 1
 
-/* dfmgr.c */
-void		    del_shlibs ARGS((int code));
+#include <dl.h>				/* for shl_t */
+
+#include "utils/dynamic_loader.h"
 
 /* dynloader.c */
 
-DynamicFunctionList *read_symbols ARGS((char *filename ));
-func_ptr 	    dynamic_load ARGS((char **err ));
-int       	    execld ARGS((char *tmp_file , char *filename ));
-void      	    free_dyna ARGS(( DynamicFunctionList **dyna_list ));
+#define	del_shlibs random		/* until i can change postinit.c */
 
-typedef struct dfHandle {
-    DynamicFunctionList *func_list;
-    shl_t handle;			/* Handle for shared library */
-    char shlib[MAXPATHLEN];		/* File of dynamic loader */
-} dfHandle;
+/* pg_dl{open,close,sym} prototypes are in utils/dynamic_loader.h */
 
 /* port.c */
 
@@ -40,3 +31,5 @@ double cbrt ARGS((double x));
 long random ARGS((void));
 void srandom ARGS((int seed));
 int getrusage ARGS((int who, struct rusage *ru));
+
+#endif /* PortProtos_H */
