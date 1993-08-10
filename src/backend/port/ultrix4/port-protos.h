0e59f403e0ead2744b3aa923efd4676506f293f5
aoki <aoki>
1993-08-10 19:39:55 +0000

    added port.c with init_address_fixup

--- src/backend/port/ultrix4/port-protos.h
+++ src/backend/port/ultrix4/port-protos.h
@@ -1,11 +1,26 @@
 /*
- * $Header$
+ *   FILE
+ *	port-protos.h
+ *
+ *   DESCRIPTION
+ *	prototypes for Ultrix-specific routines
+ *
+ *   NOTES
+ *	Since Andrew's libdl maps into the regular SVIDish dlopen() 
+ *	interface, we can just #define some of the pg_dlopen() stuff 
+ *	into the dlopen() routines.
+ *
+ *   IDENTIFICATION
+ *	$Header$
  */
 
 #include "utils/dynamic_loader.h"
 
+/* dynloader.c */
 
 #define    pg_dlsym(h, f)	((func_ptr)dl_sym(h, f))
-
 #define    pg_dlclose(h)	dl_close(h)
 
+/* port.c */
+
+extern init_address_fixup ARGS((void));
--- src/backend/port/ultrix4/port.c
+++ src/backend/port/ultrix4/port.c
@@ -1,11 +1,29 @@
-
 /*
- *  SENDANNOUNCE.C
+ *   FILE
+ *	port.c
  *
- *  A do-nothing routine?
+ *   DESCRIPTION
+ *	Ultrix-specific routines
+ *
+ *   INTERFACE ROUTINES
+ *	init_address_fixup
+ *
+ *   NOTES
+ *
+ *   IDENTIFICATION
+ *	$Header$
  */
-#define RcsId(a)
+
+#include <sys/syscall.h>
+#include <sys/sysmips.h>
+
+#include "tmp/c.h"
 
 RcsId("$Header$");
 
-int i; /* to avoid "no symbol table" warning in some linkers */
+init_address_fixup()
+{
+#ifdef NOFIXADE
+    syscall(SYS_sysmips, MIPS_FIXADE, 0, NULL, NULL, NULL);
+#endif /* NOFIXADE */
+}
