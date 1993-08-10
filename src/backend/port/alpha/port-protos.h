ba1247b1ddb14e551de054ac8005333f41a56b84
aoki <aoki>
1993-08-10 19:50:31 +0000

    added init_address_fixup

--- src/backend/port/alpha/port-protos.h
+++ src/backend/port/alpha/port-protos.h
@@ -1,9 +1,25 @@
 /*
- * $Header$
+ *   FILE
+ *	port-protos.h
+ *
+ *   DESCRIPTION
+ *	prototypes for OSF/1-specific routines
+ *
+ *   NOTES
+ *	OSF/1 provides the dlopen() interface, so most of the pg_dlopen()
+ *	stuff maps directly into it.
+ *
+ *   IDENTIFICATION
+ *	$Header$
  */
 
 #include "utils/dynamic_loader.h"
 
-#define	 pg_dlsym(h, f)		((func_ptr)dlsym(h, f))
+/* dynloader.c */
 
+#define	 pg_dlsym(h, f)		((func_ptr)dlsym(h, f))
 #define	 pg_dlclose(h)		dlclose(h)
+
+/* port.c */
+
+extern init_address_fixup ARGS((void));
--- src/backend/port/alpha/port.c
+++ src/backend/port/alpha/port.c
@@ -1,11 +1,40 @@
-
 /*
- *  SENDANNOUNCE.C
+ *   FILE
+ *	port.c
  *
- *  A do-nothing routine?
+ *   DESCRIPTION
+ *	OSF/1-specific routines
+ *
+ *   INTERFACE ROUTINES
+ *	init_address_fixup
+ *
+ *   NOTES
+ *
+ *   IDENTIFICATION
+ *	$Header$
  */
-#define RcsId(a)
+
+#include <sys/types.h>
+#include <sys/sysinfo.h>
+#include <sys/proc.h>
+
+#include "tmp/c.h"
+
+#include "utils/log.h"
 
 RcsId("$Header$");
 
-int i; /* to avoid "no symbol table" warning in some linkers */
+init_address_fixup()
+{
+#ifdef NOFIXADE
+    int buffer[] = { SSIN_UACPROC, UAC_SIGBUS };
+#endif /* NOFIXADE */
+#ifdef NOPRINTADE
+    int buffer[] = { SSIN_UACPROC, UAC_NOPRINT };
+#endif /* NOPRINTADE */
+
+    if (setsysinfo(SSI_NVPAIRS, buffer, 1, (caddr_t) NULL,
+		   (unsigned long) NULL) < 0) {
+	elog(NOTICE, "setsysinfo failed: %d\n", errno);
+    }
+}
