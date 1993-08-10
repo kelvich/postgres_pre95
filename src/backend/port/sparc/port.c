4a90a653d626b367719937fa9def08f8715b67f2
aoki <aoki>
1993-08-10 19:54:48 +0000

    cosmetic changes

--- src/backend/port/sparc/port.c
+++ src/backend/port/sparc/port.c
@@ -1,17 +1,31 @@
 /*
- * Hackety-hack for Sparc compiler error.
+ *   FILE
+ *	port.c
+ *
+ *   DESCRIPTION
+ *	SunOS4-specific routines
+ *
+ *   INTERFACE ROUTINES
+ *	sparc_bug_set_outerjoincost
+ *
+ *   NOTES
+ *
+ *   IDENTIFICATION
+ *	$Header$
  */
-
 #include "nodes/relation.h"
 
 RcsId("$Header$");
 
+/*
+ * Hackety-hack for Sparc compiler error.
+ *
+ * See planner/path/joinpath.c
+ */
 void
 sparc_bug_set_outerjoincost(p, val)
-
-char *p;
-long val;
-
+    char *p;
+    long val;
 {
-     set_outerjoincost((Path)p, (Cost)val);
+    set_outerjoincost((Path)p, (Cost)val);
 }
