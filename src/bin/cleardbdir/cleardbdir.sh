6f0907505b9f2b687deb2ee2579003df135bc54f
aoki <aoki>
1993-09-21 23:22:33 +0000

    the directory line always wraps :-P

--- src/bin/cleardbdir/cleardbdir.sh
+++ src/bin/cleardbdir/cleardbdir.sh
@@ -11,7 +11,8 @@
 # ----------------------------------------------------------------
 [ -z "$PGDATA" ] && PGDATA=_fUnKy_DATADIR_sTuFf_
 
-echo "This program completely destroys all the databases in $PGDATA"
+echo "This program completely destroys all the databases in the directory"
+echo "$PGDATA"
 echo _fUnKy_DASH_N_sTuFf_ "Are you sure you want to do this (y/n) [n]? "_fUnKy_BACKSLASH_C_sTuFf_
 read resp || exit
 case $resp in
