fdca4daed7b0c6fcdc0bc8fa634ccd1782c514d7
marcel <marcel>
1993-09-22 21:22:21 +0000

    add marcveel to the pg_user lists

--- src/backend/catalog/pg_user.h
+++ src/backend/catalog/pg_user.h
@@ -94,6 +94,7 @@ DATA(insert OID = 0 ( hartzell 6676 t t t t ));
 DATA(insert OID = 0 ( devine 6724 t t t t ));
 DATA(insert OID = 0 ( boris 6396 t t t t ));
 DATA(insert OID = 0 ( sklower 354 t t t t ));
+DATA(insert OID = 0 ( marcel 31113 t t t t ));
 
 BKI_BEGIN
 #endif ALLOW_PG_GROUP
