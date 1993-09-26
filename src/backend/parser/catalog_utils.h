bb76af2aa962f2e31bc6fde3848b045e8dabd48e
paxson <paxson>
1993-09-26 00:02:32 +0000

    added extern for varisset()
    added extern for new function get_id_typname()

--- src/backend/parser/catalog_utils.h
+++ src/backend/parser/catalog_utils.h
@@ -15,14 +15,16 @@ typedef HeapTuple	Operator;
 
 extern Type type(), get_id_type();
 extern OID att_typeid(), typeid();
+extern bool varisset();
 extern int16 tlen();
 extern bool tbyval();
 extern Relation get_rdesc(), get_rgdesc();
 extern char *outstr(), *instr(), *instr1(), *instr2();
 extern Operator oper(), right_oper(), left_oper();
-extern Name tname();
+extern Name tname(), get_id_typname();
 extern bool func_get_detail();
 extern void func_error();
+
 
 
 #endif
