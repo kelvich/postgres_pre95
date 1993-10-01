3b2f1234b60978e35c4a2fdbc5257f82bbacd6dc
aoki <aoki>
1993-10-01 22:07:23 +0000

    Initial revision

---
+++ src/backend/tmp/installinfo.h
@@ -0,0 +1,13 @@
+/*
+ * XXX - Bogus copy till i figure out how i want to
+ * locally configure postgres. - marc
+ */
+#ifndef INSTALLINFO_H
+#define INSTALLINFO_H
+
+#define DATAHOME "$TREE"                        /* loc of data/ dir */
+#define AMIINT "$TREE/$OD/support/backend"      /* for vacuumd */
+#define SRCDIR "$TREE/src/support"              /* source directory */
+#define SCRDIR "$TREE/$OD/support"              /* script directory */
+
+#endif
