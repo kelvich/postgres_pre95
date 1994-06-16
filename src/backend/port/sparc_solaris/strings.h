43151e48543a5f4444b2309f6b5e8647c51e6c89
aoki <aoki>
1994-06-16 03:23:06 +0000

    add b* routines

--- src/backend/port/sparc_solaris/strings.h
+++ src/backend/port/sparc_solaris/strings.h
@@ -17,7 +17,10 @@
 
 #include <string.h>
 
-#define	index(s,c)	strchr(s,c)
-#define	rindex(s,c)	strrchr(s,c)
+#define	index(s,c)	strchr((s),(c))
+#define	rindex(s,c)	strrchr((s),(c))
+#define bcopy(s,d,len)	memmove((d),(s),(size_t)(len))
+#define bzero(b,len)    memset((b),0,(size_t)(len))
+#define bcmp(s,d,len)	memcmp((s),(d),(size_t)(len))
 
 #endif /* stringsIncluded */
